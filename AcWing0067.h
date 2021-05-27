3983f92ae0cb86498ccbc8b572b06c6351df1469
fengwei <wuzhongfengwei@163.com>
2021-05-28 05:05:53 +0800

    Finished Problem 67.

---
+++ AcWing0067.h
@@ -0,0 +1,23 @@
+//
+// Created by Fengwei Zhang on 2021/5/28.
+//
+
+#ifndef ACWINGSOLUTION_ACWING0067_H
+#define ACWINGSOLUTION_ACWING0067_H
+
+#include <vector>
+
+using namespace std;
+
+class Problem0067 {
+public:
+    int getNumberOfK(vector<int> &nums, int k) {
+        int result = 0;
+        for (auto i = nums.begin(); i < nums.end(); ++i) {
+            result += *i == k ? 1 : 0;
+        }
+        return result;
+    }
+};
+
+#endif //ACWINGSOLUTION_ACWING0067_H
