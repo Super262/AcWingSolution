8a58db520c3d3e490dd24d258db0f9124688c564
fengwei <wuzhongfengwei@163.com>
2021-05-31 12:36:32 +0800

    Finished Problem 793.

---
+++ Problem0793.h
@@ -0,0 +1,44 @@
+//
+// Created by Fengwei Zhang on 2021/5/31.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0793_H
+#define ACWINGSOLUTION_PROBLEM0793_H
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Problem0793 {
+public:
+    string mulBigInts(string a, int b) {
+        reverse(a.begin(), a.end());
+        string result;
+        unsigned long i = 0;
+        int t = 0;
+        while (i < a.size() || t != 0) {
+            if (i < a.size()) {
+                t += (a[i] - '0') * b;
+            }
+            result.push_back((char) ('0' + t % 10));
+            t /= 10;
+            ++i;
+        }
+        while (result.size() > 1 && result.back() == '0') {
+            result.pop_back();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+
+    int main() {
+        string a;
+        int b;
+        cin >> a >> b;
+        cout << mulBigInts(a, b) << endl;
+        return 0;
+    }
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0793_H
