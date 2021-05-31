052788779aeeb9c6612ff37df2837f61313c0823
fengwei <wuzhongfengwei@163.com>
2021-05-31 13:32:43 +0800

    Finished Problem 795.

---
+++ Problem0795.h
@@ -0,0 +1,40 @@
+//
+// Created by Fengwei Zhang on 2021/5/31.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0795_H
+#define ACWINGSOLUTION_PROBLEM0795_H
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Problem0795 {
+public:
+    vector<int> getPrefixSum(const vector<int> &a) {
+        vector<int> result(a.size() + 1, 0);
+        for (unsigned long i = 1; i < result.size(); ++i) {
+            result[i] = result[i - 1] + a[i - 1];
+        }
+        return result;
+    }
+
+    int main() {
+        int n, m;
+        cin >> n >> m;
+        vector<int> arr(n, 0);
+        for (int &num:arr) {
+            cin >> num;
+        }
+        vector<int> prefixSum = getPrefixSum(arr);
+        int l, r;
+        for (int i = 0; i < m; ++i) {
+            cin >> l >> r;
+            cout << prefixSum[r] - prefixSum[l - 1] << endl;
+        }
+        return 0;
+    }
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0795_H
