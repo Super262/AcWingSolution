b43de96256ab0248569968e77791f0603f83aa06
fengwei <wuzhongfengwei@163.com>
2021-06-05 11:51:28 +0800

    Finished Problem 828.

---
+++ Problem0828.h
@@ -0,0 +1,55 @@
+//
+// Created by Fengwei Zhang on 2021/6/5.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0828_H
+#define ACWINGSOLUTION_PROBLEM0828_H
+
+#include <iostream>
+
+using namespace std;
+
+class Problem0828 {
+public:
+    void pop(unsigned long &top) {
+        --top;
+    }
+
+    void push(const int value, int data[], unsigned long &top) {
+        data[++top] = value;
+    }
+
+    bool empty(const unsigned long top) {
+        return top == 0;
+    }
+
+    int peek(const int data[], const unsigned long top) {
+        return data[top];
+    }
+
+    int main() {
+        unsigned long m;
+        cin >> m;
+        int *data = new int[m];
+        unsigned long top = 0;
+        string op;
+        int value;
+        for (int i = 0; i < m; ++i) {
+            cin >> op;
+            if (op == "push") {
+                cin >> value;
+                push(value, data, top);
+            } else if (op == "query") {
+                cout << peek(data, top) << endl;
+            } else if (op == "empty") {
+                cout << (empty(top) ? "YES" : "NO") << endl;
+            } else {
+                pop(top);
+            }
+        }
+        return 0;
+    }
+
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0828_H
