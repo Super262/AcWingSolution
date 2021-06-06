c6b563d3fae1a66c56d324a65f9790d18dbc188c
fengwei <wuzhongfengwei@163.com>
2021-06-06 09:43:57 +0800

    Finished Problem 154.

--- Problem0154.h
+++ Problem0154.h
@@ -12,54 +12,54 @@
 
 class Problem0154 {
 public:
-    void printMinInWindow(const vector<int> &arr, const long windowSize) {
-        if (windowSize >= arr.size()) {
+    void printMinInWindow(const vector<int> &arr, const long k) {
+        if (k >= arr.size()) {
             printf("%d\n", *min_element(arr.begin(), arr.end()));
             return;
         }
         deque<int> q;
-        long i = 0;
+        long = 0;
         auto p = arr.begin();
         while (p != arr.end()) {
-            while (i < windowSize) {
+            while (currentWindowSize < k) {
                 while (!q.empty() && q.back() > *p) { // 注意：边界条件应是">"，不是">="
                     q.pop_back();
                 }
                 q.emplace_back(*p);
-                ++i;
+                ++;
                 ++p;
             }
             printf("%d ", q.front());
-            if (q.front() == *(p - windowSize)) {
+            if (q.front() == *(p - k)) {
                 q.pop_front();
             }
-            --i;
+            --;
         }
         printf("\n");
     }
 
-    void printMaxInWindow(const vector<int> &arr, const long windowSize) {
-        if (windowSize >= arr.size()) {
+    void printMaxInWindow(const vector<int> &arr, const long k) {
+        if (k >= arr.size()) {
             printf("%d\n", *max_element(arr.begin(), arr.end()));
             return;
         }
         deque<int> q;
-        long i = 0;
+        long currentWindowSize = 0;
         auto p = arr.begin();
         while (p != arr.end()) {
-            while (i < windowSize) {
+            while (currentWindowSize < k) {
                 while (!q.empty() && q.back() < *p) {  // 注意：边界条件应是"<"，不是"<="
                     q.pop_back();
                 }
                 q.emplace_back(*p);
-                ++i;
+                ++currentWindowSize;
                 ++p;
             }
             printf("%d ", q.front());
-            if (q.front() == *(p - windowSize)) {
+            if (q.front() == *(p - k)) {
                 q.pop_front();
             }
-            --i;
+            --currentWindowSize;
         }
         printf("\n");
     }
