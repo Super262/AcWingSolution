eb6e7f53d36a320b0514d8571289cdd5c0cf192b
fengwei <wuzhongfengwei@163.com>
2021-06-09 23:13:50 +0800

    Finished Problem 853.

---
+++ Problem0853.h
@@ -0,0 +1,51 @@
+//
+// Created by Fengwei Zhang on 2021/6/9.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0853_H
+#define ACWINGSOLUTION_PROBLEM0853_H
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Problem0853 {
+public:
+    int bellman_ford(const int n,
+                     const int edgesCount,
+                     int pathLen,
+                     const vector<int> &edgeStart,
+                     const vector<int> &edgeEnd,
+                     const vector<int> &edgeWeight) {
+        vector<int> distance(n + 1, 0x3f3f3f3f);
+        distance[1] = 0;
+        while (pathLen--) {
+            const vector<int> prevDis(distance);
+            for (int j = 0; j < edgesCount; ++j) {
+                distance[edgeEnd[j]] = min(distance[edgeEnd[j]], prevDis[edgeStart[j]] + edgeWeight[j]);
+            }
+        }
+        return distance[n] >= 0x3f3f3f3f / 2 ? -1 : distance[n];
+    }
+
+    int main() {
+        int n, m, k;
+        scanf("%d%d%d", &n, &m, &k);
+        vector<int> edgeStart(m, 0);
+        vector<int> edgeEnd(m, 0);
+        vector<int> edgeWeight(m, 0);
+        for (int i = 0; i < m; ++i) {
+            scanf("%d%d%d", &edgeStart[i], &edgeEnd[i], &edgeWeight[i]);
+        }
+        int result = bellman_ford(n, m, k, edgeStart, edgeEnd, edgeWeight);
+        if (result == -1) {
+            printf("impossible\n");
+        } else {
+            printf("%d\n", result);
+        }
+        return 0;
+    }
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0853_H
