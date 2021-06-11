3f5f5a6df7e4d287f749a149da9143e150bf32ad
fengwei <wuzhongfengwei@163.com>
2021-06-12 01:52:59 +0800

    Finished Problem 866.

---
+++ Problem0866.h
@@ -0,0 +1,43 @@
+//
+// Created by Fengwei Zhang on 2021/6/12.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0866_H
+#define ACWINGSOLUTION_PROBLEM0866_H
+
+#include <iostream>
+
+using namespace std;
+
+class Problem0866 {
+public:
+    bool isPrime(const int &num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= num / i; ++i) {
+            if (num % i) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    int main() {
+        int n;
+        scanf("%d", &n);
+        int num;
+        while (n--) {
+            scanf("%d", &num);
+            if (isPrime(num)) {
+                puts("Yes");
+            } else {
+                puts("No");
+            }
+        }
+        return 0;
+    }
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0866_H
