4ef670bd1b819bcbf029d0cf8395dbe273093918
fengwei <wuzhongfengwei@163.com>
2021-06-12 16:06:55 +0800

    Refined code.

--- Problem0868.h
+++ Problem0868.h
@@ -13,25 +13,25 @@ using namespace std;
 class Problem0868 {
 public:
     int primesCount(const int n) {
-        bool *isNotPrime = new bool[n + 1];
+        bool *isFiltered = new bool[n + 1];
         int *primes = new int[n + 1];
-        memset(isNotPrime, 0, (n + 1) * sizeof(bool));
+        memset(isFiltered, 0, (n + 1) * sizeof(bool));
         memset(primes, 0, (n + 1) * sizeof(int));
-        int result = 0;
+        int count = 0;
         for (int f = 2; f <= n; ++f) {
-            if (!isNotPrime[f]) {
-                primes[result++] = f;
+            if (!isFiltered[f]) {
+                primes[count++] = f;
             }
             for (int j = 0; primes[j] <= n / f; ++j) {
-                isNotPrime[primes[j] * f] = true;
+                isFiltered[primes[j] * f] = true;
                 if (f % primes[j] == 0) {
                     break;
                 }
             }
         }
-        delete[] isNotPrime;
+        delete[] isFiltered;
         delete[] primes;
-        return result;
+        return count;
     }
 
     int main() {
