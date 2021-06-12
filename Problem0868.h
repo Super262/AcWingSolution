86c35a17ade881a5156a16081ef6464defc001a1
fengwei <wuzhongfengwei@163.com>
2021-06-12 10:12:01 +0800

    Finished Problem 868.

--- Problem0868.h
+++ Problem0868.h
@@ -13,19 +13,24 @@ using namespace std;
 class Problem0868 {
 public:
     int primesCount(const int n) {
-        int result = 0;
         bool *isNotPrime = new bool[n + 1];
+        int *primes = new int[n + 1];
         memset(isNotPrime, 0, (n + 1) * sizeof(bool));
+        memset(primes, 0, (n + 1) * sizeof(int));
+        int result = 0;
         for (int f = 2; f <= n; ++f) {
-            if (isNotPrime[f]) {
-                continue;
+            if (!isNotPrime[f]) {
+                primes[result++] = f;
             }
-            ++result;
-            for (int j = f * 2; j <= n; j += f) {
-                isNotPrime[j] = true;
+            for (int j = 0; primes[j] <= n / f; ++j) {
+                isNotPrime[primes[j] * f] = true;
+                if (f % primes[j] == 0) {
+                    break;
+                }
             }
         }
         delete[] isNotPrime;
+        delete[] primes;
         return result;
     }
 
