14ed7bd8c2c6d375f0f06549337980641188300d
fengwei <wuzhongfengwei@163.com>
2021-06-12 09:06:02 +0800

    Finished Problem 867.

---
+++ Problem0867.h
@@ -0,0 +1,44 @@
+//
+// Created by Fengwei Zhang on 2021/6/12.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0867_H
+#define ACWINGSOLUTION_PROBLEM0867_H
+
+#include <iostream>
+
+using namespace std;
+
+class Problem0867 {
+public:
+    void primeFactorizations(int n) {
+        int s;
+        for (int f = 2; f <= n / f; ++f) {
+            if (n % f == 0) {
+                s = 0;
+                while (n % f == 0) {
+                    n /= f;
+                    ++s;
+                }
+                printf("%d %d\n", f, s);
+            }
+        }
+        if (n > 1) {
+            printf("%d %d\n", n, 1);
+        }
+        printf("\n");
+    }
+
+    int main() {
+        int m;
+        scanf("%d", &m);
+        int n;
+        while (m--) {
+            scanf("%d", &n);
+            primeFactorizations(n);
+        }
+        return 0;
+    }
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0867_H
