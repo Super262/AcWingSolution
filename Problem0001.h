1a8728ed66f92f636e6a4e477f7b1c674509498e
fengwei <wuzhongfengwei@163.com>
2021-06-17 20:01:37 +0800

    Finished Problem 1.

---
+++ Problem0001.h
@@ -0,0 +1,24 @@
+//
+// Created by Fengwei Zhang on 2021/6/17.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0001_H
+#define ACWINGSOLUTION_PROBLEM0001_H
+
+#include <iostream>
+
+using namespace std;
+
+class Problem0001 {
+public:
+    int main() {
+        int a;
+        int b;
+        cin >> a;
+        cin >> b;
+        cout << a + b << endl;
+        return 0;
+    }
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0001_H
