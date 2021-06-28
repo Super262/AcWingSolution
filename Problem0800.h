dd1d80d43216002e9d71e0cd178705e89cbb587e
fengwei <wuzhongfengwei@163.com>
2021-06-28 20:12:44 +0800

    Refined code.

--- Problem0800.h
+++ Problem0800.h
@@ -7,37 +7,38 @@
 
 #include <iostream>
 
+using namespace std;
+
 class Problem0800 {
 public:
-    using namespace std;
-
     int main() {
-        int n, m, x;
-        scanf("%d %d %d\n", &n, &m, &x);
-        int *a = new int[n];
-        int *b = new int[m];
-        for (int i = 0; i < n; ++i) {
-            scanf("%d", a + i);
+        unsigned long long n, m;
+        int target;
+        scanf("%lld%lld%d", &n, &m, &target);
+        auto arr1 = new int[n];
+        auto arr2 = new int[m];
+        for (unsigned long long i = 0; i < n; ++i) {
+            scanf("%d", &arr1[i]);
         }
-        for (int j = 0; j < m; ++j) {
-            scanf("%d", b + j);
+        for (unsigned long long i = 0; i < m; ++i) {
+            scanf("%d", &arr2[i]);
         }
-        int i = 0;
-        int j = m - 1;
+        unsigned long long i = 0;
+        unsigned long long j = m - 1;
         while (i < n && j >= 0) {
-            if (a[i] + b[j] == x) {
-                printf("%d %d\n", i, j);
-                ++i;
-                --j;
-            } else if (a[i] + b[j] > x) {
+            if (arr1[i] + arr2[j] == target) {
+                printf("%llu %llu\n", i, j);
+                break;
+            } else if (arr1[i] + arr2[j] > target) {
                 --j;
             } else {
                 ++i;
             }
         }
+        delete[] arr1;
+        delete[] arr2;
         return 0;
     }
-
 };
 
 #endif //ACWINGSOLUTION_PROBLEM0800_H
