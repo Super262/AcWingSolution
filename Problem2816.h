eb4c5b92a52cba572b4c78bc32c54a56d41797c2
fengwei <wuzhongfengwei@163.com>
2021-06-28 20:24:00 +0800

    Refined code.

--- Problem2816.h
+++ Problem2816.h
@@ -12,36 +12,39 @@ using namespace std;
 
 class Problem2816 {
 public:
-    bool isSubSeq(const vector<int> &child, const vector<int> &parent) {
-        if (child.empty() || child.size() > parent.size()) {
-            return false;
-        }
-        unsigned long i = 0;
-        unsigned long j = 0;
-        while (i < child.size() && j < parent.size()) {
-            if (child[i] == parent[j]) {
+    bool isSubseq(const int a[], const unsigned int m, const int b[], const unsigned int n) {
+        unsigned int i = 0;
+        unsigned int j = 0;
+        while(j < n && i < m) {
+            if(a[i] == b[j]) {
                 ++i;
             }
             ++j;
         }
-        if (i < child.size()) {
-            return false;
+        if (i == m) {
+            return true;
         }
-        return true;
+        return false;
     }
 
     int main() {
-        unsigned long n, m;
-        scanf("%ld%ld", &n, &m);
-        vector<int> child(n, 0);
-        vector<int> parent(m, 0);
-        for (unsigned long i = 0; i < n; ++i) {
-            scanf("%d", &child[i]);
+        unsigned int m, n;
+        scanf("%d%d", &m, &n);
+        auto a = new int[m];
+        auto b = new int[n];
+        for (unsigned int i = 0; i < m; ++i) {
+            scanf("%d", &a[i]);
         }
-        for (unsigned long i = 0; i < m; ++i) {
-            scanf("%d", &parent[i]);
+        for (unsigned int i = 0; i < n; ++i) {
+            scanf("%d", &b[i]);
         }
-        printf("%s", (isSubSeq(child, parent) ? "Yes" : "No"));
+        if (isSubseq(a, m, b, n)) {
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
+        delete[] a;
+        delete[] b;
         return 0;
     }
 };
