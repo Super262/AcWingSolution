5c8cbbfaebed801eae5d98f4a617e33356408591
fengwei <wuzhongfengwei@163.com>
2021-06-30 10:47:09 +0800

    Refined code.

--- Problem0143.h
+++ Problem0143.h
@@ -6,20 +6,15 @@
 #define ACWINGSOLUTION_PROBLEM0143_H
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 class Problem0143 {
 public:
-    unsigned long nextIdx = 0;
-    unsigned long storage[100010 * 31][2];
-
-    void insertNum(const unsigned int &value) {
-        unsigned long currentIdx = 0;
-        bool chIndex;
+    void insertNum(unsigned int storage[][2], const unsigned int num, unsigned int &nextIdx) {
+        unsigned int currentIdx = 0;
         for (int i = 30; i >= 0; --i) {
-            chIndex = (value >> i) & 1;
+            bool chIndex = (num >> i) & 1;
             if (!storage[currentIdx][chIndex]) {
                 storage[currentIdx][chIndex] = ++nextIdx;
             }
@@ -27,32 +22,33 @@ public:
         }
     }
 
-    unsigned int getResult(const unsigned int &value) {
+    unsigned int maxXOR(const unsigned int storage[][2], const unsigned int num) {
         unsigned int result = 0;
-        unsigned long currentIdx = 0;
-        bool chIndex;
+        unsigned int currentIdx = 0;
         for (int i = 30; i >= 0; --i) {
-            chIndex = (value >> i) & 1;
+            bool chIndex = (num >> i) & 1;
             if (storage[currentIdx][!chIndex]) {
-                currentIdx = storage[currentIdx][!chIndex];
                 result = result * 2 + 1;
+                currentIdx = storage[currentIdx][!chIndex];
             } else {
-                currentIdx = storage[currentIdx][chIndex];
                 result = result * 2 + 0;
+                currentIdx = storage[currentIdx][chIndex];
             }
         }
         return result;
     }
 
     int main() {
-        unsigned long n;
-        scanf("%ld", &n);
+        unsigned int n;
+        scanf("%d", &n);
         unsigned int result = 0;
-        unsigned int temp;
-        while (n--) {
-            scanf("%d", &temp);
-            insertNum(temp);
-            result = max(result, getResult(temp));
+        unsigned int storage[31 * 100001][2];
+        unsigned int num;
+        unsigned int nextIdx = 0;
+        for (unsigned int i = 0; i < n; ++i) {
+            scanf("%d", &num);
+            insertNum(storage, num, nextIdx);
+            result = max(result, maxXOR(storage, num));
         }
         printf("%d\n", result);
         return 0;
