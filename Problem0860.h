8154eec995f4dff1fc875bec3ae614f596fcbca5
fengwei <wuzhongfengwei@163.com>
2021-07-02 10:12:42 +0800

    Refined code.

--- Problem0860.h
+++ Problem0860.h
@@ -5,45 +5,25 @@
 #ifndef ACWINGSOLUTION_PROBLEM0860_H
 #define ACWINGSOLUTION_PROBLEM0860_H
 
-#include <vector>
+#include <cstring>
 #include <iostream>
 
 using namespace std;
 
 class Problem0860 {
 public:
-    void addEdge(const int a,
-                 const int b,
-                 vector<int> &headIndex,
-                 vector<int> &vertexValue,
-                 vector<int> &nextIndex,
-                 int &idx) {
-        if (idx >= vertexValue.size()) {
-            vertexValue.emplace_back(0);
-        }
-        if (idx >= nextIndex.size()) {
-            nextIndex.emplace_back(-1);
-        }
+    void addEdge(const int a, const int b, int *headIndex, int *vertexValue, int *nextIndex, int &idx) {
         vertexValue[idx] = b;
         nextIndex[idx] = headIndex[a];
         headIndex[a] = idx;
         ++idx;
     }
 
-    bool dfs(const int start,
-             const int c,
-             vector<int> &color,
-             const vector<int> &headIndex,
-             const vector<int> &vertexValue,
-             const vector<int> &nextIndex) {
-        color[start] = c;
-        for (int neighborIdx = headIndex[start]; neighborIdx != -1; neighborIdx = nextIndex[neighborIdx]) {
-            int v = vertexValue[neighborIdx];
-            if (!color[v]) {
-                if (!dfs(v, 3 - c, color, headIndex, vertexValue, nextIndex)) {
-                    return false;
-                }
-            } else if (color[v] == c) {
+    bool dfs(const int start, const int c, int *colorValue, const int *headIndex, const int *vertexValue, const int *nextIndex) {
+        colorValue[start] = c;
+        for (int idx = headIndex[start]; idx != -1; idx = nextIndex[idx]) {
+            auto neighbor = vertexValue[idx];
+            if (colorValue[neighbor] == c || (!colorValue[neighbor] && !dfs(neighbor, 3 - c, colorValue, headIndex, vertexValue, nextIndex))) {
                 return false;
             }
         }
@@ -53,27 +33,40 @@ public:
     int main() {
         int n, m;
         scanf("%d%d", &n, &m);
-        vector<int> headIndex(n + 1, -1);
-        vector<int> vertexValue(2 * n + 1, 0);
-        vector<int> nextIndex(2 * n + 1, -1);
+        auto headIndex = new int[n + 1];
+        auto vertexValue = new int[2 * m];
+        auto nextIndex = new int[2 * m];
         int idx = 0;
-        int u, v;
-        while (m--) {
-            scanf("%d%d", &u, &v);
-            addEdge(u, v, headIndex, vertexValue, nextIndex, idx);
-            addEdge(v, u, headIndex, vertexValue, nextIndex, idx);
+        memset(headIndex, -1, sizeof(int) * (n + 1));
+        memset(vertexValue, 0, sizeof(int) * (2 * m));
+        memset(nextIndex, -1, sizeof(int) * (2 * m));
+        int x, y;
+        for (int i = 0; i < m; ++i) {
+            scanf("%d%d", &x, &y);
+            addEdge(x, y, headIndex, vertexValue, nextIndex, idx);
+            addEdge(y, x, headIndex, vertexValue, nextIndex, idx);
         }
-        vector<int> color(n + 1, 0);
-        for (int j = 1; j <= n; ++j) {
-            if (color[j]) {
+        auto colorValue = new int[n + 1];
+        memset(colorValue, 0, sizeof(int) * (n + 1));
+        bool result = true;
+        for (int v = 1; v <= n; ++v) {
+            if (colorValue[v]) {
                 continue;
             }
-            if (!dfs(j, 1, color, headIndex, vertexValue, nextIndex)) {
-                puts("No");
-                return 0;
+            if (!dfs(v, 1, colorValue, headIndex, vertexValue, nextIndex)) {
+                result = false;
+                break;
             }
         }
-        puts("Yes");
+        if (result) {
+            puts("Yes");
+        } else {
+            puts("No");
+        }
+        delete[] headIndex;
+        delete[] vertexValue;
+        delete[] nextIndex;
+        delete[] colorValue;
         return 0;
     }
 };
