ccd82c09a64b0b92f0dc6b94ed838f9db8984c44
fengwei <wuzhongfengwei@163.com>
2021-07-04 00:47:38 +0800

    Refined code.

--- Problem0895.h
+++ Problem0895.h
@@ -11,19 +11,19 @@ using namespace std;
 
 class Problem0895 {
 public:
-    unsigned int longestAscendingSubsequence(const int *arr, const unsigned int n) {
-        auto dp = new unsigned int[n];
-        for (unsigned int i = 0; i < n; ++i) {
+    int longestAscendingSubsequence(const int *arr, const int n) {
+        auto dp = new int[n];
+        for (int i = 0; i < n; ++i) {
             dp[i] = 1;
-            for (unsigned int j = 0; j < i; ++j) {
+            for (int j = 0; j < n; ++j) {
                 if (arr[j] >= arr[i]) {
                     continue;
                 }
                 dp[i] = max(dp[i], dp[j] + 1);
             }
         }
-        unsigned int result = 0;
-        for (unsigned int i = 0; i < n; ++i) {
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
             result = max(result, dp[i]);
         }
         delete[] dp;
@@ -31,14 +31,13 @@ public:
     }
 
     int main() {
-        unsigned int n;
+        int n;
         scanf("%d", &n);
         auto arr = new int[n];
-        for (unsigned int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             scanf("%d", &arr[i]);
         }
         printf("%d\n", longestAscendingSubsequence(arr, n));
-        delete[] arr;
         return 0;
     }
 };
