136d40c01d63d740e7253ec3670cb6d5a2bef6f6
fengwei <wuzhongfengwei@163.com>
2021-07-03 23:55:36 +0800

    Refined code.

--- Problem0898.h
+++ Problem0898.h
@@ -12,26 +12,25 @@ using namespace std;
 
 class Problem0898 {
 public:
-    int maxPath(int *const *matrix, const int n) {
-        const int MIN_VALUE = -50000000;
-        auto **dp = new int *[n + 1];
-        for (int i = 0; i <= n; ++i) {
-            dp[i] = new int[i + 2];  // 注意这里的长度：i + 2
-            for (int j = 0; j <= i + 1; ++j) {
-                dp[i][j] = MIN_VALUE;
+    int maxPath(int **matrix, const int n) {
+        auto dp = new int*[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = new int[i + 2];
+            for (int j = 0; j < i + 2; ++j) {
+                dp[i][j] = -0x7f7f7f7f;
             }
         }
         dp[1][1] = matrix[1][1];
         for (int i = 2; i <= n; ++i) {
             for (int j = 1; j <= i; ++j) {
-                dp[i][j] = max(dp[i - 1][j] + matrix[i][j], dp[i - 1][j - 1] + matrix[i][j]);
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + matrix[i][j];
             }
         }
-        int result = MIN_VALUE;
-        for (int i = 1; i <= n; ++i) {
+        int result = -0x7f7f7f7f;
+        for (int i = 0; i < n + 2; ++i) {
             result = max(result, dp[n][i]);
         }
-        for (int i = 0; i <= n; ++i) {
+        for (int i = 1; i <= n; ++i) {
             delete[] dp[i];
         }
         delete[] dp;
@@ -41,17 +40,15 @@ public:
     int main() {
         int n;
         scanf("%d", &n);
-        auto **matrix = new int *[n + 1];
-        for (int i = 0; i <= n; ++i) {
-            matrix[i] = new int[i + 1];
-        }
+        auto matrix = new int*[n + 1];
         for (int i = 1; i <= n; ++i) {
+            matrix[i] = new int[i + 1];
             for (int j = 1; j <= i; ++j) {
                 scanf("%d", &matrix[i][j]);
             }
         }
         printf("%d\n", maxPath(matrix, n));
-        for (int i = 0; i <= n; ++i) {
+        for (int i = 1; i <= n; ++i) {
             delete[] matrix[i];
         }
         delete[] matrix;
