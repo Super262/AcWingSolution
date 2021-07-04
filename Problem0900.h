abd03a403498372a182e7347b0c426389203a8c7
fengwei <wuzhongfengwei@163.com>
2021-07-04 16:23:25 +0800

    Refined code.

--- Problem0900.h
+++ Problem0900.h
@@ -12,24 +12,24 @@ using namespace std;
 
 class Problem0900 {
 public:
-    unsigned int combinationsOfSum(const unsigned int sum) {
-        auto dp = new unsigned int[sum + 1];
-        memset(dp, 0, sizeof(unsigned int) * (sum + 1));
-        dp[0] = 1;
-        for (unsigned int s = 1; s <= sum; ++s) {
-            for (unsigned int num = s; num <= sum; ++num) {
-                dp[num] = (dp[num] + dp[num - s]) % (1000000007);
+    int combinationsOfSum(const int sum) {
+        auto dp = new int[sum + 1];
+        memset(dp, 0, sizeof(int) * (sum + 1));
+        dp[0] = 1;  // 不要忘记初始化！
+        for (int s = 1; s <= sum; ++s) {
+            for (int num = s; num <= sum; ++num) {
+                dp[num] = (dp[num] + dp[num - s]) % 1000000007;
             }
         }
-        unsigned int result = dp[sum];
+        int result = dp[sum];
         delete[] dp;
         return result;
     }
 
     int main() {
-        unsigned int sum;
-        scanf("%d", &sum);
-        printf("%d\n", combinationsOfSum(sum));
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", combinationsOfSum(n));
         return 0;
     }
 };
