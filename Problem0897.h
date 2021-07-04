2c2bcbf876ab338792387d56becaf7d482498073
fengwei <wuzhongfengwei@163.com>
2021-07-04 15:36:02 +0800

    Refined code.

--- Problem0897.h
+++ Problem0897.h
@@ -12,22 +12,22 @@ using namespace std;
 
 class Problem0897 {
 public:
-    unsigned int longestCommonSequence(const char *a, const unsigned int l1, const char *b, const unsigned int l2) {
-        auto dp = new unsigned int *[l1 + 1];
-        for (unsigned int i = 0; i <= l1; ++i) {
-            dp[i] = new unsigned int[l2 + 1];
-            memset(dp[i], 0, sizeof(unsigned int) * (l2 + 1));
+    int longestCommonSequence(const char *a, const int l1, const char *b, const int l2) {
+        auto dp = new int*[l1 + 1];
+        for (int i = 0; i <= l1; ++i) {
+            dp[i] = new int[l2 + 1];
+            memset(dp[i], 0, sizeof(int) * (l2 + 1));
         }
-        for (unsigned int i = 1; i <= l1; ++i) {
-            for (unsigned int j = 1; j <= l2; ++j) {
+        for (int i = 1; i <= l1; ++i) {
+            for (int j = 1; j <= l2; ++j) {
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 if (a[i - 1] == b[j - 1]) {
                     dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
                 }
             }
         }
-        unsigned int result = dp[l1][l2];
-        for (unsigned int i = 0; i <= l1; ++i) {
+        int result = dp[l1][l2];
+        for (int i = 0; i <= l1; ++i) {
             delete[] dp[i];
         }
         delete[] dp;
@@ -35,7 +35,7 @@ public:
     }
 
     int main() {
-        unsigned int l1, l2;
+        int l1, l2;
         scanf("%d%d", &l1, &l2);
         auto a = new char[l1 + 1];
         auto b = new char[l2 + 1];
