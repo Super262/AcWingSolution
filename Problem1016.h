4c5d6fb0123c9ed0c280984041e85a276fbae6b6
fengwei <wuzhongfengwei@163.com>
2021-07-07 16:44:48 +0800

    Finished Problem 1016.

---
+++ Problem1016.h
@@ -0,0 +1,40 @@
+//
+// Created by Fengwei Zhang on 2021/7/7.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM1016_H
+#define ACWINGSOLUTION_PROBLEM1016_H
+
+class Problem1016 {
+public:
+    int maxSumLIS(const int *a, const int N) {
+        int result = 0;
+        auto dp = new int[N];
+        for (int i = 0; i < N; ++i) {
+            dp[i] = a[i];
+            for (int j = 0; j < i; ++j) {
+                if (a[j] >= a[i]) {
+                    continue;
+                }
+                dp[i] = max(dp[i], dp[j] + a[i]);
+            }
+            result = max(dp[i], result);
+        }
+        delete[] dp;
+        return result;
+    }
+
+    int main() {
+        int n;
+        scanf("%d", &n);
+        auto a = new int[n];
+        for (int i = 0; i < n; ++i) {
+            scanf("%d", &a[i]);
+        }
+        printf("%d\n", maxSumLIS(a, n));
+        delete[] a;
+        return 0;
+    }
+};
+
+#endif //ACWINGSOLUTION_PROBLEM1016_H
