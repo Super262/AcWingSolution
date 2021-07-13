f7797bfc8245c2f99a936645ccccbf19e57d9e5c
fengwei <wuzhongfengwei@163.com>
2021-07-13 20:13:12 +0800

    Refined code.

--- Problem0831.h
+++ Problem0831.h
@@ -11,19 +11,19 @@ using namespace std;
 
 class Problem0831 {
 public:
-    void buildNext(const char str[], unsigned long long next[], const unsigned long long len) {
+    void buildNext(const char str[], unsigned long long nextStart[], const unsigned long long len) {
         unsigned long long i = 1;
         unsigned long long now = 0;
-        next[0] = 0;
+        nextStart[0] = 0;
         while (i < len) {
             if (str[i] == str[now]) {
                 ++now;
-                next[i] = now;
+                nextStart[i] = now;
                 ++i;
             } else if (now) {
-                now = next[now - 1];
+                now = nextStart[now - 1];
             } else {
-                next[i] = 0;
+                nextStart[i] = 0;
                 ++i;
             }
         }
@@ -32,7 +32,7 @@ public:
     void printMatchedStart(const char s[],
                            const unsigned long long sLen,
                            const char p[],
-                           const unsigned long long next[],
+                           const unsigned long long nextStart[],
                            const unsigned long long pLen) {
         unsigned long long i = 0;
         unsigned long long j = 0;
@@ -41,13 +41,13 @@ public:
                 ++i;
                 ++j;
             } else if (j) {
-                j = next[j - 1];
+                j = nextStart[j - 1];
             } else {
                 ++i;
             }
             if (j == pLen) {
                 printf("%llu ", i - pLen);
-                j = next[j - 1];
+                j = nextStart[j - 1];
             }
         }
         printf("\n");
@@ -62,12 +62,12 @@ public:
         scanf("%lld", &n);
         auto s = new char[n + 1];
         scanf("%s", s);
-        auto next = new unsigned long long[m];
-        buildNext(p, next, m);
-        printMatchedStart(s, n, p, next, m);
+        auto nextStart = new unsigned long long[m];
+        buildNext(p, nextStart, m);
+        printMatchedStart(s, n, p, nextStart, m);
         delete[] p;
         delete[] s;
-        delete[] next;
+        delete[] nextStart;
         return 0;
     }
 };
