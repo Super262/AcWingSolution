6aba87b7f42022edcd77081bd119afbb422f07eb
fengwei <wuzhongfengwei@163.com>
2021-07-20 08:37:46 +0800

    Started Problem 1086.

---
+++ Problem1086.h
@@ -0,0 +1,14 @@
+//
+// Created by Fengwei Zhang on 2021/7/20.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM1086_H
+#define ACWINGSOLUTION_PROBLEM1086_H
+
+class Problem1086 {
+    // f[i][j][a][b]  平方和（i位数的最高位为j，本身模7的余数为a，各个数字和模7的余数为b）
+    // https://www.acwing.com/solution/content/5261/
+public:
+};
+
+#endif //ACWINGSOLUTION_PROBLEM1086_H
