1abcee33c04d2a38528f88f02c2ebe79ba230570
fengwei <wuzhongfengwei@163.com>
2021-07-25 10:50:04 +0800

    Started Problem 166.

---
+++ Problem0166.h
@@ -0,0 +1,13 @@
+//
+// Created by Fengwei Zhang on 2021/7/25.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0166_H
+#define ACWINGSOLUTION_PROBLEM0166_H
+
+class Problem0166 {
+    // https://www.acwing.com/solution/content/31873/
+public:
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0166_H
