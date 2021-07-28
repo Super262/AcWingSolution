e7fcb4f0705e9dc954c871b570e4935c3261d71c
fengwei <wuzhongfengwei@163.com>
2021-07-29 07:28:28 +0800

    Started Problem 340.

---
+++ Problem0340.h
@@ -0,0 +1,13 @@
+//
+// Created by Fengwei Zhang on 2021/7/28.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM0340_H
+#define ACWINGSOLUTION_PROBLEM0340_H
+
+class Problem0340 {
+    // https://www.acwing.com/solution/content/13645/
+public:
+};
+
+#endif //ACWINGSOLUTION_PROBLEM0340_H
