17d27b734b91286399ba1909ed0461e0276f4058
fengwei <wuzhongfengwei@163.com>
2021-08-17 11:14:25 -0700

    Reviewed Problem 848.

--- Problem0848.h
+++ Problem0848.h
@@ -5,81 +5,72 @@
 #ifndef ACWINGSOLUTION_PROBLEM0848_H
 #define ACWINGSOLUTION_PROBLEM0848_H
 
-#include <vector>
-#include <queue>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 class Problem0848 {
-public:
-    void addEdge(const int a, const int b, vector<int> &headIndex, vector<int> &vertexValue, vector<int> &nextIndex,
-                 vector<int> &inDegree, int &idx) {
-        while (idx >= vertexValue.size()) {
-            vertexValue.emplace_back(0);
-        }
-        while (idx >= nextIndex.size()) {
-            nextIndex.emplace_back(-1);
-        }
-        ++inDegree[b];
+private:
+    const int N = 100010;
+    int headIndex[N];
+    int nextIndex[2 * N];
+    int vertexValue[2 * N];
+    int inDegree[N];
+    int result[N];
+    int q[2 * N];
+
+    void addEdge(const int a, const int b, int &idx) {
         vertexValue[idx] = b;
+        ++inDegree[b];
         nextIndex[idx] = headIndex[a];
         headIndex[a] = idx;
         ++idx;
     }
 
-    vector<int> bfsTopo(const vector<int> &headIndex, const vector<int> &vertexValue, const vector<int> &nextIndex,
-                        vector<int> &inDegree, vector<bool> &visited) {
-        vector<int> result;
-        const int n = (int) inDegree.size() - 1;
-        queue<int> q;
-        for (int v = 1; v <= n; ++v) {
-            if (!inDegree[v]) {
-                q.emplace(v);
-                visited[v] = true;
+    bool bfs(const int n) {
+        // 拓扑排序的BFS过程不需要visited数组
+        int resultTop = 0;
+        int hh = 0, tt = -1;
+        for (int i = 1; i <= n; ++i) {
+            if (inDegree[i]) {
+                continue;
             }
+            q[++tt] = i;
         }
-        while (!q.empty()) {
-            int root = q.front();
-            q.pop();
-            for (int idx = headIndex[root]; idx != -1; idx = nextIndex[idx]) {
-                int neighbor = vertexValue[idx];
-                --inDegree[neighbor];
-                if (!inDegree[neighbor]) {
-                    q.emplace(neighbor);
-                    visited[neighbor] = true;
+        while (hh <= tt) {
+            auto node = q[hh++];
+            result[resultTop++] = node;
+            for (int idx = headIndex[node]; idx != -1; idx = nextIndex[idx]) {
+                int nV = vertexValue[idx];
+                --inDegree[nV];
+                if (inDegree[nV]) {
+                    continue;
                 }
+                q[++tt] = nV;
             }
-            result.emplace_back(root);
         }
-        if (result.size() < n) {
-            return vector<int>();
-        }
-        return result;
+        return resultTop == n;
     }
 
     int main() {
+        memset(headIndex, -1, sizeof headIndex);
+        memset(nextIndex, -1, sizeof nextIndex);
         int n, m;
         scanf("%d%d", &n, &m);
-        vector<int> headIndex(n + 1, -1);
-        vector<int> inDegree(n + 1, 0);
-        vector<int> vertexValue;
-        vector<int> nextIndex;
         int idx = 0;
-        int a, b;
         for (int i = 0; i < m; ++i) {
+            int a, b;
             scanf("%d%d", &a, &b);
-            addEdge(a, b, headIndex, vertexValue, nextIndex, inDegree, idx);
+            addEdge(a, b, idx);
         }
-        vector<bool> visited(n + 1, false);
-        vector<int> result = bfsTopo(headIndex, vertexValue, nextIndex, inDegree, visited);
-        if (result.empty()) {
-            printf("-1\n");
-        } else {
-            for (int v : result) {
-                printf("%d ", v);
+        if (bfs(n)) {
+            for (int i = 0; i < n; ++i) {
+                printf("%d ", result[i]);
             }
             printf("\n");
+        } else {
+            printf("-1\n");
         }
         return 0;
     }
