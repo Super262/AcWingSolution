96cd8bf5749dfe3a1beecd113c6e0c74aafa5881
fengwei <wuzhongfengwei@163.com>
2021-08-17 13:27:15 -0700

    Reviewed Problem 845.

--- Problem0845.h
+++ Problem0845.h
@@ -5,7 +5,7 @@
 #ifndef ACWINGSOLUTION_PROBLEM0845_H
 #define ACWINGSOLUTION_PROBLEM0845_H
 
-#include <vector>
+#include <string>
 #include <queue>
 #include <unordered_map>
 #include <iostream>
@@ -13,50 +13,49 @@
 using namespace std;
 
 class Problem0845 {
-public:
-    int bfs(const string &start) {
-        const string end = "12345678x";
+private:
+    int bfs(const string &start, const string &end) {
         queue<string> q;
-        unordered_map<string, int> distance;
-        const int dx[] = {0, 0, 1, -1};
-        const int dy[] = {1, -1, 0, 0};
+        unordered_map<string, int> dist;
+        int dx[] = {0, 1, 0, -1};
+        int dy[] = {1, 0, -1, 0};
+        dist[start] = 0;
         q.emplace(start);
-        distance[start] = 0;
         while (!q.empty()) {
-            string node = q.front();
+            auto node = q.front();
             if (node == end) {
-                return distance[node];
+                return dist[node];
             }
             q.pop();
-            const int currentDistance = distance[node];
-            const int emptyIndex = (int) node.find('x');
-            const int currentX = emptyIndex / 3;
-            const int currentY = emptyIndex % 3;
+            int tagPos = (int) node.find('x');
+            int currentDis = dist[node];
             for (int i = 0; i < 4; ++i) {
-                const int nextX = currentX + dx[i];
-                const int nextY = currentY + dy[i];
+                int nextX = tagPos / 3 + dx[i];
+                int nextY = tagPos % 3 + dy[i];
                 if (nextX < 0 || nextX >= 3 || nextY < 0 || nextY >= 3) {
                     continue;
                 }
-                swap(node[nextX * 3 + nextY], node[emptyIndex]);
-                if (distance.find(node) == distance.end()) {
-                    distance[node] = currentDistance + 1;
+                swap(node[nextX * 3 + nextY], node[tagPos]);
+                if (dist.find(node) == dist.end()) {
                     q.emplace(node);
+                    dist[node] = currentDis + 1;
                 }
-                swap(node[nextX * 3 + nextY], node[emptyIndex]);
+                swap(node[nextX * 3 + nextY], node[tagPos]);
             }
         }
         return -1;
     }
 
     int main() {
-        string s(9, '\0');
+        string start(9, '\0');
+        string end = "12345678x";
         for (int i = 0; i < 9; ++i) {
-            cin >> s[i];
+            cin >> start[i];
         }
-        printf("%d", bfs(s));
+        printf("%d\n", bfs(start, end));
         return 0;
     }
+
 };
 
 #endif //ACWINGSOLUTION_PROBLEM0845_H
