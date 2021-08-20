485c7691ef007f53fe845793f318df415da2dbfd
fengwei <wuzhongfengwei@163.com>
2021-08-20 02:05:29 -0700

    Reviewed Problem 854.

--- Problem0854.h
+++ Problem0854.h
@@ -6,55 +6,52 @@
 #define ACWINGSOLUTION_PROBLEM0854_H
 
 #include <iostream>
-#include <vector>
+#include <cstring>
 
 using namespace std;
 
 class Problem0854 {
-public:
-    void floyd(int **distance, const int n) {
+private:
+    const int N = 210;
+    const int INF = 0x7f7f7f7f;
+    int dist[N][N];
+
+    void floyd(const int n) {
         // 注意循环顺序：最外层循环的变量一定是中间节点
         // i, j 可以交换层级，k 只在最外层
         for (int k = 1; k <= n; ++k) {
             for (int i = 1; i <= n; ++i) {
                 for (int j = 1; j <= n; ++j) {
-                    if (distance[i][k] == 2147483647 || distance[k][j] == 2147483647) {
+                    if (dist[i][k] == INF || dist[k][j] == INF) {
                         continue;
                     }
-                    distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j]);
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
     }
 
     int main() {
+        memset(dist, 0x7f, sizeof dist);
         int n, m, k;
         scanf("%d%d%d", &n, &m, &k);
-        auto distance = new int *[n + 1];
         for (int i = 1; i <= n; ++i) {
-            distance[i] = new int[n + 1];
-            for (int j = 1; j <= n; ++j) {
-                distance[i][j] = i == j ? 0 : 2147483647;
-            }
+            dist[i][i] = 0;
         }
-        int x, y, z;
-        while (m--) {
-            scanf("%d%d%d", &x, &y, &z);
-            distance[x][y] = min(distance[x][y], z);
+        int a, b, w;
+        for (int i = 0; i < m; ++i) {
+            scanf("%d%d%d", &a, &b, &w);
+            dist[a][b] = min(dist[a][b], w);
         }
-        floyd(distance, n);
-        while (k--) {
-            scanf("%d%d", &x, &y);
-            if (distance[x][y] == 2147483647) {
-                puts("impossible");
+        floyd(n);
+        for (int i = 0; i < k; ++i) {
+            scanf("%d%d", &a, &b);
+            if (dist[a][b] == INF) {
+                printf("impossible\n");
             } else {
-                printf("%d\n", distance[x][y]);
+                printf("%d\n", dist[a][b]);
             }
         }
-        for (int i = 0; i <= n; ++i) {
-            delete[] distance[i];
-        }
-        delete[] distance;
         return 0;
     }
 };
