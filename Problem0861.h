b1082f6dfbcb19499f5a6ca739adf1cb80bd3421
fengwei <wuzhongfengwei@163.com>
2021-08-21 08:17:51 -0700

    Reviewed Problem 861.

--- Problem0861.h
+++ Problem0861.h
@@ -11,61 +11,57 @@
 using namespace std;
 
 class Problem0861 {
-public:
-    void addEdge(const int a, const int b, int* headIndex, int *vertexValue, int *nextIndex, int &idx) {
+private:
+    const int N = 510;
+    const int M = 100010;
+    int headIndex[N];
+    int nextIndex[M];
+    int vertexValue[M];
+    int matchedVertex[N];
+    bool visited[N];
+
+    void addEdge(const int a, const int b, int &idx) {
         vertexValue[idx] = b;
         nextIndex[idx] = headIndex[a];
         headIndex[a] = idx;
         ++idx;
     }
 
-    bool hasMatched(const int start, bool *visited, int *matchedVertex, const int* headIndex, const int *vertexValue, const int *nextIndex) {
-        for (int idx = headIndex[start]; idx != -1; idx = nextIndex[idx]) {
-            int neighbor = vertexValue[idx];
-            if (visited[neighbor]) {
+    bool hasMatched(const int a) {
+        for (auto idx = headIndex[a]; idx != -1; idx = nextIndex[idx]) {
+            auto nextV = vertexValue[idx];
+            if (visited[nextV]) {
                 continue;
             }
-            visited[neighbor] = true;
-            if (matchedVertex[neighbor] && !hasMatched(matchedVertex[neighbor], visited, matchedVertex, headIndex, vertexValue, nextIndex)) {
+            visited[nextV] = true;
+            if (matchedVertex[nextV] && !hasMatched(matchedVertex[nextV])) {
                 continue;
             }
-            matchedVertex[neighbor] = start;
+            matchedVertex[nextV] = a;
             return true;
         }
         return false;
     }
 
     int main() {
+        memset(headIndex, -1, sizeof headIndex);
+        memset(nextIndex, -1, sizeof nextIndex);
         int n1, n2, m;
         scanf("%d%d%d", &n1, &n2, &m);
-        auto headIndex = new int[n1 + 1];
-        auto vertexValue = new int[m];
-        auto nextIndex = new int[m];
-        memset(headIndex, -1, sizeof(int) * (n1 + 1));
-        memset(vertexValue, 0, sizeof(int) * m);
-        memset(nextIndex, -1, sizeof(int) * m);
+        int a, b;
         int idx = 0;
-        int x, y;
         for (int i = 0; i < m; ++i) {
-            scanf("%d%d", &x, &y);
-            addEdge(x, y, headIndex, vertexValue, nextIndex, idx);
+            scanf("%d%d", &a, &b);
+            addEdge(a, b, idx);
         }
-        auto visited = new bool[n2 + 1];
-        auto matchedVertex = new int[n2 + 1];
-        memset(matchedVertex, 0, sizeof(int) * (n2 + 1));
         int result = 0;
-        for (int v = 1; v <= n1; ++v) {
-            memset(visited, 0, sizeof(bool) * (n2 + 1));
-            if (hasMatched(v, visited, matchedVertex, headIndex, vertexValue, nextIndex)) {
+        for (int v1 = 1; v1 <= n1; ++v1) {
+            memset(visited, 0, sizeof visited);
+            if (hasMatched(v1)) {
                 ++result;
             }
         }
         printf("%d\n", result);
-        delete[] headIndex;
-        delete[] vertexValue;
-        delete[] nextIndex;
-        delete[] visited;
-        delete[] matchedVertex;
         return 0;
     }
 };
