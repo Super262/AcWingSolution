346d1df542776cea23b01de22a93904ca9254b0b
fengwei <wuzhongfengwei@163.com>
2021-08-22 08:15:14 -0700

    Reviewed Problem 9.

--- Problem0009.h
+++ Problem0009.h
@@ -6,52 +6,53 @@
 #define ACWINGSOLUTION_PROBLEM0009_H
 
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
 class Problem0009 {
-public:
-    int knapsackMaxValue(int **itemSize, int **itemValue, const int *itemsQuantity, const int kindsNum, const int packVolume) {
-        auto dp = new int[packVolume + 1];
-        memset(dp, 0, sizeof(int) * (packVolume + 1));
+private:
+    struct Item {
+        int size;
+        int value;
+
+        Item() {
+            size = -1;
+            value = 0;
+        }
+    };
+
+    Item items[110][110];
+    int dp[110];
+
+    int knapsack(const int kindsNum, const int packVolume) {
+        // 注意这里的循环顺序：组、体积、物品
         for (int i = 0; i < kindsNum; ++i) {
-            for (int v = packVolume; v > 0; --v) {
-                for (int j = 0; j < itemsQuantity[i]; ++j) {
-                    if (v < itemSize[i][j]) {
+            for (int v = packVolume; v >= 0; --v) {
+                for (int j = 0; items[i][j].size != -1; ++j) {
+                    if (v < items[i][j].size) {
                         continue;
                     }
-                    dp[v] = max(dp[v], dp[v - itemSize[i][j]] + itemValue[i][j]);
+                    dp[v] = max(dp[v], dp[v - items[i][j].size] + items[i][j].value);
                 }
             }
         }
-        int result = dp[packVolume];
-        delete[] dp;
-        return result;
+        return dp[packVolume];
     }
 
     int main() {
-        int kindsNum, packVolume;
-        scanf("%d%d", &kindsNum, &packVolume);
-        auto itemSize = new int*[kindsNum];
-        auto itemValue = new int*[kindsNum];
-        auto itemsQuantity = new int[kindsNum];
-        for (int i = 0; i < kindsNum; ++i) {
-            scanf("%d", &itemsQuantity[i]);
-            itemSize[i] = new int[itemsQuantity[i]];
-            itemValue[i] = new int[itemsQuantity[i]];
-            for (int j = 0; j < itemsQuantity[i]; ++j) {
-                scanf("%d%d", &itemSize[i][j], &itemValue[i][j]);
+        int n, v;
+        scanf("%d%d", &n, &v);
+        for (int i = 0; i < n; ++i) {
+            int k;
+            scanf("%d", &k);
+            for (int j = 0; j < k; ++j) {
+                int a, b;
+                scanf("%d%d", &a, &b);
+                items[i][j].size = a;
+                items[i][j].value = b;
             }
         }
-        printf("%d\n", knapsackMaxValue(itemSize, itemValue, itemsQuantity, kindsNum, packVolume));
-        delete[] itemsQuantity;
-        for (int i = 0; i < kindsNum; ++i) {
-            delete[] itemSize[i];
-            delete[] itemValue[i];
-        }
-        delete[] itemSize;
-        delete[] itemValue;
+        printf("%d\n", knapsack(n, v));
         return 0;
     }
 };
