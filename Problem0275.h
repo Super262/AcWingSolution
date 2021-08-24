6fd4fe119b6249e7cb13ffbd8c614d70932e2f9c
fengwei <wuzhongfengwei@163.com>
2021-08-24 16:34:36 -0700

    Reviewed Problem 275.

--- Problem0275.h
+++ Problem0275.h
@@ -9,43 +9,40 @@
 
 using namespace std;
 
-const int N = 55, M = 2 * N;
-
-int n, m;
-int w[N][N];
-int f[M][N][N];
-
-
 class Problem0275 {
     // https://www.acwing.com/solution/content/3954/
-public:
-    int main() {
-        //input
-        cin >> n >> m;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                cin >> w[i][j];
-            }
-        }
-        //dp
-        for (int k = 2; k <= n + m; ++k) {
-            for (int i = 1; i < k && i <= n; ++i) {
-                for (int j = 1; j < k && j <= n; ++j) {
-                    int v = w[i][k - i];
-                    if (i != j) v += w[j][k - j];
+private:
+    const int N = 50;
+    int graph[N + 1][N + 1];
+    int dp[2 * N + 1][N + 1][N + 1];
 
-                    int &t = f[k][i][j];
-                    t = max(t, f[k - 1][i][j]);
-                    t = max(t, f[k - 1][i - 1][j]);
-                    t = max(t, f[k - 1][i][j - 1]);
-                    t = max(t, f[k - 1][i - 1][j - 1]);
-                    t += v;
+    int maxValue(const int m, const int n) {
+        for (int s = 2; s <= n + m; ++s) {
+            for (int x1 = 1; x1 <= s - 1 && x1 <= m; ++x1) {
+                for (int x2 = 1; x2 <= s - 1 && x2 <= m; ++x2) {
+                    int v = graph[x1][s - x1];
+                    if (x1 != x2) {
+                        v += graph[x2][s - x2];
+                    }
+                    dp[s][x1][x2] = max(dp[s][x1][x2], dp[s - 1][x1][x2] + v);
+                    dp[s][x1][x2] = max(dp[s][x1][x2], dp[s - 1][x1 - 1][x2] + v);
+                    dp[s][x1][x2] = max(dp[s][x1][x2], dp[s - 1][x1][x2 - 1] + v);
+                    dp[s][x1][x2] = max(dp[s][x1][x2], dp[s - 1][x1 - 1][x2 - 1] + v);
                 }
             }
         }
+        return dp[m + n][m][m];
+    }
 
-        //output
-        cout << f[n + m][n][n] << endl;
+    int main() {
+        int n, m;
+        scanf("%d%d", &n, &m);
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                scanf("%d", &graph[i][j]);
+            }
+        }
+        printf("%d\n", maxValue(n, m));
         return 0;
     }
 };
