e8eebd72769ca5b87718072ec30627db2632aeb1
fengwei <wuzhongfengwei@163.com>
2021-08-25 16:12:06 -0700

    Reviewed Problem 272.

--- Problem0272.h
+++ Problem0272.h
@@ -13,63 +13,40 @@ using namespace std;
 class Problem0272 {
     // dp[i][j]表示所有由序列a的前i个字母和序列b的前j个字母构成的，且以b[j]结尾的公共上升子序列
 private:
-    int maxCommonLIS(const int *a, const int *b, const int N) {
-        auto dp = new int *[N + 1];
-        for (int i = 0; i <= N; ++i) {
-            dp[i] = new int[N + 1];
-            memset(dp[i], 0, sizeof(int) * (N + 1));
-        }
-// 优化前的代码：
-//        for (int i = 1; i <= N; ++i) {
-//            for (int j = 1; j <= N; ++j) {
-//                dp[i][j] = dp[i - 1][j];
-//                if (a[i] == b[j]) {
-//                    dp[i][j] = max(dp[i][j], 1);
-//                    for (int k = 1; k < j; ++k) {
-//                        if (b[k] < b[j]) {
-//                            dp[i][j] = max(dp[i][j], dp[i][k] + 1);
-//                        }
-//                    }
-//                }
-//            }
-//        }
-        for (int i = 1; i <= N; ++i) {
+    const int N = 3010;
+    int dp[N][N];
+    int a[N], b[N];
+
+    int longestCommonIS(const int l1, const int l2) {
+        for (int i = 1; i <= l1; ++i) {
             int maxPreJ = 0;
-            for (int j = 1; j <= N; ++j) {
+            for (int j = 1; j <= l2; ++j) {
                 dp[i][j] = dp[i - 1][j];
                 if (a[i] == b[j]) {
-                    dp[i][j] = max(dp[i][j], maxPreJ + 1);
-                } else if (b[j] < a[i]) {
+                    dp[i][j] = max(maxPreJ + 1, dp[i][j]);
+                } else if (b[j] < a[i]){
                     maxPreJ = max(maxPreJ, dp[i][j]);
                 }
             }
         }
         int result = 0;
         // 注意：不能通过遍历a的索引来获得结果，只能遍历b的索引（参见转移方程）
-        for (int i = 0; i <= N; ++i) {
-            result = max(result, dp[N][i]);
+        for (int i = 1; i <= l2; ++i) {
+            result = max(result, dp[l1][i]);
         }
-        for (int i = 0; i <= N; ++i) {
-            delete[] dp[i];
-        }
-        delete[] dp;
         return result;
     }
 
     int main() {
         int n;
         scanf("%d", &n);
-        auto a = new int[n + 1];
-        auto b = new int[n + 1];
         for (int i = 1; i <= n; ++i) {
             scanf("%d", &a[i]);
         }
         for (int i = 1; i <= n; ++i) {
             scanf("%d", &b[i]);
         }
-        printf("%d\n", maxCommonLIS(a, b, n));
-        delete[] a;
-        delete[] b;
+        printf("%d\n", longestCommonIS(n, n));
         return 0;
     }
 };
