628f89115a5e7e9a82e3e08fd0a7f8d0f52f7836
fengwei <wuzhongfengwei@163.com>
2021-08-27 17:43:39 -0700

    Reviewed Problem 6.

--- Problem0006.h
+++ Problem0006.h
@@ -12,42 +12,50 @@ using namespace std;
 
 class Problem0006 {
     // https://www.acwing.com/solution/content/6500/
-public:
-    int main() {
-        auto dq = new int[20010];
-        int itemsNum, packVolume;
-        scanf("%d%d", &itemsNum, &packVolume);
-        auto dp = new int[packVolume + 1];
-        auto pre = new int[packVolume + 1];
-        memset(dp, 0, sizeof(int) * (packVolume + 1));
-        memset(pre, 0, sizeof(int) * (packVolume + 1));
-        for (int i = 0; i < itemsNum; ++i) {
-            int v, w, s;
-            scanf("%d%d%d", &v, &w, &s);
+private:
+    struct Item {
+        int v;
+        int w;
+        int s;
+    };
+
+    Item items[1010];
+    const int N = 20010;
+    int dq[N];
+    int pre[N];
+    int dp[N];
+
+    int knapsack(const int kindsNum, const int packVolume) {
+        for (int i = 0; i < kindsNum; ++i) {
             memcpy(pre, dp, sizeof(int) * (packVolume + 1));
-            for (int j = 0; j < v; ++j) {
-                int dqHead = 0;
-                int dqTail = -1;
-                for (int k = j; k <= packVolume; k += v) {
-                    while (dqHead <= dqTail && dq[dqHead] < k - s * v) {
-                        // 队头划出大小为s的窗口
-                        ++dqHead;
+            auto v = items[i].v;
+            auto w = items[i].w;
+            auto s = items[i].s;
+            for (int r = 0; r < v; ++r) {
+                int hh = 0;
+                int tt = -1;
+                for (int j = r; j <= packVolume; j += v) {
+                    while (hh <= tt && dq[hh] < j - s * v) {
+                        ++hh;
                     }
-                    while (dqHead <= dqTail && pre[dq[dqTail]] - (dq[dqTail] - j) / v * w <= pre[k] - (k - j) / v * w) {
-                        // 新元素大于队尾，不满足单调下降
-                        --dqTail;
+                    while (hh <= tt && pre[dq[tt]] - dq[tt] / v * w <= pre[j] - j / v * w) {
+                        --tt;
                     }
-                    if (dqHead <= dqTail) {
-                        dp[k] = pre[dq[dqHead]] + (k - dq[dqHead]) / v * w;
-                    }
-                    dq[++dqTail] = k;
+                    dq[++tt] = j;
+                    dp[j] = pre[dq[hh]] + (j - dq[hh]) / v * w;
                 }
             }
         }
-        printf("%d\n", dp[packVolume]);
-        delete[] dp;
-        delete[] pre;
-        delete[] dq;
+        return dp[packVolume];
+    }
+
+    int main() {
+        int kindsNum, packVolume;
+        scanf("%d%d", &kindsNum, &packVolume);
+        for (int i = 0; i < kindsNum; ++i) {
+            scanf("%d%d%d", &items[i].v, &items[i].w, &items[i].s);
+        }
+        printf("%d\n", knapsack(kindsNum, packVolume));
         return 0;
     }
 };
