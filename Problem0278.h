f75cd3fc6b2e7ab470b1c4d3ab64b526fa79aa5a
fengwei <wuzhongfengwei@163.com>
2021-08-28 16:34:23 -0700

    Reviewed Problem 278.

--- Problem0278.h
+++ Problem0278.h
@@ -10,21 +10,27 @@
 using namespace std;
 
 class Problem0278 {
-public:
-    int main() {
-        int N, M;
-        scanf("%d%d", &N, &M);
-        auto dp = new int[M + 1];
+private:
+    int dp[10010];
+    int items[100];
+
+    int knapsack(const int n, const int m) {
         dp[0] = 1;
-        for (int i = 0; i < N; ++i) {
-            int num;
-            scanf("%d", &num);
-            for (int j = M; j >= num; --j) {
-                dp[j] += dp[j - num];
+        for (int i = 0; i < n; ++i) {
+            for (int j = m; j >= items[i]; --j) {
+                dp[j] += dp[j - items[i]];
             }
         }
-        printf("%d\n", dp[M]);
-        delete[] dp;
+        return dp[m];
+    }
+
+    int main() {
+        int n, m;
+        scanf("%d%d", &n, &m);
+        for (int i = 0; i < n; ++i) {
+            scanf("%d", &items[i]);
+        }
+        printf("%d\n", knapsack(n, m));
         return 0;
     }
 };
