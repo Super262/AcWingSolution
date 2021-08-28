5d8dcc88613e02cc6383cc089213fcc8f79a641d
fengwei <wuzhongfengwei@163.com>
2021-08-28 15:53:25 -0700

    Reviewed Problem 1020.

--- Problem1020.h
+++ Problem1020.h
@@ -13,51 +13,46 @@ using namespace std;
 // https://www.acwing.com/solution/content/6485/
 
 class Problem1020 {
-public:
-    int main() {
-        const int M = 21;
-        const int N = 79;
-        auto dp = new int *[M + 1];
-        for (int i = 0; i <= M; ++i) {
-            dp[i] = new int[N + 1];
-            memset(dp[i], -1, sizeof(int) * (N + 1));
-        }
+    // “01背包”的改进：物品体积可以超过背包容量
+    // 非法状态的值是0x3f3f3f3f
+private:
+    struct Item {
+        int ox, ni, w;
+    };
+
+    const int M = 22, N = 80, K = 1000;
+    int dp[M][N];  // dp[i][j]代表氧的容量不少于i、氮的容量不少于j时的最小重量
+    Item items[K];
+
+    int knapsack(const int m, const int n, const int k) {
+        const int INF = 0x3f3f3f3f;
+        memset(dp, 0x3f, sizeof dp);
         dp[0][0] = 0;
+        for (int d = 0; d < k; ++d) {
+            for (int i = M - 1; i >= 0; --i) {
+                for (int j = N - 1; j >= 0; --j) {
+                    auto preI = max(i - items[d].ox, 0);
+                    auto preJ = max(j - items[d].ni, 0);
+                    dp[i][j] = min(dp[i][j], dp[preI][preJ] + items[d].w);
+                }
+            }
+        }
+        int result = INF;
+        for (int i = M - 1; i >= m; --i) {
+            for (int j = N - 1; j >= n; --j) {
+                result = min(result, dp[i][j]);
+            }
+        }
+        return result;
+    }
+
+    int main() {
         int m, n, k;
         scanf("%d%d%d", &m, &n, &k);
-        for (int d = 0; d < k; ++d) {
-            int a, b, c;
-            scanf("%d%d%d", &a, &b, &c);
-            for (int i = M; i >= 0; --i) {
-                for (int j = N; j >= 0; --j) {
-                    // “01背包”的改进：物品体积可以超过背包容量
-                    int preI = max(i - a, 0);
-                    int preJ = max(j - b, 0);
-                    if (dp[preI][preJ] == -1) {
-                        continue;
-                    }
-                    if (dp[i][j] == -1) {
-                        dp[i][j] = dp[preI][preJ] + c;
-                    } else {
-                        dp[i][j] = min(dp[i][j], dp[preI][preJ] + c);
-                    }
-                }
-            }
+        for (int i = 0; i < k; ++i) {
+            scanf("%d%d%d", &items[i].ox, &items[i].ni, &items[i].w);
         }
-        int result = 0x7f7f7f7f;
-        for (int i = M; i >= m; --i) {
-            for (int j = N; j >= n; --j) {
-                if (dp[i][j] == -1) {
-                    continue;
-                }
-                result = min(dp[i][j], result);
-            }
-        }
-        printf("%d\n", result);
-        for (int i = 0; i <= M; ++i) {
-            delete[] dp[i];
-        }
-        delete[] dp;
+        printf("%d\n", knapsack(m, n, k));
         return 0;
     }
 };
