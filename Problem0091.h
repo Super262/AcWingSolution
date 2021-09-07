bb4bed66512f9ac0a26a7b3d2b6a7b4d4e1ddec8
fengwei <wuzhongfengwei@163.com>
2021-09-06 22:25:54 -0700

    Reviewed Problem 91.

--- Problem0091.h
+++ Problem0091.h
@@ -29,7 +29,7 @@ private:
                     continue;
                 }
                 for (int s0 = 0; s0 < n; ++s0) {
-                    if (!(((p - (1 << s1)) >> s0) & 1)) {
+                    if (s0 == s1) {
                         continue;
                     }
                     if (dp[p - (1 << s1)][s0] == 0x7f7f7f7f || graph[s0][s1] == 0x7f7f7f7f) {
