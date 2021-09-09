8dabf078166c33544ff099bc3dda8f8533db14b6
fengwei <wuzhongfengwei@163.com>
2021-09-09 01:10:50 -0700

    Reviewed Problem 1072.

--- Problem1072.h
+++ Problem1072.h
@@ -13,30 +13,30 @@ using namespace std;
 class Problem1072 {
     // 任取一点为起点，找到距离该点最远的点u
     // 找到距离u最远的点v；u和v间的距离即为直径
-public:
-    int headIndex[10010];
-    int nextIndex[20010];
-    int vertexValue[20010];
-    int weight[20010];
+private:
+    const int N = 10005;
+    int headIndex[N];
+    int nextIndex[2 * N];
+    int vertexValue[2 * N];
+    int weight[2 * N];
 
-    void addEdge(const int a, const int b, const int w, int &idx) {
+    void addEdge(int a, int b, int w, int &idx) {
         vertexValue[idx] = b;
+        weight[idx] = w;
         nextIndex[idx] = headIndex[a];
         headIndex[a] = idx;
-        weight[idx] = w;
         ++idx;
     }
 
-    int dfs(const int root, const int father, int &answer) {
-        int d1 = 0; // 最长的向下的路的长度
-        int d2 = 0; // 次长的向下的路的长度
+    int treeModel(int root, int father, int &answer) {
+        int d1 = 0, d2 = 0;
         for (int idx = headIndex[root]; idx != -1; idx = nextIndex[idx]) {
-            int child = vertexValue[idx];
-            if (child == father) {
+            auto childV = vertexValue[idx];
+            if (childV == father) {
                 continue;
             }
-            int childD1 = dfs(child, root, answer) + weight[idx];
-            if (childD1 >= d1) { // 注意：这里是“大于或等于”，不是“大于”！
+            int childD1 = treeModel(childV, root, answer) + weight[idx];
+            if (childD1 >= d1) {
                 d2 = d1;
                 d1 = childD1;
             } else if (childD1 > d2) {
@@ -50,20 +50,21 @@ public:
     int main() {
         memset(headIndex, -1, sizeof headIndex);
         memset(nextIndex, -1, sizeof nextIndex);
-        int idx = 0;
         int n;
         scanf("%d", &n);
-        for (int i = 0; i < n - 1; ++i) {
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
             int a, b, w;
             scanf("%d%d%d", &a, &b, &w);
             addEdge(a, b, w, idx);
             addEdge(b, a, w, idx);
         }
         int answer = 0;
-        dfs(1, -1, answer);  // 任选1个点即可
+        treeModel(1, -1, answer);
         printf("%d\n", answer);
         return 0;
     }
+
 };
 
 #endif //ACWINGSOLUTION_PROBLEM1072_H
