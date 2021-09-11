9c45180d55ea51189ae8a8513edfb368b0978879
fengwei <wuzhongfengwei@163.com>
2021-09-11 10:28:04 -0700

    Reviewed Problem 1088.

--- Problem1088.h
+++ Problem1088.h
@@ -12,73 +12,67 @@ using namespace std;
 
 class Problem1088 {
     // https://www.acwing.com/solution/content/34005/
-public:
-    void visitorHelper1(const long long *prefixSum, bool *visited, const int N) {
-        auto q = new int[2 * N];
-        memset(q, 0, sizeof(int) * 2 * N);
+private:
+    const int N = 3000010;
+    int oil[N], cost[N];
+    bool visited[N];
+    long long dq[N];
+    long long prefix[2 * N];
+
+    void helper1(const int n) {
         int hh = 0, tt = -1;
-        for (int i = 2 * N; i >= 1; --i) {
-            while (hh <= tt && q[hh] > i + N - 1) {
+        for (int i = 2 * n; i >= 1; --i) {
+            while (hh <= tt && dq[hh] - i + 1 > n + 1) {
                 ++hh;
             }
-            while (hh <= tt && prefixSum[q[tt]] >= prefixSum[i]) {
+            while (hh <= tt && prefix[i] <= prefix[dq[tt]]) {
                 --tt;
             }
-            q[++tt] = i;
-            if (i <= N && prefixSum[q[hh]] >= prefixSum[i - 1]) {
+            dq[++tt] = i;
+            if (i <= n && prefix[dq[hh]] - prefix[i - 1] >= 0) {
                 visited[i] = true;
             }
         }
-        delete[] q;
     }
 
-    void visitorHelper2(const long long *prefixSum, bool *visited, const int N) {
-        auto q = new int[2 * N];
-        memset(q, 0, sizeof(int) * 2 * N);
+    void helper2(const int n) {
         int hh = 0, tt = -1;
-        for (int i = 1; i <= 2 * N; ++i) {
-            while (hh <= tt && q[hh] < i - N + 1) {
+        for (int i = 1; i <= 2 * n; ++i) {
+            while (hh <= tt && i - dq[hh] + 1 > n + 1) {
                 ++hh;
             }
-            while (hh <= tt && prefixSum[q[tt]] >= prefixSum[i]) {
+            while (hh <= tt && prefix[i] <= prefix[dq[tt]]) {
                 --tt;
             }
-            q[++tt] = i;
-            if (i > N && prefixSum[q[hh]] >= prefixSum[i + 1]) {
-                visited[i - N] = true;
+            dq[++tt] = i;
+            if (i > n && prefix[dq[hh]] - prefix[i + 1] >= 0) {
+                visited[i - n] = true;
             }
         }
-        delete[] q;
     }
 
     int main() {
         int n;
         scanf("%d", &n);
-        auto visited = new bool[n + 1];
-        memset(visited, 0, sizeof(bool) * (n + 1));
-        auto oil = new int[n + 1];
-        auto cost = new int[n + 1];
         for (int i = 1; i <= n; ++i) {
             scanf("%d%d", &oil[i], &cost[i]);
         }
-        auto prefixSum = new long long[2 * n + 1];
-        memset(prefixSum, 0, sizeof(long long) * (2 * n + 1));
         for (int i = 1; i <= n; ++i) {
-            prefixSum[i] = prefixSum[i + n] = oil[i] - cost[i];
+            prefix[i] = prefix[i + n] = oil[i] - cost[i];
         }
         for (int i = 1; i <= 2 * n; ++i) {
-            prefixSum[i] += prefixSum[i - 1];
+            prefix[i] += prefix[i - 1];
         }
-        visitorHelper1(prefixSum, visited, n);
-        memset(prefixSum, 0, sizeof(long long) * (2 * n + 1));
+        helper1(n);
+        memset(prefix, 0, sizeof prefix);
         cost[0] = cost[n];
         for (int i = 1; i <= n; ++i) {
-            prefixSum[i] = prefixSum[i + n] = oil[i] - cost[i - 1];
+            prefix[i] = prefix[i + n] = oil[i] - cost[i - 1];
         }
         for (int i = 2 * n; i >= 1; --i) {
-            prefixSum[i] += prefixSum[i + 1];
+            prefix[i] += prefix[i + 1];
         }
-        visitorHelper2(prefixSum, visited, n);
+        helper2(n);
         for (int i = 1; i <= n; ++i) {
             if (visited[i]) {
                 printf("TAK\n");
@@ -86,10 +80,6 @@ public:
                 printf("NIE\n");
             }
         }
-        delete[] oil;
-        delete[] cost;
-        delete[] visited;
-        delete[] prefixSum;
         return 0;
     }
 };
