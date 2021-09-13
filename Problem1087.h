b088d2a9ff545ff957472186fb3f36855d2149b5
fengwei <wuzhongfengwei@163.com>
2021-09-13 11:15:56 -0700

    Reviewed Problem 1087.

--- Problem1087.h
+++ Problem1087.h
@@ -11,47 +11,43 @@ using namespace std;
 
 class Problem1087 {
 // https://www.acwing.com/solution/content/27326/
-public:
-    long long gFunc(const long long i, const long long *s, const long long *dp) {
-        if (i == 0) {
+private:
+    const int N = 100005;
+    long long s[N];
+    long long q[N];
+    long long dp[N];
+
+    long long gFunc(int i) {
+        if (i < 1) {
             return 0;
         }
         return dp[i - 1] - s[i];
     }
 
-    long long maxSeq(const long long *s, const long long N, const long long K) {
-        auto dp = new long long[N + 1];
-        dp[0] = 0;
-        auto q = new long long[N + 1];
-        long long hh = 0, tt = -1;
+    long long maxSeq(int n, int k) {
+        int hh = 0, tt = -1;
         q[++tt] = 0;
-        for (long long i = 1; i <= N; ++i) {
-            while (hh <= tt && q[hh] < i - K) {
+        for (int i = 1; i <= n; ++i) {
+            while (hh <= tt && i - q[hh] + 1 > k + 1) {
                 ++hh;
             }
-            dp[i] = max(dp[i - 1], gFunc(q[hh], s, dp) + s[i]);
-            while (hh <= tt && gFunc(q[tt], s, dp) <= gFunc(i, s, dp)) {
+            dp[i] = max(dp[i - 1], gFunc(q[hh]) + s[i]);
+            while (hh <= tt && gFunc(q[tt]) <= gFunc(i)) {
                 --tt;
             }
             q[++tt] = i;
         }
-        long long result = dp[N];
-        delete[] dp;
-        delete[] q;
-        return result;
+        return dp[n];
     }
 
     int main() {
-        long long n, k;
-        scanf("%lld%lld", &n, &k);
-        auto s = new long long[n + 1];
-        s[0] = 0;
-        for (long long i = 1; i <= n; ++i) {
-            scanf("%lld", &s[i]);
+        int n, k;
+        scanf("%d%d", &n, &k);
+        for (int i = 1; i <= n; ++i) {
+            scanf("%d", &s[i]);
             s[i] += s[i - 1];
         }
-        printf("%lld\n", maxSeq(s, n, k));
-        delete[] s;
+        printf("%lld\n", maxSeq(n, k));
         return 0;
     }
 };
