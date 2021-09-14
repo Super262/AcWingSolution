65c0cb978a719b1fd4e083571a873e9a7202f041
fengwei <wuzhongfengwei@163.com>
2021-09-13 23:42:23 -0700

    Reviewed Problem 844.

--- Problem0844.h
+++ Problem0844.h
@@ -5,63 +5,58 @@
 #ifndef ACWINGSOLUTION_PROBLEM0844_H
 #define ACWINGSOLUTION_PROBLEM0844_H
 
-#include <vector>
-#include <queue>
 #include <algorithm>
 #include <iostream>
 
 using namespace std;
 
 class Problem0844 {
-public:
-    int bfs(vector<vector<int>> &matrix, const vector<int> &dx, const vector<int> &dy) {
-        const int n = matrix.size() - 1;
-        const int m = matrix[matrix.size() - 1].size() - 1;
-        if (matrix[0][0] == 1 || matrix[n][m] == 1) {
-            return 2147483647;
-        }
+private:
+    const int N = 100;
+    int matrix[N][N];
+    bool visited[N][N];
+    pair<int, int> q[N * N + 10];
+
+    int bfs(const int n, const int m) {
+        int dx[] = {0, 1, 0, -1}, dy[] = {-1, 0, 1, 0};
+        int hh = 0, tt = -1;
+        q[++tt] = pair<int, int>(n - 1, m - 1);
+        visited[n - 1][m - 1] = true;
         int result = 0;
-        queue<pair<int, int>> q;
-        q.emplace(pair<int, int>(0, 0));
-        matrix[0][0] = 1;
-        while (!q.empty()) {
-            int levelSize = q.size();
-            while (levelSize--) {
-                pair<int, int> node = q.front();
-                q.pop();
-                if (node.first == n && node.second == m) {
+        while (hh <= tt) {
+            int curL = tt - hh + 1;
+            while (curL--) {
+                auto root = q[hh++];
+                if (root.first == 0 && root.second == 0) {
                     return result;
                 }
-                for (int i = 0; i < dx.size() && i < dy.size(); ++i) {
-                    int nextX = node.first + dx[i];
-                    int nextY = node.second + dy[i];
-                    if (nextX < 0 || nextX > n || nextY < 0 || nextY > m || matrix[nextX][nextY] == 1) {
+                for (int i = 0; i < 4; ++i) {
+                    auto nextX = root.first + dx[i];
+                    auto nextY = root.second + dy[i];
+                    if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m || matrix[nextX][nextY] == 1 ||
+                        visited[nextX][nextY]) {
                         continue;
                     }
-                    q.emplace(pair<int, int>(nextX, nextY));
-                    matrix[nextX][nextY] = 1;
+                    q[++tt] = pair<int, int>(nextX, nextY);
+                    visited[nextX][nextY] = true;
                 }
             }
             ++result;
         }
-        return 2147483647;
+        return 0x7f7f7f7f;
     }
 
     int main() {
         int n, m;
         scanf("%d%d", &n, &m);
-        vector<vector<int>> matrix(n, vector<int>(m, 0));
-        for (vector<int> &row:matrix) {
-            for (int &num:row) {
-                scanf("%d", &num);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                scanf("%d", &matrix[i][j]);
             }
         }
-        vector<int> dx = {0, 0, 1, -1};
-        vector<int> dy = {1, -1, 0, 0};
-        printf("%d\n", bfs(matrix, dx, dy));
+        printf("%d\n", bfs(n, m));
         return 0;
     }
-
 };
 
 #endif //ACWINGSOLUTION_PROBLEM0844_H
