223709bf2e8947adbc714ed06fc158afffcdb610
fengwei <wuzhongfengwei@163.com>
2021-09-19 18:20:07 -0700

    Finished Problem 1252.

---
+++ Problem1252.h
@@ -0,0 +1,64 @@
+//
+// Created by Fengwei Zhang on 9/19/21.
+//
+
+#ifndef ACWINGSOLUTION_PROBLEM1252_H
+#define ACWINGSOLUTION_PROBLEM1252_H
+
+class Problem1252 {
+private:
+    const int N = 10010;
+    int dp[N];
+    int value[N], price[N];
+    int parent[N];
+
+    int findRoot(const int x) {
+        if (x != parent[x]) {
+            parent[x] = findRoot(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void mergeSets(const int a, const int b) {
+        auto pa = findRoot(a);
+        auto pb = findRoot(b);
+        if (pa == pb) {
+            return;
+        }
+        parent[pa] = pb;
+        value[pb] += value[pa];
+        price[pb] += price[pa];
+    }
+
+    int knapsack(const int n, const int m) {
+        for (int i = 1; i <= n; ++i) {
+            if (parent[i] != i) { // 不是根结点，跳过
+                continue;
+            }
+            for (int j = m; j >= price[i]; --j) {
+                dp[j] = max(dp[j], dp[j - price[i]] + value[i]);
+            }
+        }
+        return dp[m];
+    }
+
+    int main() {
+        int n, m, w;
+        scanf("%d%d%d", &n, &m, &w);
+        for (int i = 1; i <= n; ++i) {
+            parent[i] = i;
+        }
+        for (int i = 1; i <= n; ++i) {
+            scanf("%d%d", &price[i], &value[i]);
+        }
+        int a, b;
+        for (int i = 0; i < m; ++i) {
+            scanf("%d%d", &a, &b);
+            mergeSets(a, b);
+        }
+        printf("%d\n", knapsack(n, w));
+        return 0;
+    }
+};
+
+#endif //ACWINGSOLUTION_PROBLEM1252_H
