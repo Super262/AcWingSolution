996f438b8720cba0c879644c58c11488e2ce1bfe
fengwei <wuzhongfengwei@163.com>
2021-10-19 15:27:06 -0700

    Reviewed Problem 851.

--- Problem0851.h
+++ Problem0851.h
@@ -14,11 +14,11 @@ class Problem0851 {
 private:
     const int N = 100010;
     int headIndex[N];
-    int nextIndex[2 * N];
-    int weight[2 * N];
-    int vertexValue[2 * N];
+    int nextIndex[N];
+    int vertexValue[N];
+    int weight[N];
 
-    void addEdge(const int a, const int b, const int w, int &idx) {
+    void addEdge(int a, int b, int w, int &idx) {
         vertexValue[idx] = b;
         weight[idx] = w;
         nextIndex[idx] = headIndex[a];
@@ -26,25 +26,24 @@ private:
         ++idx;
     }
 
-    int spfa(const int root, const int target, const int n) {
-        auto q = new int[2 * N];  // 队列的空间应该和边的数量一致
-        auto dist = new int[n + 1];
-        auto isInQueue = new bool[n + 1];
-        memset(dist, 0x7f, sizeof(int) * (n + 1));
-        memset(isInQueue, 0, sizeof(bool) * (n + 1));
+    int spfa(int start, int end, int n) {
+        int q[N];
         int hh = 0, tt = -1;
-        q[++tt] = root;
-        isInQueue[root] = true;
-        dist[root] = 0;
+        bool isInQueue[n + 1];
+        int dist[n + 1];
+        memset(dist, 0x3f, sizeof dist);
+        memset(isInQueue, 0, sizeof isInQueue);
+        q[++tt] = start;
+        dist[start] = 0;
         while (hh <= tt) {
-            auto node = q[hh++];
-            isInQueue[node] = false;
-            for (int idx = headIndex[node]; idx != -1; idx = nextIndex[idx]) {
+            auto root = q[hh++];
+            isInQueue[root] = false;
+            for (int idx = headIndex[root]; idx != -1; idx = nextIndex[idx]) {
                 auto nextV = vertexValue[idx];
-                if (dist[nextV] <= dist[node] + weight[idx]) {
+                if (dist[nextV] < dist[root] + weight[idx]) {
                     continue;
                 }
-                dist[nextV] = dist[node] + weight[idx];
+                dist[nextV] = dist[root] + weight[idx];
                 if (isInQueue[nextV]) {
                     continue;
                 }
@@ -52,14 +51,10 @@ private:
                 isInQueue[nextV] = true;
             }
         }
-        delete[] isInQueue;
-        delete[] q;
-        auto result = dist[target];
-        delete[] dist;
-        if (result == 0x7f7f7f7f) {
-            result = -1;
+        if (dist[end] == 0x3f3f3f3f) {
+            return -1;
         }
-        return result;
+        return dist[end];
     }
 
     int main() {
@@ -67,13 +62,13 @@ private:
         memset(nextIndex, -1, sizeof nextIndex);
         int n, m;
         scanf("%d%d", &n, &m);
+        int x, y, w;
         int idx = 0;
-        int a, b, w;
         for (int i = 0; i < m; ++i) {
-            scanf("%d%d%d", &a, &b, &w);
-            addEdge(a, b, w, idx);
+            scanf("%d%d%d", &x, &y, &w);
+            addEdge(x, y, w, idx);
         }
-        int result = spfa(1, n, n);
+        auto result = spfa(1, n, n);
         if (result == -1) {
             printf("impossible\n");
         } else {
