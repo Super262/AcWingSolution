980b929954dfe4a937b8ee222a71626b41289ef7
fengwei <wuzhongfengwei@163.com>
2021-10-19 08:10:00 -0700

    Reviewed Problem 853.

--- Problem0853.h
+++ Problem0853.h
@@ -20,23 +20,19 @@ private:
 
     Edge edges[10010];
 
-    int bellmanFord(const int start, const int n, const int edgesSize, int pathLen) {
-        auto dist = new int[n + 1];
-        auto temp = new int[n + 1];
-        memset(dist, 0x7f, sizeof(int) * (n + 1));
+    int bellmanFord(const int start, const int end, const int n, const int edgesSize, int pathLen) {
+        int dist[n + 1];
+        int temp[n + 1];
+        memset(dist, 0x3f, sizeof dist);
         dist[start] = 0;
         while (pathLen--) {
-            // 不要忘记这个备份！
-            memcpy(temp, dist, sizeof(int) * (n + 1));
+            memcpy(temp, dist, sizeof dist);
             for (int i = 0; i < edgesSize; ++i) {
                 dist[edges[i].end] = min(dist[edges[i].end], temp[edges[i].start] + edges[i].weight);
             }
         }
-        int result = dist[n];
-        delete[] temp;
-        delete[] dist;
         // "正无穷"可能被负权边更新而减小，所以这里使用"0x3f3f3f3f/2"
-        return result >= 0x3f3f3f3f / 2 ? 0x3f3f3f3f : result;
+        return dist[n] >= 0x3f3f3f3f / 2 ? 0x3f3f3f3f : dist[n];
     }
 
     int main() {
@@ -45,8 +41,8 @@ private:
         for (int i = 0; i < m; ++i) {
             scanf("%d%d%d", &edges[i].start, &edges[i].end, &edges[i].weight);
         }
-        int result = bellmanFord(1, n, m, k);
-        if (result == 0x7f7f7f7f) {
+        int result = bellmanFord(1, n, n, m, k);
+        if (result == 0x3f3f3f3f) {
             printf("impossible\n");
         } else {
             printf("%d\n", result);
