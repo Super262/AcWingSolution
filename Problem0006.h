bd19978f7d47b74720823133904faf93095a31ad
fengwei <wuzhongfengwei@163.com>
2021-11-21 11:30:18 -0800

    Reviewed Problem 6.

--- Problem0006.h
+++ Problem0006.h
@@ -14,48 +14,43 @@ class Problem0006 {
     // https://www.acwing.com/solution/content/6500/
 private:
     struct Item {
-        int v;
-        int w;
-        int s;
+        int v, w, s;
     };
 
     Item items[1010];
     const int N = 20010;
+    int dp[2][N];  // 滚动数组
     int dq[N];
-    int pre[N];
-    int dp[N];
 
-    int knapsack(const int kindsNum, const int packVolume) {
-        for (int i = 0; i < kindsNum; ++i) {
-            memcpy(pre, dp, sizeof(int) * (packVolume + 1));
+    int knapsack(const int m, const int n) {
+        for (int i = 0; i < n; ++i) {
             auto v = items[i].v;
             auto w = items[i].w;
             auto s = items[i].s;
             for (int r = 0; r < v; ++r) {
-                int hh = 0;
-                int tt = -1;
-                for (int j = r; j <= packVolume; j += v) {
+                int hh = 0, tt = -1;
+                for (int j = r; j <= m; j += v) {
                     while (hh <= tt && dq[hh] < j - s * v) {
                         ++hh;
                     }
-                    while (hh <= tt && pre[dq[tt]] - dq[tt] / v * w <= pre[j] - j / v * w) {
+                    while (hh <= tt && dp[i % 2][dq[tt]] - dq[tt] / v * w <= dp[i % 2][j] - j / v * w) {
                         --tt;
                     }
                     dq[++tt] = j;
-                    dp[j] = max(dp[j], pre[dq[hh]] + (j - dq[hh]) / v * w);
+                    dp[(i + 1) % 2][j] = max(dp[(i + 1) % 2][j], dp[i % 2][dq[hh]] + (j - dq[hh]) / v * w);
                 }
             }
         }
-        return dp[packVolume];
+        return dp[n % 2][m];
     }
 
     int main() {
-        int kindsNum, packVolume;
-        scanf("%d%d", &kindsNum, &packVolume);
-        for (int i = 0; i < kindsNum; ++i) {
+        int n, m;
+        scanf("%d%d", &n, &m);
+        for (int i = 0; i < n; ++i) {
             scanf("%d%d%d", &items[i].v, &items[i].w, &items[i].s);
         }
-        printf("%d\n", knapsack(kindsNum, packVolume));
+        printf("%d\n", knapsack(m, n));
         return 0;
     }
 };
