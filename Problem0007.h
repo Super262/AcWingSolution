d8d5f62ff46ce67cdfb87ea95e777f463404925d
fengwei <wuzhongfengwei@163.com>
2021-11-22 02:53:27 -0800

    Reviewed Problem 7.

--- Problem0007.h
+++ Problem0007.h
@@ -12,35 +12,41 @@ using namespace std;
 class Problem0007 {
 private:
     struct Item {
-        int v, w, s;
+        int v;
+        int w;
+        int s;
     };
 
-    const int N = 1010;
-    int dp[N];
-    Item items[N];
-
-    int knapsack(const int m, const int n) {
+    int knapsack(const Item items[], const int &n, const int &m) {
+        int dp[m + 1];
+        int temp[m + 1];
+        int q[m + 1];
+        memset(dp, 0, sizeof dp);
         for (int i = 0; i < n; ++i) {
             auto v = items[i].v;
             auto w = items[i].w;
             auto s = items[i].s;
             if (s == 0) {
-                for (int j = v; j <= m; ++j) {
-                    dp[j] = max(dp[j], dp[j - v] + w);
+                for (int j = items[i].v; j <= m; ++j) {
+                    dp[j] = max(dp[j - items[i].v] + items[i].w, dp[j]);
                 }
             } else {
                 if (s == -1) {
                     s = 1;
                 }
-                for (int factor = 1; factor <= s; factor *= 2) {
-                    for (int j = m; j >= factor * v; --j) {
-                        dp[j] = max(dp[j], dp[j - factor * v] + factor * w);
-                    }
-                    s -= factor;
-                }
-                if (s > 0) {
-                    for (int j = m; j >= s * v; --j) {
-                        dp[j] = max(dp[j], dp[j - s * v] + s * w);
+                memcpy(temp, dp, sizeof dp);
+                for (int r = 0; r < v; ++r) {
+                    int hh = 0;
+                    int tt = -1;
+                    for (int j = r; j <= m; j += v) {
+                        while (hh <= tt && q[hh] < j - s * v) {
+                            ++hh;
+                        }
+                        while (hh <= tt && temp[q[tt]] - q[tt] / v * w <= temp[j] - j / v * w) {
+                            --tt;
+                        }
+                        q[++tt] = j;
+                        dp[j] = max(dp[j], temp[q[hh]] + (j - q[hh]) / v * w);
                     }
                 }
             }
@@ -51,10 +57,11 @@ private:
     int main() {
         int n, m;
         scanf("%d%d", &n, &m);
+        Item items[n];
         for (int i = 0; i < n; ++i) {
             scanf("%d%d%d", &items[i].v, &items[i].w, &items[i].s);
         }
-        printf("%d\n", knapsack(m, n));
+        printf("%d\n", knapsack(items, n, m));
         return 0;
     }
 };
