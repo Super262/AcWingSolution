efd32346f8f0016d6918ae22156d024cd1f8df4a
fengwei <wuzhongfengwei@163.com>
2021-11-22 02:27:44 -0800

    Reviewed Problem 5.

--- Problem0005.h
+++ Problem0005.h
@@ -6,48 +6,48 @@
 #define ACWINGSOLUTION_PROBLEM0005_H
 
 #include <iostream>
-#include <vector>
+#include <cstring>
 
 using namespace std;
 
 class Problem0005 {
 private:
     struct Item {
-        int size;
-        int value;
+        int v;
+        int w;
+        int s;
     };
 
-    int dp[2010];
-
-    int knapsack(const vector<Item> &items, const int packVolume) {
-        for (auto item : items) {
-            for (int j = packVolume; j >= item.size; --j) {
-                dp[j] = max(dp[j], dp[j - item.size] + item.value);
+    int knapsack(Item items[], const int &n, const int &m) {
+        int dp[m + 1];
+        memset(dp, 0, sizeof dp);
+        for (int i = 0; i < n; ++i) {
+            auto v = items[i].v;
+            auto w = items[i].w;
+            auto s = items[i].s;
+            for (int factor = 1; factor <= s; factor *= 2) {
+                for (int j = m; j >= factor * v; --j) {
+                    dp[j] = max(dp[j], dp[j - factor * v] + factor * w);
+                }
+                s -= factor;
+            }
+            if (s > 0) {
+                for (int j = m; j >= s * v; --j) {
+                    dp[j] = max(dp[j], dp[j - s * v] + s * w);
+                }
             }
         }
-        return dp[packVolume];
+        return dp[m];
     }
 
     int main() {
         int n, m;
         scanf("%d%d", &n, &m);
-        vector<Item> items;
+        Item items[n];
         for (int i = 0; i < n; ++i) {
-            int s, v, num;
-            scanf("%d%d%d", &s, &v, &num);
-            int factor = 1;
-            while (num >= factor) {
-                Item item{factor * s, factor * v};
-                items.emplace_back(item);
-                num -= factor;
-                factor <<= 1;
-            }
-            if (num > 0) {
-                Item item{num * s, num * v};
-                items.emplace_back(item);
-            }
+            scanf("%d%d%d", &items[i].v, &items[i].w, &items[i].s);
         }
-        printf("%d\n", knapsack(items, m));
+        printf("%d\n", knapsack(items, n, m));
         return 0;
     }
 };
