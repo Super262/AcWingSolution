86ed636f18bc458e7e0c27852d71eda2f8daf612
fengwei <wuzhongfengwei@163.com>
2021-11-21 23:57:20 -0800

    Reviewed Problem 487.

--- Problem0487.h
+++ Problem0487.h
@@ -6,35 +6,35 @@
 #define ACWINGSOLUTION_PROBLEM0487_H
 
 #include <vector>
-#include <algorithm>
+#include <cstring>
 #include <iostream>
 
 using namespace std;
 
 class Problem0487 {
 private:
-    int knapsack(const int m,
-                 const vector<pair<int, int>> &masters,
-                 const vector<vector<pair<int, int>>> &accessories) {
-        vector<int> dp(m + 1, 0);
-        for (int i = 1; i < masters.size(); ++i) {
+    int knapsack(const vector<pair<int, int>> &masters,
+                 const vector<vector<pair<int, int>>> &accessories,
+                 const int &m) {
+        int dp[m + 1];
+        memset(dp, 0, sizeof dp);
+        for (int i = 1; i < (int) masters.size(); ++i) {
             if (masters[i].first == 0) {
                 continue;
             }
             for (int j = m; j >= 0; --j) {
-                // 利用二进制进行压缩
-                for (int k = 0; k < (1 << accessories[i].size()); ++k) {
+                for (int s = 0; s < (1 << accessories[i].size()); ++s) {
                     auto v = masters[i].first;
-                    auto wv = masters[i].second;
-                    for (int u = 0; u < accessories[i].size(); ++u) {
-                        if (((k >> u) & 1) == 0) {
+                    auto vw = masters[i].second;
+                    for (int k = 0; k < (int) accessories[i].size(); ++k) {
+                        if (((s >> k) & 1) == 0) {
                             continue;
                         }
-                        v += accessories[i][u].first;
-                        wv += accessories[i][u].second;
+                        v += accessories[i][k].first;
+                        vw += accessories[i][k].second;
                     }
                     if (j >= v) {
-                        dp[j] = max(dp[j - v] + wv, dp[j]);
+                        dp[j] = max(dp[j], dp[j - v] + vw);
                     }
                 }
             }
@@ -43,7 +43,8 @@ private:
     }
 
     int main() {
-        int m, n;
+        int m;
+        int n;
         scanf("%d%d", &m, &n);
         vector<pair<int, int>> masters(n + 1, pair<int, int>(0, 0));
         vector<vector<pair<int, int>>> accessories(n + 1, vector<pair<int, int>>());
@@ -57,7 +58,7 @@ private:
                 masters[i].second = w * v;
             }
         }
-        printf("%d\n", knapsack(m, masters, accessories));
+        printf("%d\n", knapsack(masters, accessories, m));
         return 0;
     }
 };
