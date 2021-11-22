50a2ff123b9ee0f88ab09289dfd82c44fcddc75c
fengwei <wuzhongfengwei@163.com>
2021-11-22 02:52:11 -0800

    Reviewed Problem 6.

--- Problem0006.h
+++ Problem0006.h
@@ -14,32 +14,46 @@ class Problem0006 {
     // https://www.acwing.com/solution/content/6500/
 private:
     struct Item {
-        int v, w, s;
+        int v;
+        int w;
+        int s;
     };
 
-    int knapsack(Item items[], const int m, const int n) {
-        int dp[2][m + 1];
+    int knapsack(const Item items[], const int &n, const int &m) {
+        int dp[m + 1];
+        int temp[m + 1];
         int q[m + 1];
         memset(dp, 0, sizeof dp);
         for (int i = 0; i < n; ++i) {
             auto v = items[i].v;
             auto w = items[i].w;
             auto s = items[i].s;
-            for (int r = 0; r < v; ++r) {
-                int hh = 0, tt = -1;
-                for (int j = r; j <= m; j += v) {
-                    while (hh <= tt && q[hh] < j - s * v) {
-                        ++hh;
+            if (s == 0) {
+                for (int j = items[i].v; j <= m; ++j) {
+                    dp[j] = max(dp[j - items[i].v] + items[i].w, dp[j]);
+                }
+            } else {
+                if (s == -1) {
+                    s = 1;
+                }
+                memcpy(temp, dp, sizeof dp);
+                for (int r = 0; r < v; ++r) {
+                    int hh = 0;
+                    int tt = -1;
+                    for (int j = r; j <= m; j += v) {
+                        while (hh <= tt && q[hh] < j - s * v) {
+                            ++hh;
+                        }
+                        while (hh <= tt && temp[q[tt]] - q[tt] / v * w <= temp[j] - j / v * w) {
+                            --tt;
+                        }
+                        q[++tt] = j;
+                        dp[j] = max(dp[j], temp[q[hh]] + (j - q[hh]) / v * w);
                     }
-                    while (hh <= tt && dp[i % 2][q[tt]] - q[tt] / v * w <= dp[i % 2][j] - j / v * w) {
-                        --tt;
-                    }
-                    q[++tt] = j;
-                    dp[(i + 1) % 2][j] = max(dp[(i + 1) % 2][j], dp[i % 2][q[hh]] + (j - q[hh]) / v * w);
                 }
             }
         }
-        return dp[n % 2][m];
+        return dp[m];
     }
 
     int main() {
@@ -49,7 +63,7 @@ private:
         for (int i = 0; i < n; ++i) {
             scanf("%d%d%d", &items[i].v, &items[i].w, &items[i].s);
         }
-        printf("%d\n", knapsack(items, m, n));
+        printf("%d\n", knapsack(items, n, m));
         return 0;
     }
 };
