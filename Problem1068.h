fa207876ed7b9bb59f84fb9b81824dbf1d05699c
fengwei <wuzhongfengwei@163.com>
2021-11-23 12:02:59 -0800

    Reviewed Problem 1068.

--- Problem1068.h
+++ Problem1068.h
@@ -14,47 +14,44 @@ class Problem1068 {
     // https://www.acwing.com/solution/content/15641/
     // 化环为链，大大降低复杂度
 private:
-    const int N = 200;
-    int items[2 * N + 1];
-    int dpMax[2 * N + 1][2 * N + 1];
-    int dpMin[2 * N + 1][2 * N + 1];
-
-    void rangeModel(const int n) {
+    void rangeModel(int items[], const int &n) {
+        int dpMax[2 * n + 1][2 * n + 1];
+        int dpMin[2 * n + 1][2 * n + 1];
         memset(dpMax, -0x3f, sizeof dpMax);
         memset(dpMin, 0x3f, sizeof dpMin);
         for (int i = 1; i <= 2 * n; ++i) {
+            items[i] += items[i - 1];
             dpMin[i][i] = 0;
             dpMax[i][i] = 0;
-            items[i] += items[i - 1];
         }
-        for (int length = 1; length <= n; ++length) {
-            for (int start = 1; start + length - 1 <= 2 * n; ++start) {
-                int end = start + length - 1;
-                for (int mid = start; mid < end; ++mid) {
-                    dpMax[start][end] = max(dpMax[start][end],
-                                            dpMax[start][mid] + dpMax[mid + 1][end] + items[end] - items[start - 1]);
-                    dpMin[start][end] = min(dpMin[start][end],
-                                            dpMin[start][mid] + dpMin[mid + 1][end] + items[end] - items[start - 1]);
+        for (int length = 2; length <= n; ++length) {
+            for (int st = 1; st + length - 1 <= 2 * n; ++st) {
+                auto ed = st + length - 1;
+                for (int mid = st; mid < ed; ++mid) {
+                    dpMax[st][ed] = max(dpMax[st][ed], dpMax[st][mid] + dpMax[mid + 1][ed] + items[ed] - items[st - 1]);
+                    dpMin[st][ed] = min(dpMin[st][ed], dpMin[st][mid] + dpMin[mid + 1][ed] + items[ed] - items[st - 1]);
                 }
             }
         }
         int resMin = 0x3f3f3f3f;
         int resMax = -0x3f3f3f3f;
-        for (int i = 1; i <= n; ++i) {
-            resMin = min(resMin, dpMin[i][i + n - 1]);
-            resMax = max(resMax, dpMax[i][i + n - 1]);
+        for (int st = 1; st + n - 1 <= 2 * n; ++st) {
+            resMin = min(resMin, dpMin[st][st + n - 1]);
+            resMax = max(resMax, dpMax[st][st + n - 1]);
         }
-        printf("%d\n%d\n", resMin, resMax);
+        printf("%d\n", resMin);
+        printf("%d\n", resMax);
     }
 
     int main() {
         int n;
         scanf("%d", &n);
+        int items[2 * n + 1];
         for (int i = 1; i <= n; ++i) {
             scanf("%d", &items[i]);
             items[i + n] = items[i];
         }
-        rangeModel(n);
+        rangeModel(items, n);
         return 0;
     }
 };
