efc5b12c6002b0a6706d9ccbfd87a33492603910
fengwei <wuzhongfengwei@163.com>
2021-11-23 15:34:31 -0800

    Reviewed Problem 1072.

--- Problem1072.h
+++ Problem1072.h
@@ -6,7 +6,7 @@
 #define ACWINGSOLUTION_PROBLEM1072_H
 
 #include <iostream>
-#include <cstring>
+#include <vector>
 
 using namespace std;
 
@@ -14,28 +14,13 @@ class Problem1072 {
     // 任取一点为起点，找到距离该点最远的点u
     // 找到距离u最远的点v；u和v间的距离即为直径
 private:
-    const int N = 10005;
-    int headIndex[N];
-    int nextIndex[2 * N];
-    int vertexValue[2 * N];
-    int weight[2 * N];
-
-    void addEdge(int a, int b, int w, int &idx) {
-        vertexValue[idx] = b;
-        weight[idx] = w;
-        nextIndex[idx] = headIndex[a];
-        headIndex[a] = idx;
-        ++idx;
-    }
-
-    int treeModel(int root, int father, int &answer) {
+    int treeModel(const int root, const int father, const vector<vector<pair<int, int>>> &graph, int &answer) {
         int d1 = 0, d2 = 0;
-        for (int idx = headIndex[root]; idx != -1; idx = nextIndex[idx]) {
-            auto childV = vertexValue[idx];
-            if (childV == father) {
+        for (auto t: graph[root]) {
+            if (t.first == father) {
                 continue;
             }
-            int childD1 = treeModel(childV, root, answer) + weight[idx];
+            int childD1 = treeModel(t.first, root, graph, answer) + t.second;
             if (childD1 >= d1) {
                 d2 = d1;
                 d1 = childD1;
@@ -48,19 +33,17 @@ private:
     }
 
     int main() {
-        memset(headIndex, -1, sizeof headIndex);
-        memset(nextIndex, -1, sizeof nextIndex);
         int n;
         scanf("%d", &n);
-        int idx = 0;
-        for (int i = 0; i < n; ++i) {
+        vector<vector<pair<int, int>>> graph(n + 1, vector<pair<int, int>>());
+        for (int i = 0; i < n - 1; ++i) {
             int a, b, w;
             scanf("%d%d%d", &a, &b, &w);
-            addEdge(a, b, w, idx);
-            addEdge(b, a, w, idx);
+            graph[a].push_back({b, w});
+            graph[b].push_back({a, w});
         }
         int answer = 0;
-        treeModel(1, -1, answer);
+        treeModel(1, -1, graph, answer);
         printf("%d\n", answer);
         return 0;
     }
