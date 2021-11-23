d6a1ab0d5537bb6ca3460e519f72aa94ac6ea7c2
fengwei <wuzhongfengwei@163.com>
2021-11-23 09:58:56 -0800

    Reviewed Problem 1058.

--- Problem1058.h
+++ Problem1058.h
@@ -10,29 +10,28 @@
 using namespace std;
 
 class Problem1058 {
-    // 状态机（最大收益）：dp[i][0]（手中有货），dp[i][1]（手中无货的第1天），dp[i][2]（手中无货的第2天及以后）
+    // 状态机（最大收益）：dp[i][1]（手中有货），dp[i][2]（手中无货的第1天），dp[i][0]（手中无货的第2天及以后）
 private:
-    int items[100010];
-    int dp[2][3];
-
-    int stateMachine(const int n) {
+    int stateMachine(const int items[], const int &n) {
+        int dp[2][3];
         memset(dp, -0x3f, sizeof dp);
-        dp[0][2] = 0;
+        dp[0][0] = 0;
         for (int i = 1; i <= n; ++i) {
-            dp[i % 2][0] = max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][2] - items[i]);
-            dp[i % 2][1] = dp[(i - 1) % 2][0] + items[i];
-            dp[i % 2][2] = max(dp[(i - 1) % 2][2], dp[(i - 1) % 2][1]);
+            dp[i % 2][0] = max(dp[(i - 1) % 2][2], dp[(i - 1) % 2][0]);
+            dp[i % 2][1] = max(dp[(i - 1) % 2][0] - items[i], dp[(i - 1) % 2][1]);
+            dp[i % 2][2] = dp[(i - 1) % 2][1] + items[i];
         }
-        return max(dp[n % 2][1], dp[n % 2][2]);
+        return max(dp[n % 2][0], dp[n % 2][2]);
     }
 
     int main() {
         int n;
         scanf("%d", &n);
+        int items[n + 1];
         for (int i = 1; i <= n; ++i) {
             scanf("%d", &items[i]);
         }
-        printf("%d\n", stateMachine(n));
+        printf("%d\n", stateMachine(items, n));
         return 0;
     }
 };
