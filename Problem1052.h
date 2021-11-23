32942abc511f55ac5d43f8758520eb3deb9e6246
fengwei <wuzhongfengwei@163.com>
2021-11-23 11:13:33 -0800

    Reviewed Problem 1052.

--- Problem1052.h
+++ Problem1052.h
@@ -13,41 +13,39 @@ using namespace std;
 class Problem1052 {
     // https://www.acwing.com/solution/content/55449/
 private:
-    const int N = 52;
-    int dp[N][N]; // dp[i][j]：构造一个长度为i的密码，且后缀与模式串匹配的最大长度为j的方案数
-    char temp[N];
-    int nextStart[N];
-
-    void buildNextStart(const int m) {
+    void buildNext(const char s[], const int &n, int next[]) {
+        next[0] = 0;
         int left = 0;
         int right = 1;
-        while (right < m) {
-            if (temp[right] == temp[left]) {
+        while (right < n) {
+            if (s[left] == s[right]) {
                 ++left;
-                nextStart[right] = left;
+                next[right] = left;
                 ++right;
-            } else if (left) {
-                left = nextStart[left - 1];
+            } else if (left != 0) {
+                left = next[left - 1];
             } else {
-                nextStart[right] = 0;
+                next[right] = 0;
                 ++right;
             }
         }
     }
 
-    int stateMachine(const int n, const int m) {
-        buildNextStart(m);
+    int stateMachine(const int &n, const char str[], const int &m, int nextStart[]) {
         const int MOD = 1000000007;
+        buildNext(str, m, nextStart);
+        int dp[n + 1][m + 1];
+        memset(dp, 0, sizeof dp);
         dp[0][0] = 1;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {  // 循环当前的密码长度，探索下一位
             for (int j = 0; j < m; ++j) {
                 // 遍历结果（密码字符串）第i位的所有可能取值（a-z）
                 for (char ch = 'a'; ch <= 'z'; ++ch) {
-                    auto u = j; // u是模版字符串下一个待匹配的字符的索引
-                    while (u && temp[u] != ch) {
+                    auto u = j; // u是str下一个待匹配的字符的索引
+                    while (u && ch != str[u]) {
                         u = nextStart[u - 1];
                     }
-                    if (temp[u] == ch) {
+                    if (str[u] == ch) {
                         ++u;
                     }
                     // 新的结果字符串（长度为i + 1）的方案数包括原结果字符串（长度为i）的方案数
@@ -55,18 +53,22 @@ private:
                 }
             }
         }
-        int result = 0;
-        for (int i = 0; i < m; ++i) {
-            result = (int) (((long) result + dp[n][i]) % MOD);
+        long result = 0;
+        for (int i = m - 1; i >= 0; --i) { // 统计所有与str匹配的长度小于m的方案
+            result += dp[n][i];
+            result %= MOD;
         }
-        return result;
+        return (int) result;
     }
 
     int main() {
         int n;
         scanf("%d", &n);
-        scanf("%s", temp);
-        printf("%d\n", stateMachine(n, strlen(temp)));
+        char str[51];
+        scanf("%s", str);
+        const int m = (int) strlen(str);
+        int nextStart[m];
+        printf("%d\n", stateMachine(n, str, m, nextStart));
         return 0;
     }
 };
