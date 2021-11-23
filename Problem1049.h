49e41a9ac3e4228420b8d2f548599d997cd7603c
fengwei <wuzhongfengwei@163.com>
2021-11-23 08:38:43 -0800

    Reviewed Problem 1049.

--- Problem1049.h
+++ Problem1049.h
@@ -11,29 +11,28 @@ using namespace std;
 
 class Problem1049 {
 private:
-    int dp[2][2];  // 滚动数组优化
-    int items[100010];
-
-    int stateMachine(const int n) {
+    int stateMachine(const int items[], const int &n) {  // 利用状态机，把每个dp[i]分解为2个状态
+        int dp[2][2];
         dp[0][0] = 0;
-        dp[0][1] = -1;
-        for (int i = 1; i <= n; ++i) {
-            dp[i % 2][0] = max(dp[(i - 1) % 2][1], dp[(i - 1) % 2][0]);
+        dp[0][1] = items[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i % 2][0] = max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1]);
             dp[i % 2][1] = dp[(i - 1) % 2][0] + items[i];
         }
-        return max(dp[n % 2][0], dp[n % 2][1]);
+        return max(dp[(n - 1) % 2][0], dp[(n - 1) % 2][1]);
     }
 
     int main() {
         int t;
+        int n;
+        int items[100000];
         scanf("%d", &t);
         while (t--) {
-            int n;
             scanf("%d", &n);
-            for (int i = 1; i <= n; ++i) {
+            for (int i = 0; i < n; ++i) {
                 scanf("%d", &items[i]);
             }
-            printf("%d\n", stateMachine(n));
+            printf("%d\n", stateMachine(items, n));
         }
         return 0;
     }
