0e9344e1c8f5b866d0a960cb90e0e26a0e3582be
fengwei <wuzhongfengwei@163.com>
2021-11-23 18:06:49 -0800

    Reviewed Problem 1075.

--- Problem1075.h
+++ Problem1075.h
@@ -6,35 +6,21 @@
 #define ACWINGSOLUTION_PROBLEM1075_H
 
 #include <iostream>
-#include <cstring>
+#include <vector>
 
 using namespace std;
 
 class Problem1075 {
     // https://www.acwing.com/solution/content/8015/
-public:
-    const int MAX_N = 100010;
-    int headIndex[MAX_N];
-    int vertexValue[2 * MAX_N];
-    int nextIndex[2 * MAX_N];
-
-    void addEdge(const int a, const int b, int &idx) {
-        vertexValue[idx] = b;
-        nextIndex[idx] = headIndex[a];
-        headIndex[a] = idx;
-        ++idx;
-    }
-
-    int dfs(const int root, const int father, int &maxDistance, bool *visited) {
+private:
+    int downMax(int root, int father, int &answer, const vector<vector<int>> &graph, vector<bool> &visited) {
         visited[root] = true;
-        int d1 = 0;
-        int d2 = 0;
-        for (int idx = headIndex[root]; idx != -1; idx = nextIndex[idx]) {
-            int child = vertexValue[idx];
-            if (child == father) {
+        int d1 = 0, d2 = 0;
+        for (const auto &t: graph[root]) {
+            if (t == father) {
                 continue;
             }
-            int d = dfs(child, root, maxDistance, visited) + 1;
+            auto d = downMax(t, root, answer, graph, visited) + 1;
             if (d >= d1) {
                 d2 = d1;
                 d1 = d;
@@ -42,45 +28,40 @@ public:
                 d2 = d;
             }
         }
-        maxDistance = max(maxDistance, d1 + d2);
+        answer = max(answer, d1 + d2);
         return d1;
     }
 
     int main() {
-        memset(headIndex, -1, sizeof headIndex);
-        memset(nextIndex, -1, sizeof nextIndex);
         int n;
         scanf("%d", &n);
-        auto divisorsSum = new int[n + 1];
-        memset(divisorsSum, 0, sizeof(int) * (n + 1));
-        for (int a = 1; a <= n; ++a) {
+        vector<int> divsSum(n + 1, 0);
+        for (int a = 1; a <= n; ++a) {  // 时间复杂度：O(nlogn)
+            // 避免溢出，不使用“a * b <= n”
             // b不从1开始，避免累加自身（n）到因子和中
-            for (int b = 2; b <= n / a; ++b) {  // 避免溢出，不使用“a * b <= n”
-                divisorsSum[a * b] += a;
+            for (int b = 2; b <= n / a; ++b) {
+                divsSum[a * b] += a;
             }
         }
-        int idx = 0;
+        vector<vector<int>> graph(n + 1, vector<int>());
         for (int i = 1; i <= n; ++i) {
-            if (divisorsSum[i] > i) {
+            if (i < divsSum[i]) {
                 continue;
             }
             // 添加双向边
-            addEdge(divisorsSum[i], i, idx);
-            addEdge(i, divisorsSum[i], idx);
+            graph[i].push_back(divsSum[i]);
+            graph[divsSum[i]].push_back(i);
         }
-        auto visited = new bool[n + 1];
-        memset(visited, 0, sizeof(bool) * (n + 1));
-        int maxDistance = 0;
-        for (int i = 1; i < n; ++i) {
-            // 遍历所有可能的根结点（数、因数和）
-            if (headIndex[i] == -1 || visited[i]) {
+        int answer = 0;
+        vector<bool> visited(n + 1, false);
+        for (int i = 1; i <= n; ++i) {
+            if (graph[i].empty() || visited[i]) {
                 continue;
             }
-            dfs(i, -1, maxDistance, visited);
+            // 遍历所有可能的根结点（数、因数和）
+            downMax(i, -1, answer, graph, visited);
         }
-        delete[] divisorsSum;
-        delete[] visited;
-        printf("%d\n", maxDistance);
+        printf("%d\n", answer);
         return 0;
     }
 };
