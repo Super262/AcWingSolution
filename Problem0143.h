dc961ee5919ee70cd959e6acd1fc48eef7f70f9a
fengwei <wuzhongfengwei@163.com>
2021-11-27 11:42:47 -0800

    Reviewed Problem 143.

--- Problem0143.h
+++ Problem0143.h
@@ -10,45 +10,67 @@
 using namespace std;
 
 class Problem0143 {
-public:
-    void insertNum(unsigned int storage[][2], const unsigned int num, unsigned int &nextIdx) {
-        unsigned int currentIdx = 0;
+private:
+    struct Node {
+        Node *next[2]{};
+
+        Node() {
+            next[0] = nullptr;
+            next[1] = nullptr;
+        }
+    };
+
+    void insertNum(Node *root, const unsigned int num) {
+        auto p = root;
         for (int i = 30; i >= 0; --i) {
-            bool chIndex = (num >> i) & 1;
-            if (!storage[currentIdx][chIndex]) {
-                storage[currentIdx][chIndex] = ++nextIdx;
-            }
-            currentIdx = storage[currentIdx][chIndex];
+            if ((num >> i) & 1) {
+                if (!p->next[1]) {
+                    p->next[1] = new Node();
+                }
+                p = p->next[1];
+            } else {
+                if (!p->next[0]) {
+                    p->next[0] = new Node();
+                }
+                p = p->next[0];
+            };
         }
     }
 
-    unsigned int maxXOR(const unsigned int storage[][2], const unsigned int num) {
+    unsigned int maxXOR(Node *root, const unsigned int num) {
         unsigned int result = 0;
-        unsigned int currentIdx = 0;
+        auto p = root;
         for (int i = 30; i >= 0; --i) {
-            bool chIndex = (num >> i) & 1;
-            if (storage[currentIdx][!chIndex]) {
-                result = result * 2 + 1;
-                currentIdx = storage[currentIdx][!chIndex];
+            result *= 2;
+            if ((num >> i) & 1) {
+                if (p->next[0]) {
+                    p = p->next[0];
+                    result += 1;
+                } else {
+                    p = p->next[1];
+                }
             } else {
-                result = result * 2 + 0;
-                currentIdx = storage[currentIdx][chIndex];
-            }
+                if (p->next[1]) {
+                    p = p->next[1];
+                    result += 1;
+                } else {
+                    p = p->next[0];
+                }
+            };
         }
         return result;
     }
 
     int main() {
-        unsigned int n;
+        int n;
         scanf("%d", &n);
-        unsigned int result = 0;
-        unsigned int storage[31 * 100001][2];
         unsigned int num;
-        unsigned int nextIdx = 0;
-        for (unsigned int i = 0; i < n; ++i) {
+        auto root = new Node();
+        unsigned int result = 0;
+        for (int i = 0; i < n; ++i) {
             scanf("%d", &num);
-            insertNum(storage, num, nextIdx);
-            result = max(result, maxXOR(storage, num));
+            insertNum(root, num);
+            result = max(result, maxXOR(root, num));
         }
         printf("%d\n", result);
         return 0;
