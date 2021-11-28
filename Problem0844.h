695dddb99d913a97954173f9edce6a0a2b435258
fengwei <wuzhongfengwei@163.com>
2021-11-28 13:29:21 -0800

    Reviewed Problem 844.

--- Problem0844.h
+++ Problem0844.h
@@ -17,7 +17,7 @@ private:
     bool visited[N][N];
     pair<int, int> q[N * N + 10];
 
-    int bfs(const int n, const int m) {  // 反向BFS：从终点搜索到起点
+    int bfs(const int n, const int m) {  // 反向BFS：从终点搜索到起点，和正向搜索等价
         int dx[] = {0, 1, 0, -1}, dy[] = {-1, 0, 1, 0};
         int hh = 0, tt = -1;
         q[++tt] = pair<int, int>(n - 1, m - 1);
