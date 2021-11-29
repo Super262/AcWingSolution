8055b5222912f53b2bc49999825b4f301fd03dbe
fengwei <wuzhongfengwei@163.com>
2021-11-28 21:43:09 -0800

    Reviewed Problem 852.

--- Problem0852.h
+++ Problem0852.h
@@ -7,81 +7,63 @@
 
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
 class Problem0852 {
+    // 1. 虚拟源点
+    // 2. 最短路径经过的边数大于等于n，有负环
 private:
-    const int N = 2010;
-    const int M = 10010;
-    int headIndex[N];
-    int nextIndex[M];
-    int vertexValue[M];
-    int weight[M];
-
-    void addEdge(const int a, const int b, const int w, int &idx) {
-        vertexValue[idx] = b;
-        weight[idx] = w;
-        nextIndex[idx] = headIndex[a];
-        headIndex[a] = idx;
-        ++idx;
-    }
-
-    bool spfa(const int n, const int m) {
-        auto q = new int[m * n + 10];
-        auto dist = new int[n + 1];
-        auto isInQueue = new bool[n + 1];
-        auto routeLen = new int[n + 1];
-        memset(dist, 0x7f, sizeof(int) * (n + 1));
-        memset(isInQueue, 0, sizeof(bool) * (n + 1));
-        memset(routeLen, 0, sizeof(int) * (n + 1));
-        int hh = 0, tt = -1;
+    bool spfa(const int n, const vector<vector<pair<int, int>>> &graph) {
+        int dist[n + 1];
+        bool in_queue[n + 1];
+        int edges_cnt[n + 1];
+        queue<int> q;
+        memset(dist, 0x3f, sizeof dist);
+        memset(in_queue, 0, sizeof in_queue);
+        memset(edges_cnt, 0, sizeof edges_cnt);
         for (int v = 1; v <= n; ++v) {
-            isInQueue[v] = true;
-            q[++tt] = v;
+            q.emplace(v);
             dist[v] = 0;
+            in_queue[v] = true;
         }
-        bool result = false;
-        while (hh <= tt) {
-            auto node = q[hh++];
-            isInQueue[node] = false;
-            for (auto idx = headIndex[node]; idx != -1; idx = nextIndex[idx]) {
-                auto nextV = vertexValue[idx];
-                if (dist[nextV] <= dist[node] + weight[idx]) {
+        while (!q.empty()) {
+            auto root = q.front();
+            q.pop();
+            in_queue[root] = false;
+            for (const auto &t: graph[root]) {
+                auto nd = t.first;
+                auto nv = t.second;
+                if (dist[nv] <= dist[root] + nd) {
                     continue;
                 }
-                dist[nextV] = dist[node] + weight[idx];
-                routeLen[nextV] = routeLen[node] + 1;
-                if (routeLen[nextV] >= n) {
-                    result = true;
-                    break;
+                dist[nv] = dist[root] + nd;
+                edges_cnt[nv] = edges_cnt[root] + 1;
+                if (edges_cnt[nv] >= n) {
+                    return true;
                 }
-                if (isInQueue[nextV]) {
+                if (in_queue[nv]) {
                     continue;
                 }
-                q[++tt] = nextV;
-                isInQueue[nextV] = true;
+                q.emplace(nv);
+                in_queue[nv] = true;
             }
         }
-        delete[] q;
-        delete[] dist;
-        delete[] isInQueue;
-        delete[] routeLen;
-        return result;
+        return false;
     }
 
     int main() {
-        memset(headIndex, -1, sizeof headIndex);
-        memset(nextIndex, -1, sizeof nextIndex);
         int n, m;
         scanf("%d%d", &n, &m);
-        int a, b, w;
-        int idx = 0;
+        vector<vector<pair<int, int>>> graph(n + 1, vector<pair<int, int>>());
+        int u, v, w;
         for (int i = 0; i < m; ++i) {
-            scanf("%d%d%d", &a, &b, &w);
-            addEdge(a, b, w, idx);
+            scanf("%d%d%d", &u, &v, &w);
+            graph[u].push_back({w, v});
         }
-        if (spfa(n, m)) {
+        if (spfa(n, graph)) {
             printf("Yes\n");
         } else {
             printf("No\n");
