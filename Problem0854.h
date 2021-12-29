fd943327ea6f1ff2dea541077e6136d718fa97d0
fengwei <virusformail@gmail.com>
2021-12-29 14:47:16 -0800

    Reviewed Problem 854.

--- Problem0854.h
+++ Problem0854.h
@@ -13,6 +13,8 @@ using namespace std;
 
 class Problem0854 {
 private:
+    const int INF = 0x3f3f3f3f;
+
     vector<vector<int>> floyd(const int n, const vector<vector<int>> &graph) {
         vector<vector<int>> dist(graph);
         for (int i = 1; i <= n; ++i) {  // 注意：起点和终点相同，距离为0
@@ -21,7 +23,7 @@ private:
         for (int mid = 1; mid <= n; ++mid) {
             for (int st = 1; st <= n; ++st) {
                 for (int ed = 1; ed <= n; ++ed) {
-                    if (dist[st][mid] == 0x3f3f3f3f || dist[mid][ed] == 0x3f3f3f3f) {  // 排除负边的影响，INF恒为0x3f3f3f3f
+                    if (dist[st][mid] == INF || dist[mid][ed] == INF) {  // 排除负边的影响，INF恒为0x3f3f3f3f
                         continue;
                     }
                     dist[st][ed] = min(dist[st][ed], dist[st][mid] + dist[mid][ed]);
@@ -34,7 +36,7 @@ private:
     int main() {
         int n, m, k;
         scanf("%d%d%d", &n, &m, &k);
-        vector<vector<int>> graph(n + 1, vector<int>(n + 1, 0x3f3f3f3f));
+        vector<vector<int>> graph(n + 1, vector<int>(n + 1, INF));
         int x, y, z;
         for (int i = 0; i < m; ++i) {
             scanf("%d%d%d", &x, &y, &z);
@@ -44,7 +46,7 @@ private:
         int a, b;
         for (int i = 0; i < k; ++i) {
             scanf("%d%d", &a, &b);
-            if (dist[a][b] == 0x3f3f3f3f) {
+            if (dist[a][b] == INF) {
                 printf("impossible\n");
             } else {
                 printf("%d\n", dist[a][b]);
