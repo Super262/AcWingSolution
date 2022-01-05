ac0580903d2b4f019cb89c5b29696e89e9ef9017
fengwei <virusformail@gmail.com>
2022-01-05 09:24:46 -0800

    Reviewed Problem 1123.

--- Problem1123.h
+++ Problem1123.h
@@ -21,18 +21,18 @@ class Problem1123 {
     // 有向图，所有边都连通，存在欧拉回路的充分必要条件：所有点的出度都等于入度
 private:
     int main() {
-        double x1, y1, x2, y2;
+        long x1, y1, x2, y2;  // 防止溢出发生（dx * dx）
         cin >> x1 >> y1;
-        double length = 0;
+        long double length = 0;
         while (cin >> x1 >> y1 >> x2 >> y2) {
             auto dx = x2 - x1;
             auto dy = y2 - y1;
             length += sqrt(dx * dx + dy * dy) * 2;
         }
-        int minutes = (int) round(length / 1000 / 20 * 60);
-        int hours = minutes / 60;
+        auto minutes = (long) round(length / 1000 / 20 * 60);
+        auto hours = minutes / 60;
         minutes %= 60;
-        printf("%d:%02d\n", hours, minutes);
+        printf("%ld:%02ld\n", hours, minutes);
         return 0;
     }
 };
