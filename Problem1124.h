b7c8dba0f8fc13f3a82226ed86df74260e7d183f
fengwei <virusformail@gmail.com>
2022-01-05 16:43:07 -0800

    Reviewed Problem 1124.

--- Problem1124.h
+++ Problem1124.h
@@ -6,56 +6,55 @@
 #define ACWINGSOLUTION_PROBLEM1124_H
 
 #include <iostream>
-#include <cstdio>
+#include <vector>
 #include <cstring>
 #include <algorithm>
 
 using namespace std;
 
 class Problem1124 {
-    // 邻接矩阵上的欧拉回路
+    // 邻接矩阵上的欧拉路径，并要求字典需最小
 private:
-    const int N = 501;
-    int graph[N][N];
-    int path[2 * N + 50];
-    int ptt = 0;
-    int degree[N];
+    const int N = 500;
 
-    void dfs(const int u) {
-        for (int i = 1; i <= N; ++i) {
-            if (graph[u][i] == 0) {
+    void Dfs(const int u, vector<vector<int>> &graph, int path[], int &p_top) {
+        for (int v = 1; v <= N; ++v) {  // 为保证结果是字典序最小的，只要从小到大遍历邻接节点即可
+            if (graph[u][v] == 0) {
                 continue;
             }
-            --graph[u][i];
-            --graph[i][u];
-            dfs(i);
+            // 删除当前边
+            --graph[u][v];
+            --graph[v][u];
+            Dfs(v, graph, path, p_top);
         }
-        path[ptt++] = u;
+        path[p_top++] = u;
     }
 
     int main() {
+        vector<vector<int>> graph(N + 1, vector<int>(N + 1));
+        int degree[N];
+        memset(degree, 0, sizeof degree);
         int m;
         scanf("%d", &m);
         int a, b;
         while (m--) {
             scanf("%d%d", &a, &b);
-            ++graph[a][b];
+            ++graph[a][b];  // 无向图，每次2条边
             ++graph[b][a];
             ++degree[a];
             ++degree[b];
         }
-        int root = 1;
-        while (!degree[root]) {
-            ++root;
-        }
+        int root = 1; // 设置起点初值为1，避免出错，所有点度数为偶数时，可以从任意点开始遍历
         for (int i = 1; i <= N; ++i) {
-            if (degree[i] % 2) {
+            if (degree[i] % 2) {  // 选择编号较小的奇数度点
                 root = i;
                 break;
             }
         }
-        dfs(root);
-        for (int i = ptt - 1; i >= 0; --i) {
+        int path[2 * N + 50];
+        int p_top = 0;
+        Dfs(root, graph, path, p_top);
+        for (int i = p_top - 1; i >= 0; --i) {
             printf("%d\n", path[i]);
         }
         return 0;
