ae2ab4f468fe5d0ca7ba9d2cc8c7461c08391973
fengwei <virusformail@gmail.com>
2022-01-05 17:57:26 -0800

    Reviewed Problem 1185.

--- Problem1185.h
+++ Problem1185.h
@@ -14,26 +14,25 @@ using namespace std;
 class Problem1185 {
     // 有向图判断欧拉路径是否存在：https://www.acwing.com/solution/content/66811/
 private:
-    const int N = 26;
-    int parent[N];  // 记录节点的祖宗节点
-    int degreeIn[N], degreeOut[N];  // 记录节点的入度和出度
-    int used[N];  // 记录字母的使用情况
-    char temp[1010];
-
-    int findRoot(const int x) {
+    int findRoot(const int x, int parent[]) {
         if (x != parent[x]) {
-            parent[x] = findRoot(parent[x]);
+            parent[x] = findRoot(parent[x], parent);
         }
         return parent[x];
     }
 
     int main() {
+        const int N = 26;
+        int parent[N];  // 记录节点的祖宗节点
+        int in_degree[N], out_degree[N];  // 记录节点的入度和出度
+        int existed[N];  // 记录字母是否在输入中出现
+        char temp[1010];
         int t;
         scanf("%d", &t);
         while (t--) {
-            memset(degreeIn, 0, sizeof degreeIn);
-            memset(degreeOut, 0, sizeof degreeOut);
-            memset(used, 0, sizeof used);
+            memset(in_degree, 0, sizeof in_degree);
+            memset(out_degree, 0, sizeof out_degree);
+            memset(existed, 0, sizeof existed);
             for (int i = 0; i < N; ++i) {
                 parent[i] = i;
             }
@@ -44,44 +43,52 @@ private:
                 auto len = (int) strlen(temp);
                 int a = temp[0] - 'a';
                 int b = temp[len - 1] - 'a';
-                used[a] = true;
-                used[b] = true;
-                ++degreeOut[a];
-                ++degreeIn[b];
-                parent[findRoot(a)] = findRoot(b);  // 将首位两端的元素合并到同一个并查集里面去
+                existed[a] = true;
+                existed[b] = true;
+                ++out_degree[a];
+                ++in_degree[b];
+                parent[findRoot(a, parent)] = findRoot(b, parent);  // 将首位两端的元素合并到同一个并查集里面去
             }
 
             bool success = true;
             int endCnt = 0, startCnt = 0;  // 起点和终点的数量
             for (int i = 0; i < N; ++i) {
-                if (degreeIn[i] == degreeOut[i]) {
+                if (in_degree[i] == out_degree[i]) {
                     continue;
                 }
-                if (degreeIn[i] - degreeOut[i] == 1) {  // 终点
+                if (in_degree[i] - out_degree[i] == 1) {  // 终点
                     ++endCnt;
-                } else if (degreeOut[i] - degreeIn[i] == 1) {  // 起点
+                } else if (out_degree[i] - in_degree[i] == 1) {  // 起点
                     ++startCnt;
                 } else {
                     success = false;
                     break;
                 }
             }
+            if (!success) {
+                puts("The door cannot be opened.");
+                continue;
+            }
 
             // 出入度合法的情况下不存在有向图欧拉路径的那两种情况，那么说明是不合法的
             if (success && !(endCnt == 0 && startCnt == 0 || endCnt == 1 && startCnt == 1)) {
                 success = false;
             }
+            if (!success) {
+                puts("The door cannot be opened.");
+                continue;
+            }
 
             // 可能存在孤立点的存在，所以要判断是否是一个联通块之内的
             int root = -1;
             for (int i = 0; i < N; ++i) {
-                if (!used[i]) {
+                if (!existed[i]) {
                     continue;
                 }
                 if (root == -1) {
-                    root = findRoot(i);
+                    root = findRoot(i, parent);
                 } else {
-                    if (root != findRoot(i)) {
+                    if (root != findRoot(i, parent)) {
                         success = false;
                         break;
                     }
