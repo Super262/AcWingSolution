73d76d9710edccc362d3c8245d3da14e8b48e762
fengwei <virusformail@gmail.com>
2022-01-11 11:28:31 -0800

    Reviewed Problem 870.

--- Problem0870.h
+++ Problem0870.h
@@ -12,15 +12,15 @@ using namespace std;
 
 class Problem0870 {
 public:
-    void getFactorsCount(unsigned long n, unordered_map<unsigned long, unsigned long> &primeCount) {
+    void GetFactorsCount(unsigned long n, unordered_map<unsigned long, unsigned long> &prime_count) {
         for (unsigned long f = 2; f <= n / f; ++f) {
             while (n % f == 0) {
                 n /= f;
-                ++primeCount[f];
+                ++prime_count[f];
             }
         }
         if (n > 1) {
-            ++primeCount[n];
+            ++prime_count[n];
         }
     }
 
@@ -28,14 +28,14 @@ public:
         int m;
         scanf("%d", &m);
         unsigned long n;
-        unordered_map<unsigned long, unsigned long> primeCount;
+        unordered_map<unsigned long, unsigned long> prime_count;
         while (m--) {
             scanf("%ld", &n);
-            getFactorsCount(n, primeCount);
+            GetFactorsCount(n, prime_count);
         }
         unsigned long result = 1;
         const unsigned long base = 1e9 + 7;
-        for (const auto &p:primeCount) {
+        for (const auto &p: prime_count) {
             result *= p.second + 1;
             result %= base;
         }
