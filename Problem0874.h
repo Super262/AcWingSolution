fba0cec8b1ef2db09822e08b2b50a23b680c33ad
fengwei <virusformail@gmail.com>
2022-01-10 22:32:19 -0800

    Reviewed Problem 874.

--- Problem0874.h
+++ Problem0874.h
@@ -11,23 +11,23 @@
 using namespace std;
 
 class Problem0874 {
-public:
-    unsigned long long eulerSum(const unsigned long long &n) {
-        auto *phi = new unsigned long long[n + 1];
-        auto *primes = new unsigned long long[n + 1];
-        auto *isFiltered = new bool[n + 1];
-        memset(phi, 0, (n + 1) * sizeof(unsigned long long));
-        memset(primes, 0, (n + 1) * sizeof(unsigned long long));
-        memset(isFiltered, 0, (n + 1) * sizeof(bool));
+private:
+    long long EulerSum(const int &n) {
+        int phi[n + 1];
+        int primes[n + 1];
+        bool filtered[n + 1];
+        memset(phi, 0, sizeof phi);
+        memset(primes, 0, sizeof primes);
+        memset(filtered, 0, sizeof filtered);
         phi[1] = 1;
-        unsigned long long count = 0;
-        for (unsigned long long f = 2; f <= n; ++f) {
-            if (!isFiltered[f]) {
+        int count = 0;
+        for (int f = 2; f <= n; ++f) {
+            if (!filtered[f]) {
                 primes[count++] = f;
                 phi[f] = f - 1;
             }
-            for (unsigned long long j = 0; primes[j] <= n / f; ++j) {
-                isFiltered[primes[j] * f] = true;
+            for (int j = 0; primes[j] <= n / f; ++j) {
+                filtered[primes[j] * f] = true;
                 if (f % primes[j]) {
                     phi[f * primes[j]] = phi[f] * (primes[j] - 1);
                 } else {
@@ -36,17 +36,17 @@ public:
                 }
             }
         }
-        unsigned long long sum = 0;
-        for (unsigned long long i = 1; i <= n; ++i) {
+        long long sum = 0;
+        for (int i = 1; i <= n; ++i) {
             sum += phi[i];
         }
         return sum;
     }
 
     int main() {
-        unsigned long long n;
-        scanf("%lld", &n);
-        printf("%lld\n", eulerSum(n));
+        int n;
+        scanf("%d", &n);
+        printf("%lld\n", EulerSum(n));
         return 0;
     }
 };
