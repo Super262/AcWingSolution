25a7bcdd2adf6ec24decbb261ef6483189896b6a
fengwei <virusformail@gmail.com>
2022-01-11 18:44:06 -0800

    Reviewed Problem 868.

--- Problem0868.h
+++ Problem0868.h
@@ -11,20 +11,20 @@
 using namespace std;
 
 class Problem0868 {
-public:
+private:
     // 时间复杂度：O(n)
     int PrimesCount(const int n) {
-        bool is_filtered[n + 1];
+        bool filtered[n + 1];
         int primes[n + 1];
         int p_cnt = 0;
-        memset(is_filtered, 0, sizeof is_filtered);
+        memset(filtered, 0, sizeof filtered);
         memset(primes, 0, sizeof primes);
         for (int f = 2; f <= n; ++f) {
-            if (!is_filtered[f]) {
+            if (!filtered[f]) {
                 primes[p_cnt++] = f;
             }
             for (int j = 0; primes[j] <= n / f; ++j) {
-                is_filtered[primes[j] * f] = true;
+                filtered[primes[j] * f] = true;
                 if (f % primes[j] == 0) {
                     break;
                 }
