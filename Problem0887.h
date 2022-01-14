0e46abcd5f3212c1f1ecff6732ee28053e45160c
fengwei <virusformail@gmail.com>
2022-01-14 09:57:30 -0800

    Reviewed Problem 887.

--- Problem0887.h
+++ Problem0887.h
@@ -10,43 +10,44 @@
 using namespace std;
 
 class Problem0887 {
-public:
-    unsigned int qmi(unsigned int a, unsigned int x, const unsigned int &m) {
-        unsigned int result = 1;
+    // 卢卡斯定理：https://www.acwing.com/solution/content/26553/
+private:
+    int QuickPower(int a, int x, const int &m) {
+        int result = 1;
         while (x) {
             if (x & 1) {
-                result = (unsigned int) ((unsigned long long) result * a % m);
+                result = (int) ((long long) result * a % m);
             }
             x >>= 1;
-            a = (unsigned int) ((unsigned long long) a * a % m);
+            a = (int) ((long long) a * a % m);
         }
         return result;
     }
 
-    unsigned int combinatorialNumber(const unsigned int &n, const unsigned int &m, const unsigned int &p) {
-        unsigned int result = 1;
-        for (unsigned int i = 1, j = n; i <= m; ++i, --j) {
-            result = (unsigned int) ((unsigned long long) result * j % p);
-            result = (unsigned int) ((unsigned long long) result * qmi(i, p - 2, p) % p);
+    int Combination(const int n, const int m, const int p) {
+        int result = 1;
+        for (int i = 1, j = n; i <= m; ++i, --j) {
+            result = (int) ((long long) result * j % p);
+            result = (int) ((long long) result * QuickPower(i, p - 2, p) % p);
         }
         return result;
     }
 
-    unsigned int lucas(const unsigned long long &n, const unsigned long long &m, const unsigned int &p) {
+    int Lucas(const long long n, const long long m, const int p) {
         if (n < p && m < p) {
-            return combinatorialNumber((unsigned int) n, (unsigned int) m, p);
+            return Combination((int) n, (int) m, p);
         }
-        return (unsigned int) ((unsigned long long) combinatorialNumber(n % p, m % p, p) * lucas(n / p, m / p, p) % p);
+        return (int) ((long long) Combination((int) (n % p), (int) (m % p), p) * Lucas(n / p, m / p, p) % p);
     }
 
     int main() {
         int n;
         scanf("%d", &n);
-        unsigned long long a, b;
-        unsigned int p;
+        long long a, b;
+        int p;
         while (n--) {
             scanf("%lld%lld%d", &a, &b, &p);
-            printf("%d\n", lucas(a, b, p));
+            printf("%d\n", Lucas(a, b, p));
         }
         return 0;
     }
