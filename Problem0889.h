9944fb61fce0b1ddc68a254bf718688f1765c509
fengwei <virusformail@gmail.com>
2022-01-14 18:31:40 -0800

    Reviewed Problem 889.

--- Problem0889.h
+++ Problem0889.h
@@ -10,35 +10,35 @@
 using namespace std;
 
 class Problem0889 {
-public:
-    unsigned int qmi(unsigned int a, unsigned int x, const unsigned int mod) {
-        unsigned int result = 1;
+private:
+    int qmi(int a, int x, const int mod) {
+        int result = 1;
         while (x) {
             if (x & 1) {
-                result = (unsigned int) ((unsigned long long) result * a % mod);
+                result = (int) ((long long) result * a % mod);
             }
-            a = (unsigned int) ((unsigned long long) a * a % mod);
+            a = (int) ((long long) a * a % mod);
             x >>= 1;
         }
         return result;
     }
 
-    unsigned int catalanNumber(const unsigned int n) {
+    int catalanNumber(const int n) {
         // 1 / (n + 1) * C(2 * n, n) = (2 * n)! / (n! * n!) == ((n + 1) * ... * (2 * n)) / n!
-        const unsigned mod = 1e9 + 7;
-        unsigned int result = 1;
-        for (unsigned i = n + 1; i <= 2 * n; ++i) {
-            result = (unsigned int) ((unsigned long long) result * i % mod);
+        const int mod = 1e9 + 7;
+        int result = 1;
+        for (auto i = n + 1; i <= 2 * n; ++i) {
+            result = (int) ((long long) result * i % mod);
         }
-        for (unsigned i = 1; i <= n; ++i) {
-            result = (unsigned int) ((unsigned long long) result * qmi(i, mod - 2, mod) % mod);
+        for (int i = 1; i <= n; ++i) {
+            result = (int) ((long long) result * qmi(i, mod - 2, mod) % mod);
         }
-        result = (unsigned int) ((unsigned long long) result * qmi(n + 1, mod - 2, mod) % mod);
+        result = (int) ((long long) result * qmi(n + 1, mod - 2, mod) % mod);
         return result;
     }
 
     int main() {
-        unsigned int n;
+        int n;
         scanf("%d", &n);
         printf("%d", catalanNumber(n));
         return 0;
