4ea17661633877c21784cffd1dd0e7e8a0726319
fengwei <virusformail@gmail.com>
2022-01-16 15:56:59 -0800

    Reviewed Problem 884.

--- Problem0884.h
+++ Problem0884.h
@@ -10,27 +10,29 @@
 using namespace std;
 
 class Problem0884 {
-public:
-    int gaussXOR(int **matrix, const int n) {
-        int column, row, nonZeroRow;
-        for (column = row = 0; column < n; ++column) {
+private:
+    static const int N = 101;
+
+    int GaussXOR(int matrix[][N], const int n, const int m) {
+        int column, row, non_zero_row;
+        for (column = row = 0; column < m - 1; ++column) {
 
             // 1. 找到当前列非0的行
-            nonZeroRow = row;
+            non_zero_row = row;
             for (int r = row; r < n; ++r) {
                 if (matrix[r][column]) {
-                    nonZeroRow = r;
+                    non_zero_row = r;
                     break;
                 }
             }
 
-            if (!matrix[nonZeroRow][column]) {
+            if (!matrix[non_zero_row][column]) {
                 continue;
             }
 
             // 2. 移动目标行到当前行
-            for (int c = column; c <= n; ++c) {
-                swap(matrix[nonZeroRow][c], matrix[row][c]);
+            for (int c = column; c < m; ++c) {
+                swap(matrix[non_zero_row][c], matrix[row][c]);
             }
 
             // 3. 将当前列的其它行调整为0
@@ -38,7 +40,7 @@ public:
                 if (!matrix[r][column]) {
                     continue;
                 }
-                for (int c = n; c >= column; --c) {
+                for (int c = m - 1; c >= column; --c) {
                     matrix[r][c] ^= matrix[row][c] & matrix[r][column];
                 }
             }
@@ -55,8 +57,8 @@ public:
 
         // 消去X(i+1)～X(n)，求解X(i)
         for (int i = n - 1; i >= 0; --i) {
-            for (int j = i + 1; j < n; ++j) {
-                matrix[i][n] ^= matrix[i][j] & matrix[j][n];
+            for (int j = i + 1; j < m - 1; ++j) {
+                matrix[i][m - 1] ^= matrix[i][j] & matrix[j][m - 1];
             }
         }
         return 0;
@@ -65,29 +67,25 @@ public:
     int main() {
         int n;
         scanf("%d", &n);
-        int **matrix = new int *[n];
-        int v;
+        int matrix[n][N];
+        auto m = n + 1;
         for (int i = 0; i < n; ++i) {
-            matrix[i] = new int[n + 1];
-            for (int j = 0; j <= n; ++j) {
+            for (int j = 0; j < m; ++j) {
+                int v;
                 scanf("%d", &v);
                 matrix[i][j] = v;
             }
         }
-        int t = gaussXOR(matrix, n);
+        auto t = GaussXOR(matrix, n, m);
         if (t == 1) {
             puts("Multiple sets of solutions");
         } else if (t == 2) {
             puts("No solution");
         } else {
             for (int i = 0; i < n; ++i) {
-                printf("%d\n", matrix[i][n]);
+                printf("%d\n", matrix[i][m - 1]);
             }
         }
-        for (int i = 0; i < n; ++i) {
-            delete[] matrix[i];
-        }
-        delete[] matrix;
         return 0;
     }
 };
