153ca4f56e168bca522a09117fb9bc4f85da64ec
fengwei <virusformail@gmail.com>
2022-01-27 15:59:38 -0800

    Reviewed Problem 798.

--- Problem0798.h
+++ Problem0798.h
@@ -11,44 +11,39 @@
 using namespace std;
 
 class Problem0798 {
-public:
-    void increase(vector <vector<int>> &difference_matrix,
-                  const unsigned long &x1,
-                  const unsigned long &y1,
-                  const unsigned long &x2,
-                  const unsigned long &y2,
-                  const int &value) {
-        difference_matrix[x1][y1] += value;
-        difference_matrix[x2 + 1][y1] -= value;
-        difference_matrix[x1][y2 + 1] -= value;
-        difference_matrix[x2 + 1][y2 + 1] += value;
+private:
+    void increase(vector<vector<int>> &diff, const int x1, const int y1, const int x2, const int y2, const int v) {
+        diff[x1][y1] += v;
+        diff[x2 + 1][y1] -= v;
+        diff[x1][y2 + 1] -= v;
+        diff[x2 + 1][y2 + 1] += v;
     }
 
     int main() {
-        unsigned long n, m, q;
-        scanf("%ld%ld%ld", &n, &m, &q);
+        int n, m, q;
+        scanf("%d%d%d", &n, &m, &q);
 
         // 行数至少为n+2，列数至少为m+2；否则，触发越界异常！
-        vector <vector<int>> matrix(n + 2, vector<int>(m + 2, 0));
-        int t;
-        for (unsigned long i = 1; i <= n; ++i) {
-            for (unsigned long j = 1; j <= m; ++j) {
+        vector<vector<int>> matrix(n + 2, vector<int>(m + 2, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                int t;
                 scanf("%d", &t);
                 increase(matrix, i, j, i, j, t);
             }
         }
-        unsigned long x1, y1, x2, y2;
-        for (unsigned long i = 0; i < q; ++i) {
-            scanf("%ld%ld%ld%ld%d", &x1, &y1, &x2, &y2, &t);
+        for (int i = 0; i < q; ++i) {
+            int x1, y1, x2, y2, t;
+            scanf("%d%d%d%d%d", &x1, &y1, &x2, &y2, &t);
             increase(matrix, x1, y1, x2, y2, t);
         }
-        for (unsigned long i = 1; i <= n; ++i) {
-            for (unsigned long j = 1; j <= m; ++j) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
                 matrix[i][j] += matrix[i - 1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1];
             }
         }
-        for (unsigned long i = 1; i <= n; ++i) {
-            for (unsigned long j = 1; j <= m; ++j) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
                 printf("%d ", matrix[i][j]);
             }
             printf("\n");
