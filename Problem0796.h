ff442f2031226d33767980fa59c8e8a3ea257021
fengwei <virusformail@gmail.com>
2022-01-27 12:33:48 -0800

    Reviewed Problem 796.

--- Problem0796.h
+++ Problem0796.h
@@ -13,14 +13,14 @@ using namespace std;
 
 class Problem0796 {
 public:
-    vector<vector<int>> getPrefixSum(const vector<vector<int>> &matrix) {
-        vector<vector<int>> result(matrix.size() + 1, vector<int>(matrix[0].size() + 1, 0));
-        for (unsigned long i = 1; i < result.size(); ++i) {
-            for (unsigned long j = 1; j < result[0].size(); ++j) {
-                result[i][j] = result[i - 1][j] + result[i][j - 1] - result[i - 1][j - 1] + matrix[i - 1][j - 1];
+    vector<vector<int>> GetPrefixSum(const vector<vector<int>> &matrix) {
+        vector<vector<int>> res(matrix.size() + 1, vector<int>(matrix[0].size() + 1, 0));
+        for (unsigned long i = 1; i < res.size(); ++i) {
+            for (unsigned long j = 1; j < res[0].size(); ++j) {
+                res[i][j] = res[i - 1][j] + res[i][j - 1] - res[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
-        return result;
+        return res;
     }
 
     int main() {
@@ -32,12 +32,11 @@ public:
                 scanf("%d", &matrix[i][j]);
             }
         }
-        vector<vector<int>> prefixSum = getPrefixSum(matrix);
+        vector<vector<int>> prefix = GetPrefixSum(matrix);
         int x1, y1, x2, y2;
         for (int i = 0; i < q; ++i) {
             scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
-            printf("%d\n",
-                   prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1]);
+            printf("%d\n", prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1]);
         }
         return 0;
     }
