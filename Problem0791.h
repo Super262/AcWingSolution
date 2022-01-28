446c9e5f788e84ea86abf5cc58ef35bd927b2b7e
fengwei <virusformail@gmail.com>
2022-01-27 18:09:58 -0800

    Reviewed Problem 791.

--- Problem0791.h
+++ Problem0791.h
@@ -11,26 +11,26 @@
 using namespace std;
 
 class Problem0791 {
-public:
-    string addBigInt(string a, string b) {
+private:
+    string AddBigInt(string a, string b) {
         reverse(a.begin(), a.end());
         reverse(b.begin(), b.end());
         string result;
-        unsigned int num = 0;
-        for (unsigned long i = 0; i < a.size() || i < b.size(); ++i) {
+        int carry = 0;
+        for (int i = 0; i < a.size() || i < b.size(); ++i) {
             if (i < a.size()) {
-                num += a[i] - '0';
+                carry += a[i] - '0';
             }
             if (i < b.size()) {
-                num += b[i] - '0';
+                carry += b[i] - '0';
             }
-            result.push_back((char) ('0' + num % 10));
-            num /= 10;
+            result.push_back((char) ('0' + carry % 10));
+            carry /= 10;
         }
 
         // 千万别忘记最后的进位！
-        if (num) {
-            result.push_back((char) ('0' + num));
+        if (carry) {
+            result.push_back((char) ('0' + carry));
         }
         reverse(result.begin(), result.end());
         return result;
@@ -39,7 +39,7 @@ public:
     int main() {
         string a, b;
         cin >> a >> b;
-        cout << addBigInt(a, b) << endl;
+        cout << AddBigInt(a, b) << endl;
         return 0;
     }
 };
