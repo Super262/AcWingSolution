e5ec3e7cb16b1f2a10cdc21c6d607b9e8fb7c6d1
fengwei <virusformail@gmail.com>
2022-02-07 14:02:02 -0800

    Reviewed Problem 898.

--- Problem0898.h
+++ Problem0898.h
@@ -12,24 +12,20 @@ using namespace std;
 class Problem0898 {
 private:
     static const int N = 510;
-    int dp[N][N];
+    int dp[2][N];
     int matrix[N][N];
 
     int maxPath(const int n) {
-        for (int i = 0; i <= n; ++i) {
-            for (int j = 0; j <= n; ++j) {
-                dp[i][j] = -0x7f7f7f7f;
-            }
-        }
+        memset(dp, -0x3f, sizeof dp);
         dp[1][1] = matrix[1][1];
         for (int i = 2; i <= n; ++i) {
             for (int j = 1; j <= i; ++j) {
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + matrix[i][j];
+                dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[(i - 1) % 2][j - 1]) + matrix[i][j];
             }
         }
-        int result = -0x7f7f7f7f;
+        int result = -0x3f3f3f3f;
         for (int i = 1; i <= n; ++i) {
-            result = max(result, dp[n][i]);
+            result = max(result, dp[n % 2][i]);
         }
         return result;
     }
