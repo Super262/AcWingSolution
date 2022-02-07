8db485cc2c219ff3077e3adc1f789b9d8fed5bc6
fengwei <virusformail@gmail.com>
2022-02-07 14:53:04 -0800

    Reviewed Problem 1012.

--- Problem1012.h
+++ Problem1012.h
@@ -5,24 +5,24 @@
 #ifndef ACWINGSOLUTION_PROBLEM1012_H
 #define ACWINGSOLUTION_PROBLEM1012_H
 
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 class Problem1012 {
 private:
-    const int N = 5000;
-    pair<int, int> cityPairs[N];
-    int dp[N];
+    static const int N = 5000;
+    pair<int, int> city_pairs[N];
 
     int mostLines(const int n) {
-        sort(cityPairs, cityPairs + n);
+        int dp[n];
+        sort(city_pairs, city_pairs + n);
         int result = 0;
         for (int i = 0; i < n; ++i) {
             dp[i] = 1;
             for (int j = 0; j < i; ++j) {
-                if (cityPairs[j].second >= cityPairs[i].second) {
+                if (city_pairs[j].second >= city_pairs[i].second) {
                     continue;
                 }
                 dp[i] = max(dp[i], dp[j] + 1);
@@ -36,7 +36,7 @@ private:
         int n;
         scanf("%d", &n);
         for (int i = 0; i < n; ++i) {
-            scanf("%d%d", &cityPairs[i].first, &cityPairs[i].second);
+            scanf("%d%d", &city_pairs[i].first, &city_pairs[i].second);
         }
         printf("%d\n", mostLines(n));
         return 0;
