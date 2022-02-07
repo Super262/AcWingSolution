3f3eaccecb266e8b2c7f3abffc700223aefdea5f
fengwei <virusformail@gmail.com>
2022-02-07 15:18:47 -0800

    Reviewed Problem 896.

--- Problem0896.h
+++ Problem0896.h
@@ -36,12 +36,11 @@ private:
     int main() {
         int n;
         scanf("%d", &n);
-        auto arr = new int[n];
+        int arr[n];
         for (int i = 0; i < n; ++i) {
             scanf("%d", &arr[i]);
         }
         printf("%d\n", longestIS(arr, n));
-        delete[] arr;
         return 0;
     }
 };
