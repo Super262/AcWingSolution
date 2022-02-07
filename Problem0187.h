4312a9627adf395a768289d9974cc49c30e9464f
fengwei <virusformail@gmail.com>
2022-02-07 15:59:14 -0800

    Reviewed Problem 187.

--- Problem0187.h
+++ Problem0187.h
@@ -12,60 +12,60 @@ using namespace std;
 class Problem0187 {
 private:
     static const int N = 50;
-    int lis_head[N];
-    int lds_head[N];
+    int lds_tail[N];
+    int lis_tail[N];
     int height[N];
 
-    void dfs(const int n, const int idx, const int lis_cnt, const int lds_cnt, int &result) {
+    void dfs(const int n, const int idx, const int lds_cnt, const int lis_cnt, int &result) {
         // 剪枝：当前结果比已有结果差，直接返回
-        if (lis_cnt + lds_cnt >= result) {
+        if (lds_cnt + lis_cnt >= result) {
             return;
         }
         if (idx == n) {
-            result = lis_cnt + lds_cnt;
+            result = lds_cnt + lis_cnt;
             return;
         }
-        // 尝试在所有的LIS中寻找插入位置或建立新的LIS
+        // 尝试在所有的LDS中寻找插入位置或建立新的LDS
         int k = -1;
-        for (int i = 0; i < lis_cnt; ++i) {
-            if (lis_head[i] <= height[idx]) {
+        for (int i = 0; i < lds_cnt; ++i) {
+            if (lds_tail[i] <= height[idx]) {
                 continue;
             }
-            if (k == -1 || lis_head[i] < lis_head[k]) {
+            if (k == -1 || lds_tail[i] < lds_tail[k]) {
                 k = i;
             }
         }
         if (k == -1) {
-            auto t = lis_head[lis_cnt];
-            lis_head[lis_cnt] = height[idx];
-            dfs(n, idx + 1, lis_cnt + 1, lds_cnt, result);
-            lis_head[lis_cnt] = t;
+            auto t = lds_tail[lds_cnt];
+            lds_tail[lds_cnt] = height[idx];
+            dfs(n, idx + 1, lds_cnt + 1, lis_cnt, result);
+            lds_tail[lds_cnt] = t;
         } else {
-            auto t = lis_head[k];
-            lis_head[k] = height[idx];
-            dfs(n, idx + 1, lis_cnt, lds_cnt, result);
-            lis_head[k] = t;
+            auto t = lds_tail[k];
+            lds_tail[k] = height[idx];
+            dfs(n, idx + 1, lds_cnt, lis_cnt, result);
+            lds_tail[k] = t;
         }
         // 尝试在所有的LDS中寻找插入位置或建立新的LDS
         k = -1;
-        for (int i = 0; i < lds_cnt; ++i) {
-            if (lds_head[i] >= height[idx]) {
+        for (int i = 0; i < lis_cnt; ++i) {
+            if (lis_tail[i] >= height[idx]) {
                 continue;
             }
-            if (k == -1 || lds_head[i] > lds_head[k]) {
+            if (k == -1 || lis_tail[i] > lis_tail[k]) {
                 k = i;
             }
         }
         if (k == -1) {
-            auto t = lds_head[lds_cnt];
-            lds_head[lds_cnt] = height[idx];
-            dfs(n, idx + 1, lis_cnt, lds_cnt + 1, result);
-            lds_head[lds_cnt] = t;
+            auto t = lis_tail[lis_cnt];
+            lis_tail[lis_cnt] = height[idx];
+            dfs(n, idx + 1, lds_cnt, lis_cnt + 1, result);
+            lis_tail[lis_cnt] = t;
         } else {
-            auto t = lds_head[k];
-            lds_head[k] = height[idx];
-            dfs(n, idx + 1, lis_cnt, lds_cnt, result);
-            lds_head[k] = t;
+            auto t = lis_tail[k];
+            lis_tail[k] = height[idx];
+            dfs(n, idx + 1, lds_cnt, lis_cnt, result);
+            lis_tail[k] = t;
         }
     }
 
