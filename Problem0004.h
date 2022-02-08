a3b549838ce472804fb16490dcf07dfc268d48ae
fengwei <virusformail@gmail.com>
2022-02-07 18:22:22 -0800

    Reviewed Problem 4.

--- Problem0004.h
+++ Problem0004.h
@@ -17,20 +17,20 @@ private:
         int num;
     };
 
-    const int N = 110;
-    const int M = 110;
+    static const int N = 110;
+    static const int M = 110;
     Item items[N];
     int dp[N][M];
 
-    int knapsack(const int itemsNum, const int packVolume) {
-        for (int i = 1; i <= itemsNum; ++i) {
-            for (int j = 1; j <= packVolume; ++j) {
+    int knapsack(const int n, const int v) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= v; ++j) {
                 for (int k = 0; k <= items[i].num && k * items[i].size <= j; ++k) {
                     dp[i][j] = max(dp[i][j], dp[i - 1][j - k * items[i].size] + k * items[i].value);
                 }
             }
         }
-        return dp[itemsNum][packVolume];
+        return dp[n][v];
     }
 
     int main() {
