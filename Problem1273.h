378f4e22c13f82f3a7bc7868fdc39af7c876e28c
fengwei <virusformail@gmail.com>
2022-02-10 16:12:51 -0800

    Finished Problem 1273.

--- Problem1273.h
+++ Problem1273.h
@@ -11,44 +11,47 @@
 
 using namespace std;
 
-static const int N = 200010, M = 18;
+class Problem1273 {
+private:
+    static const int N = 200010, M = 18;
 
-int dp[N][M];  // dp[i][j]：从i开始长度是2^j的区间中的最大值
+    int dp[N][M];  // dp[i][j]：从i开始长度是2^j的区间中的最大值
 
-void init(const int nums[], int n) {
-    for (int j = 0; j < M; ++j) {
-        for (int i = 1; i + (1 << j) - 1 <= n; ++i) {
-            if (j) {
-                dp[i][j] = max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
-            } else {
-                dp[i][j] = nums[i];
+    void init(const int nums[], int n) {
+        for (int j = 0; j < M; ++j) {
+            for (int i = 1; i + (1 << j) - 1 <= n; ++i) {
+                if (j) {
+                    dp[i][j] = max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+                } else {
+                    dp[i][j] = nums[i];
+                }
             }
         }
     }
-}
 
-int query(int l, int r) {
-    auto length = r - l + 1;
-    auto k = (int) (log(length) / log(2));
-    return max(dp[l][k], dp[r - (1 << k) + 1][k]);
-}
+    int query(int l, int r) {
+        auto length = r - l + 1;
+        auto k = (int) (log(length) / log(2));
+        return max(dp[l][k], dp[r - (1 << k) + 1][k]);
+    }
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    int nums[n + 1];
-    for (int i = 1; i <= n; ++i) {
-        scanf("%d", &nums[i]);
+    int main() {
+        int n;
+        scanf("%d", &n);
+        int nums[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            scanf("%d", &nums[i]);
+        }
+        init(nums, n);
+        int m;
+        scanf("%d", &m);
+        while (m--) {
+            int l, r;
+            scanf("%d%d", &l, &r);
+            printf("%d\n", query(l, r));
+        }
+        return 0;
     }
-    init(nums, n);
-    int m;
-    scanf("%d", &m);
-    while (m--) {
-        int l, r;
-        scanf("%d%d", &l, &r);
-        printf("%d\n", query(l, r));
-    }
-    return 0;
-}
+};
 
 #endif //ACWINGSOLUTION_PROBLEM1273_H
