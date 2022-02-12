b1d2623dfd50b6fbecb1f42edf0e488c506da42f
fengwei <virusformail@gmail.com>
2022-02-11 16:27:14 -0800

    Reviewed Problem 1049.

--- Problem1049.h
+++ Problem1049.h
@@ -10,8 +10,9 @@
 using namespace std;
 
 class Problem1049 {
+    // 状态机：dp[i][0]表示不盗窃i号房的收益，dp[i][1]表示盗窃i号房的收益
 private:
-    int stateMachine(const int items[], const int n) {  // 利用状态机，把每个dp[i]分解为2个状态
+    int stateMachine(const int items[], const int n) {
         int dp[2][2];
         dp[0][0] = 0;
         dp[0][1] = items[0];
