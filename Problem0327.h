0bde906ae0620ef05628c97f30e3641cb31ba00f
fengwei <virusformail@gmail.com>
2022-02-12 13:13:58 -0800

    Reviewed Problem 327.

--- Problem0327.h
+++ Problem0327.h
@@ -7,12 +7,13 @@
 
 #include <vector>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 class Problem0327 {
     // https://www.acwing.com/solution/content/17569/
-public:
+private:
     bool noAdjacentOnes(const int s, const int N) {
         // N < 2，此函数应仍能正确计算出结果，而不是直接返回true：(offset + 1) 可以大于 (N - 1)
         for (int offset = 0; offset < N; ++offset) {
@@ -24,52 +25,54 @@ public:
     }
 
     vector<vector<int>> getPossiblePrev(const vector<int> &states) {
-        vector<vector<int>> prevStates(states.size(), vector<int>());
+        vector<vector<int>> prev_states(states.size(), vector<int>());
         for (int i = 0; i < states.size(); ++i) {
             for (int j = 0; j < states.size(); ++j) {
                 if (states[i] & states[j]) {
                     continue;
                 }
-                prevStates[i].emplace_back(j);
+                prev_states[i].emplace_back(j);
             }
         }
-        return prevStates;
+        return prev_states;
     }
 
-    int solutionsNum(const int M, const int N, const vector<int> &graph) {
-        const int MOD = 100000000;
+    int solutionsNum(const int m, const int n, const int graph[]) {
+        const int MOD = 1e8;
         vector<int> states;
-        for (int s = 0; s < (1 << N); ++s) {
-            if (noAdjacentOnes(s, N)) {
+        for (int s = 0; s < (1 << n); ++s) {
+            if (noAdjacentOnes(s, n)) {
                 states.emplace_back(s);
             }
         }
-        vector<vector<int>> prevStates = getPossiblePrev(states);
+        vector<vector<int>> prev_states = getPossiblePrev(states);
 
         // dp[i][s] 表示前i行摆放完成、第i行摆放方案为s（0是空位，1是）的方案数
-        vector<vector<int>> dp(M + 2, vector<int>(1 << N, 0));
+        int dp[m + 2][1 << n];
+        memset(dp, 0, sizeof dp);
         dp[0][0] = 1;
 
-        for (int i = 1; i <= M + 1; ++i) {
-            for (int k = 0; k < states.size(); ++k) {
+        for (int i = 1; i <= m + 1; ++i) {
+            for (int k = 0; k < (int) states.size(); ++k) {
                 if (states[k] & graph[i]) {
                     continue;
                 }
-                for (int prevStateIdx : prevStates[k]) {
-                    dp[i][states[k]] = (int) (((long long) dp[i][states[k]] + dp[i - 1][states[prevStateIdx]]) % MOD);
+                for (const auto &prev_idx: prev_states[k]) {
+                    dp[i][states[k]] = (int) (((long long) dp[i][states[k]] + dp[i - 1][states[prev_idx]]) % MOD);
                 }
             }
         }
-        return dp[M + 1][0];
+        return dp[m + 1][0];
     }
 
     int main() {
-        int M, N;
-        scanf("%d%d", &M, &N);
-        vector<int> graph(M + 2, 0);
-        for (int i = 1; i <= M; ++i) {
+        int m, n;
+        scanf("%d%d", &m, &n);
+        int graph[m + 2];
+        memset(graph, 0, sizeof graph);
+        for (int i = 1; i <= m; ++i) {
             int t;
-            for (int j = 0; j < N; ++j) {
+            for (int j = 0; j < n; ++j) {
                 graph[i] *= 2;
                 scanf("%d", &t);
                 if (t == 0) {
@@ -77,7 +80,7 @@ public:
                 }
             }
         }
-        printf("%d\n", solutionsNum(M, N, graph));
+        printf("%d\n", solutionsNum(m, n, graph));
         return 0;
     }
 };
