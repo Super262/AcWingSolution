2422a7b743669d6a0815b87104a83536f6de8936
fengwei <virusformail@gmail.com>
2022-02-13 17:38:52 -0800

    Reviewed Problems 1085.

--- Problem1085.h
+++ Problem1085.h
@@ -11,8 +11,8 @@
 using namespace std;
 
 class Problem1085 {
-public:
-    const int N = 10;
+private:
+    static const int N = 10;
     int f[N + 1][10];
 
     void init() {
@@ -37,29 +37,29 @@ public:
         }
     }
 
-    int helper(int a) {
+    int dp(int a) {
         if (a == 0) {
             return 1;
         }
-        vector<int> nums;
+        vector<int> digits;
         while (a) {
-            nums.emplace_back(a % 10);
+            digits.emplace_back(a % 10);
             a /= 10;
         }
         int result = 0;
-        int prevDigit = 0;
-        for (int i = (int) nums.size() - 1; i >= 0; --i) {
-            int x = nums[i];
+        int prev = 0;
+        for (auto i = (int) digits.size() - 1; i >= 0; --i) {
+            const auto &x = digits[i];
             for (int j = 0; j < x; ++j) {
-                if (j == 4 || (prevDigit == 6 && j == 2)) {
+                if (j == 4 || (prev == 6 && j == 2)) {
                     continue;
                 }
                 result += f[i + 1][j];
             }
-            if (x == 4 || (prevDigit == 6 && x == 2)) {
+            if (x == 4 || (prev == 6 && x == 2)) {
                 break;
             }
-            prevDigit = x;
+            prev = x;
             if (i == 0) {
                 ++result;
             }
@@ -75,7 +75,7 @@ public:
             if (n == 0 || m == 0) {
                 break;
             }
-            printf("%d\n", helper(m) - helper(n - 1));
+            printf("%d\n", dp(m) - dp(n - 1));
         }
         return 0;
     }
