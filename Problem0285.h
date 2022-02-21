3f92861c81334c8129d6bef3b073d73300bb3e01
fengwei <virusformail@gmail.com>
2022-02-21 00:50:58 -0800

    Reviewed Problems 285.

--- Problem0285.h
+++ Problem0285.h
@@ -7,58 +7,46 @@
 
 #include <cstring>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Problem0285 {
 private:
-    const int N = 6010;
     // dp[u][0]：所有从以u为根的子树中选择，但不选择u的方案
     // dp[u][1]：所有从以u为根的子树中选择，选择u的方案
-    int dp[N][2];
-    int headIndex[N];
-    int nextIndex[2 * N];
-    int vertexValue[2 * N];
-    bool hasFather[N];
-    int happiness[N];
 
-    void addEdge(const int a, const int b, int &idx) {
-        hasFather[b] = true;
-        vertexValue[idx] = b;
-        nextIndex[idx] = headIndex[a];
-        headIndex[a] = idx;
-        ++idx;
-    }
-
-    void dfs(const int root) {
-        dp[root][1] = happiness[root];
-        for (auto idx = headIndex[root]; idx != -1; idx = nextIndex[idx]) {
-            auto childV = vertexValue[idx];
-            dfs(childV);
-            dp[root][1] += dp[childV][0];
-            dp[root][0] += max(dp[childV][0], dp[childV][1]);
+    void dfs(const int u, const vector<vector<int>> &graph, vector<vector<int>> &dp, const int happiness[]) {
+        dp[u][1] = happiness[u];
+        for (auto v: graph[u]) {
+            dfs(v, graph, dp, happiness);
+            dp[u][1] += dp[v][0];
+            dp[u][0] += max(dp[v][0], dp[v][1]);
         }
     }
 
     int main() {
-        memset(headIndex, -1, sizeof headIndex);
-        memset(nextIndex, -1, sizeof nextIndex);
         int n;
         scanf("%d", &n);
+        int happiness[n + 1];
         for (int i = 1; i <= n; ++i) {
             scanf("%d", &happiness[i]);
         }
-        int idx = 0;
+        vector<vector<int>> graph(n + 1);
+        bool has_father[n + 1];
+        memset(has_father, 0, sizeof has_father);
         for (int i = 0; i < n - 1; ++i) {
             int a, b;
             scanf("%d%d", &a, &b);
-            addEdge(b, a, idx);
+            graph[b].emplace_back(a);
+            has_father[a] = true;
         }
         int root = 1;
-        while (hasFather[root]) {
+        while (has_father[root]) {
             ++root;
         }
-        dfs(root);
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        dfs(root, graph, dp, happiness);
         printf("%d\n", max(dp[root][0], dp[root][1]));
         return 0;
     }
