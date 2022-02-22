ebda9cb68f3873d42e3fe12cc2b173e822e89932
fengwei <virusformail@gmail.com>
2022-02-22 14:44:31 -0800

    Reviewed Problems 841.

--- Problem0841.h
+++ Problem0841.h
@@ -12,23 +12,23 @@ using namespace std;
 class Problem0841 {
     // 我们不能把某个前缀映射成0。否则，不同的字符串（AA和AAAA）可能有相同的哈希值。
 private:
-    const int N = 100010;
+    static const int N = 100010;
     const int BASE = 13331;  // BASE 取 131 或 13331
     unsigned long long power[N];
-    unsigned long long prefixHashValue[N];
+    unsigned long long prefix[N];
     char str[N];
 
     void buildHashTable() {
         power[0] = 1;
-        prefixHashValue[0] = 0;
+        prefix[0] = 0;
         for (int i = 1; str[i - 1]; ++i) {
             power[i] = power[i - 1] * BASE;
-            prefixHashValue[i] = prefixHashValue[i - 1] * BASE + str[i - 1];  // 注意，i从1开始，表示字符序号
+            prefix[i] = prefix[i - 1] * BASE + str[i - 1];  // 注意，i从1开始，表示字符序号
         }
     }
 
-    unsigned long long hashValue(const int start, const int end) {
-        return prefixHashValue[end] - prefixHashValue[start - 1] * power[end - start + 1];
+    unsigned long long hashValue(const int l, const int r) {
+        return prefix[r] - prefix[l - 1] * power[r - l + 1];
     }
 
     int main() {
