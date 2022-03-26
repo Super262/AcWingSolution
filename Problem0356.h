f7c8e0924e248bc57596283d36a4263c2921f142
fengwei <virusformail@gmail.com>
2022-03-26 16:43:22 -0700

    Reviewed Problem 356.

--- Problem0356.h
+++ Problem0356.h
@@ -13,6 +13,10 @@
 using namespace std;
 
 class Problem0356 {
+    // 用LCA加速求解次小生成树
+    // f[i][j]：树上编号为i的节点向上移动2^j步后抵达的节点
+    // d1[i][j]：在树上，编号为i的节点到编号为(i+2^j)的节点的路径上的最小边权
+    // d2[i][j]：在树上，编号为i的节点到编号为(i+2^j)的节点的路径上的次小边权
 private:
     static const int H = 16;
     static const int INF = 0x3f3f3f3f;
@@ -26,24 +30,24 @@ private:
         }
     };
 
-    int FindRoot(const int x, int parent[]) {
+    int findRoot(const int x, int parent[]) {
         if (x != parent[x]) {
-            parent[x] = FindRoot(parent[x], parent);
+            parent[x] = findRoot(parent[x], parent);
         }
         return parent[x];
     }
 
-    long long Kruskal(Edge edges[], const int m, const int n) {
+    long long kruskal(Edge edges[], const int m, const int n) {
         int parent[n + 1];
         for (int v = 0; v <= n; ++v) {
             parent[v] = v;
         }
-        long long result = 0;
         sort(edges, edges + m);
+        long long result = 0;
         for (int i = 0; i < m; ++i) {
             auto &e = edges[i];
-            auto pu = FindRoot(e.u, parent);
-            auto pv = FindRoot(e.v, parent);
+            auto pu = findRoot(e.u, parent);
+            auto pv = findRoot(e.v, parent);
             if (pu == pv) {
                 continue;
             }
@@ -54,7 +58,7 @@ private:
         return result;
     }
 
-    vector<vector<pair<int, int>>> BuildGraph(const int n, const Edge edges[], const int m) {
+    vector<vector<pair<int, int>>> buildGraph(const int n, const Edge edges[], const int m) {
         vector<vector<pair<int, int>>> graph(n + 1);
         for (int i = 0; i < m; ++i) {
             if (!edges[i].used) {
@@ -69,9 +73,9 @@ private:
         return graph;
     }
 
-    void Bfs(const vector<vector<pair<int, int>>> &graph,
+    void bfs(const vector<vector<pair<int, int>>> &graph,
              int depth[],
-             int father[][H + 1],
+             int f[][H + 1],
              int d1[][H + 1],
              int d2[][H + 1]) {
         depth[0] = 0;  // 哨兵
@@ -89,12 +93,12 @@ private:
                 }
                 depth[nv] = depth[x] + 1;
                 q.emplace(nv);
-                father[nv][0] = x;
+                f[nv][0] = x;
                 d1[nv][0] = nd;
                 d2[nv][0] = -INF;
                 for (int k = 1; k <= H; ++k) {
-                    auto ancestor = father[nv][k - 1];
-                    father[nv][k] = father[ancestor][k - 1];
+                    auto ancestor = f[nv][k - 1];
+                    f[nv][k] = f[ancestor][k - 1];
                     int temp[4] = {d1[nv][k - 1], d1[ancestor][k - 1], d2[nv][k - 1], d2[ancestor][k - 1]};
                     d1[nv][k] = -INF;
                     d2[nv][k] = -INF;
@@ -111,7 +115,7 @@ private:
         }
     }
 
-    int Lca(const Edge &e,
+    int lca(const Edge &e,
             const vector<vector<pair<int, int>>> &graph,
             const int depth[],
             int father[][H + 1],
@@ -122,14 +126,14 @@ private:
         if (depth[a] < depth[b]) {
             swap(a, b);
         }
-        static int temp[300010];
+        static int q[300010];  // 存储所有可能被选中的边权（这里不能使用queue，会超时；不忘记static）
         int tt = 0;
         for (int k = H; k >= 0; --k) {
             if (depth[father[a][k]] < depth[b]) {
                 continue;
             }
-            temp[tt++] = d1[a][k];
-            temp[tt++] = d2[a][k];
+            q[tt++] = d1[a][k];
+            q[tt++] = d2[a][k];
             a = father[a][k];
         }
         if (a != b) {
@@ -137,20 +141,20 @@ private:
                 if (father[a][k] == father[b][k]) {
                     continue;
                 }
-                temp[tt++] = d1[a][k];
-                temp[tt++] = d2[a][k];
-                temp[tt++] = d1[b][k];
-                temp[tt++] = d2[b][k];
+                q[tt++] = d1[a][k];
+                q[tt++] = d2[a][k];
+                q[tt++] = d1[b][k];
+                q[tt++] = d2[b][k];
                 a = father[a][k];
                 b = father[b][k];
             }
-            temp[tt++] = d1[a][0];
-            temp[tt++] = d1[b][0];
+            q[tt++] = d1[a][0];
+            q[tt++] = d1[b][0];
         }
         int t1 = -INF;
         int t2 = -INF;
         for (int i = 0; i < tt; ++i) {
-            auto x = temp[i];
+            auto x = q[i];
             if (x > t1) {
                 t2 = t1;
                 t1 = x;
@@ -170,14 +174,17 @@ private:
     int main() {
         int n, m;
         scanf("%d%d", &n, &m);
+
         Edge edges[m];
         for (int i = 0; i < m; ++i) {
             int x, y, z;
             scanf("%d%d%d", &x, &y, &z);
             edges[i] = {x, y, z, false};
         }
-        auto mst_sum = Kruskal(edges, m, n);
-        auto graph = BuildGraph(n, edges, m);
+
+        auto mst_sum = kruskal(edges, m, n);
+        auto graph = buildGraph(n, edges, m);
+
         int depth[n + 1];
         int father[n + 1][H + 1];
         int d1[n + 1][H + 1];
@@ -186,15 +193,18 @@ private:
         memset(father, 0, sizeof father);
         memset(d1, 0, sizeof d1);
         memset(d2, 0, sizeof d2);
-        Bfs(graph, depth, father, d1, d2);
+
+        bfs(graph, depth, father, d1, d2);
+
         long long result = 1e18;  // 无效值一定要足够大
         for (int i = 0; i < m; ++i) {
             if (edges[i].used) {
                 continue;
             }
-            result = min(result, mst_sum + Lca(edges[i], graph, depth, father, d1, d2));
+            result = min(result, mst_sum + lca(edges[i], graph, depth, father, d1, d2));
         }
         printf("%lld\n", result);
+
         return 0;
     }
 };
