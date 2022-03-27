2938c96715c88bc2968a34aac54286152cf35c6f
fengwei <virusformail@gmail.com>
2022-03-27 01:40:48 -0700

    Reviewed Problem 300.

--- Problem0300.h
+++ Problem0300.h
@@ -12,37 +12,36 @@ using namespace std;
 
 class Problem0300 {
     // https://www.acwing.com/solution/content/13391/
-    // f[i]=min{ (f[j] + sumT[i] * (sumC[i] - sumC[j]) + S * (sumC[N] - sumC[j]) }
-public:
-    int minCost(const int *sumT, const int *sumC, const int N, const int s) {
-        auto dp = new int[N + 1];
-        memset(dp, 0x7f, sizeof(int) * (N + 1));
-        dp[0] = 0;
-        for (int i = 1; i <= N; ++i) {
+    // f[i]：将前i个任务分成若干批执行的最小费用（添加启动时间的影响S到后续所有任务）
+    // f[i] = min{f[i], (f[j] + sumT[i] * (sumC[i] - sumC[j]) + S * (sumC[N] - sumC[j])}
+private:
+    long long minCost(const int t_prefix[], const int c_prefix[], const int n, const int s) {
+        long long f[n + 1];
+        memset(f, 0x3f, sizeof f);
+        f[0] = 0;
+        for (int i = 1; i <= n; ++i) {
             for (int j = 0; j < i; ++j) {
-                dp[i] = min(dp[i], dp[j] + sumT[i] * (sumC[i] - sumC[j]) + s * (sumC[N] - sumC[j]));
+                f[i] = min(f[i],
+                           f[j] + (long long) t_prefix[i] * (c_prefix[i] - c_prefix[j]) +
+                           (long long) s * (c_prefix[n] - c_prefix[j]));
             }
         }
-        int result = dp[N];
-        delete[] dp;
-        return result;
+        return f[n];
     }
 
     int main() {
         int n, s;
         scanf("%d%d", &n, &s);
-        auto sumC = new int[n + 1];
-        auto sumT = new int[n + 1];
-        memset(sumC, 0, sizeof(int) * (n + 1));
-        memset(sumT, 0, sizeof(int) * (n + 1));
+        int c_sum[n + 1];
+        int t_sum[n + 1];
+        memset(c_sum, 0, sizeof c_sum);
+        memset(t_sum, 0, sizeof t_sum);
         for (int i = 1; i <= n; ++i) {
-            scanf("%d%d", &sumT[i], &sumC[i]);
-            sumT[i] += sumT[i - 1];
-            sumC[i] += sumC[i - 1];
+            scanf("%d%d", &t_sum[i], &c_sum[i]);
+            t_sum[i] += t_sum[i - 1];
+            c_sum[i] += c_sum[i - 1];
         }
-        printf("%d\n", minCost(sumT, sumC, n, s));
-        delete[] sumT;
-        delete[] sumC;
+        printf("%lld\n", minCost(t_sum, c_sum, n, s));
         return 0;
     }
 };
