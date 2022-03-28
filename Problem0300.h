9ae5f304aa9b69a321d7783f1307cb8a5fa6c4dd
fengwei <virusformail@gmail.com>
2022-03-27 17:18:01 -0700

    Reviewed Problem 300.

--- Problem0300.h
+++ Problem0300.h
@@ -13,17 +13,16 @@ using namespace std;
 class Problem0300 {
     // https://www.acwing.com/solution/content/13391/
     // f[i]：将前i个任务分成若干批执行的最小费用（添加启动时间的影响S到后续所有任务）
-    // f[i] = min{f[i], (f[j] + sumT[i] * (sumC[i] - sumC[j]) + S * (sumC[N] - sumC[j])}
+    // f[i] = min{f[i], (f[j] + st[i] * (sc[i] - sc[j]) + S * (sc[N] - sc[j])}
+    // sc：c的前缀和；st：t的前缀和
 private:
-    long long minCost(const int t_prefix[], const int c_prefix[], const int n, const int s) {
+    long long minCost(const int st[], const int sc[], const int n, const int s) {
         long long f[n + 1];
         memset(f, 0x3f, sizeof f);
         f[0] = 0;
         for (int i = 1; i <= n; ++i) {
             for (int j = 0; j < i; ++j) {
-                f[i] = min(f[i],
-                           f[j] + (long long) t_prefix[i] * (c_prefix[i] - c_prefix[j]) +
-                           (long long) s * (c_prefix[n] - c_prefix[j]));
+                f[i] = min(f[i], f[j] + (long long) st[i] * (sc[i] - sc[j]) + (long long) s * (sc[n] - sc[j]));
             }
         }
         return f[n];
@@ -32,16 +31,16 @@ private:
     int main() {
         int n, s;
         scanf("%d%d", &n, &s);
-        int c_sum[n + 1];
-        int t_sum[n + 1];
-        memset(c_sum, 0, sizeof c_sum);
-        memset(t_sum, 0, sizeof t_sum);
+        int sc[n + 1];
+        int st[n + 1];
+        memset(sc, 0, sizeof sc);
+        memset(st, 0, sizeof st);
         for (int i = 1; i <= n; ++i) {
-            scanf("%d%d", &t_sum[i], &c_sum[i]);
-            t_sum[i] += t_sum[i - 1];
-            c_sum[i] += c_sum[i - 1];
+            scanf("%d%d", &st[i], &sc[i]);
+            st[i] += st[i - 1];
+            sc[i] += sc[i - 1];
         }
-        printf("%lld\n", minCost(t_sum, c_sum, n, s));
+        printf("%lld\n", minCost(st, sc, n, s));
         return 0;
     }
 };
