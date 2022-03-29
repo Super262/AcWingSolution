0ed1e4b3bc5969ac75319cb4e745b5da00ebe621
fengwei <virusformail@gmail.com>
2022-03-28 17:13:23 -0700

    Reviewed Problem 253.

--- Problem0253.h
+++ Problem0253.h
@@ -6,23 +6,22 @@
 #define ACWINGSOLUTION_PROBLEM0253_H
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 class Problem0253 {
 private:
     struct Node {
-        int l_child;
-        int r_child;
-        int key;
-        int val;
-        int cnt;
-        int size;
+        int l;
+        int r;
+        int key;  // 用于BST
+        int val;  // 用于大根堆
+        int cnt;  // 重复数字的频率
+        int size; // 当前子树包含的数字个数
 
         Node() {
-            l_child = 0;
-            r_child = 0;
+            l = 0;
+            r = 0;
             key = 0;
             val = 0;
             cnt = 0;
@@ -32,7 +31,7 @@ private:
 
     const int INF = 0x3f3f3f3f;
 
-    int GetNode(int key, vector<Node> &tr, int &idx) {
+    int createNode(int key, Node tr[], int &idx) {
         ++idx;
         tr[idx].key = key;
         tr[idx].val = (int) random();
@@ -41,148 +40,148 @@ private:
         return idx;
     }
 
-    void PushUp(int p, vector<Node> &tr) {
-        tr[p].size = tr[tr[p].l_child].size + tr[tr[p].r_child].size + tr[p].cnt;
+    void pushUp(int p, Node tr[]) {
+        tr[p].size = tr[tr[p].l].size + tr[tr[p].r].size + tr[p].cnt;
     }
 
-    int BuildTree(vector<Node> &tr, int &idx) {
-        auto root = GetNode(-INF, tr, idx);
-        tr[root].r_child = GetNode(INF, tr, idx);
-        PushUp(root, tr);
+    int buildTree(Node tr[], int &idx) {
+        auto root = createNode(-INF, tr, idx);  // 两个"哨兵"：INF和-INF
+        tr[root].r = createNode(INF, tr, idx);
+        pushUp(root, tr);
         return root;
     }
 
-    void Zig(int &p, vector<Node> &tr) {  // 右旋
-        auto q = tr[p].l_child;
-        tr[p].l_child = tr[q].r_child;
-        tr[q].r_child = p;
+    void zig(int &p, Node tr[]) {  // 右旋
+        auto q = tr[p].l;
+        tr[p].l = tr[q].r;
+        tr[q].r = p;
         p = q;
-        PushUp(tr[p].r_child, tr);
-        PushUp(p, tr);
+        pushUp(tr[p].r, tr);
+        pushUp(p, tr);
     }
 
-    void Zag(int &p, vector<Node> &tr) {  // 左旋
-        auto q = tr[p].r_child;
-        tr[p].r_child = tr[q].l_child;
-        tr[q].l_child = p;
+    void zag(int &p, Node tr[]) {  // 左旋
+        auto q = tr[p].r;
+        tr[p].r = tr[q].l;
+        tr[q].l = p;
         p = q;
-        PushUp(tr[p].l_child, tr);
-        PushUp(p, tr);
+        pushUp(tr[p].l, tr);
+        pushUp(p, tr);
     }
 
-    void Insert(int &p, int key, vector<Node> &tr, int &idx) {
+    void insert(int &p, int key, Node tr[], int &idx) {
         if (!p) {
-            p = GetNode(key, tr, idx);
+            p = createNode(key, tr, idx);
         } else if (tr[p].key == key) {
             ++tr[p].cnt;
         } else if (tr[p].key > key) {
-            Insert(tr[p].l_child, key, tr, idx);
-            if (tr[tr[p].l_child].val > tr[p].val) {
-                Zig(p, tr);
+            insert(tr[p].l, key, tr, idx);
+            if (tr[tr[p].l].val > tr[p].val) {
+                zig(p, tr);
             }
         } else {
-            Insert(tr[p].r_child, key, tr, idx);
-            if (tr[tr[p].r_child].val > tr[p].val) {
-                Zag(p, tr);
+            insert(tr[p].r, key, tr, idx);
+            if (tr[tr[p].r].val > tr[p].val) {
+                zag(p, tr);
             }
         }
-        PushUp(p, tr);
+        pushUp(p, tr);
     }
 
-    void Erase(int &p, int key, vector<Node> &tr) {
+    void erase(int &p, int key, Node tr[]) {
         if (!p) {
             return;
         }
         if (tr[p].key == key) {
             if (tr[p].cnt > 1) {
                 --tr[p].cnt;
-            } else if (tr[p].l_child || tr[p].r_child) {
-                if (!tr[p].r_child || tr[tr[p].l_child].val > tr[tr[p].r_child].val) {
-                    Zig(p, tr);
-                    Erase(tr[p].r_child, key, tr);
+            } else if (tr[p].l || tr[p].r) {
+                if (!tr[p].r || tr[tr[p].l].val > tr[tr[p].r].val) {
+                    zig(p, tr);
+                    erase(tr[p].r, key, tr);
                 } else {
-                    Zag(p, tr);
-                    Erase(tr[p].l_child, key, tr);
+                    zag(p, tr);
+                    erase(tr[p].l, key, tr);
                 }
             } else {
                 p = 0;
             }
         } else if (tr[p].key > key) {
-            Erase(tr[p].l_child, key, tr);
+            erase(tr[p].l, key, tr);
         } else {
-            Erase(tr[p].r_child, key, tr);
+            erase(tr[p].r, key, tr);
         }
-        PushUp(p, tr);
+        pushUp(p, tr);
     }
 
-    int GetRankByKey(int p, int key, vector<Node> &tr) {
+    int getRankByKey(int p, int key, Node tr[]) {
         if (!p) {
             return 0;
         }
         if (tr[p].key == key) {
-            return tr[tr[p].l_child].size + 1;
+            return tr[tr[p].l].size + 1;
         }
         if (tr[p].key > key) {
-            return GetRankByKey(tr[p].l_child, key, tr);
+            return getRankByKey(tr[p].l, key, tr);
         }
-        return tr[tr[p].l_child].size + tr[p].cnt + GetRankByKey(tr[p].r_child, key, tr);
+        return tr[tr[p].l].size + tr[p].cnt + getRankByKey(tr[p].r, key, tr);
     }
 
-    int GetKeyByRank(int p, int rank, vector<Node> &tr) {
+    int getKeyByRank(int p, int rank, Node tr[]) {
         if (!p) {
             return INF;
         }
-        if (tr[tr[p].l_child].size >= rank) {
-            return GetKeyByRank(tr[p].l_child, rank, tr);
+        if (tr[tr[p].l].size >= rank) {
+            return getKeyByRank(tr[p].l, rank, tr);
         }
-        if (tr[tr[p].l_child].size + tr[p].cnt >= rank) {
+        if (tr[tr[p].l].size + tr[p].cnt >= rank) {
             return tr[p].key;
         }
-        return GetKeyByRank(tr[p].r_child, rank - tr[tr[p].l_child].size - tr[p].cnt, tr);
+        return getKeyByRank(tr[p].r, rank - tr[tr[p].l].size - tr[p].cnt, tr);
     }
 
-    int GetPrev(int p, int key, vector<Node> &tr) {
+    int getPrev(int p, int key, Node tr[]) {
         if (!p) {
             return -INF;
         }
         if (tr[p].key >= key) {
-            return GetPrev(tr[p].l_child, key, tr);
+            return getPrev(tr[p].l, key, tr);
         }
-        return max(tr[p].key, GetPrev(tr[p].r_child, key, tr));
+        return max(tr[p].key, getPrev(tr[p].r, key, tr));
     }
 
-    int GetNext(int p, int key, vector<Node> &tr) {
+    int getNext(int p, int key, Node tr[]) {
         if (!p) {
             return INF;
         }
         if (tr[p].key <= key) {
-            return GetNext(tr[p].r_child, key, tr);
+            return getNext(tr[p].r, key, tr);
         }
-        return min(tr[p].key, GetNext(tr[p].l_child, key, tr));
+        return min(tr[p].key, getNext(tr[p].l, key, tr));
     }
 
     int main() {
         int n = 0;
         scanf("%d", &n);
-        vector<Node> tree(n + 1);
+        Node tree[n + 1];
         int idx = 0;
-        auto root = BuildTree(tree, idx);
+        auto root = buildTree(tree, idx);
         while (n--) {
             int opt;
             int x;
             scanf("%d%d", &opt, &x);
             if (opt == 1) {
-                Insert(root, x, tree, idx);
+                insert(root, x, tree, idx);
             } else if (opt == 2) {
-                Erase(root, x, tree);
+                erase(root, x, tree);
             } else if (opt == 3) {
-                printf("%d\n", GetRankByKey(root, x, tree) - 1);
+                printf("%d\n", getRankByKey(root, x, tree) - 1);
             } else if (opt == 4) {
-                printf("%d\n", GetKeyByRank(root, x + 1, tree));
+                printf("%d\n", getKeyByRank(root, x + 1, tree));
             } else if (opt == 5) {
-                printf("%d\n", GetPrev(root, x, tree));
+                printf("%d\n", getPrev(root, x, tree));
             } else {
-                printf("%d\n", GetNext(root, x, tree));
+                printf("%d\n", getNext(root, x, tree));
             }
         }
         return 0;
