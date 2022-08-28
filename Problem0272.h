3dc4a98286e6f3b293f73cae757d094f7ac48261
fengwei <virusformail@gmail.com>
2022-08-28 00:17:56 -0700

    Reviewed Problem 272.

--- Problem0272.h
+++ Problem0272.h
@@ -21,22 +21,22 @@ private:
     int longestCommonIS(const int l1, const int l2) {
         // 不要忘记优化过程
         for (int i = 1; i <= l1; ++i) {
-            int max_pre_j = 0;
+            int max_pre_j = 1;
             for (int j = 1; j <= l2; ++j) {
                 dp[i][j] = dp[i - 1][j];
                 if (a[i] == b[j]) {
-                    dp[i][j] = max(max_pre_j + 1, dp[i][j]);
+                    dp[i][j] = max(max_pre_j, dp[i][j]);
                 } else if (b[j] < a[i]) {
-                    max_pre_j = max(max_pre_j, dp[i][j]);
+                    max_pre_j = max(max_pre_j, dp[i - 1][j] + 1);
                 }
             }
         }
-        int result = 0;
+        int ans = 0;
         // 注意：不能通过遍历a的索引来获得结果，只能遍历b的索引（参见转移方程）
         for (int i = 1; i <= l2; ++i) {
-            result = max(result, dp[l1][i]);
+            ans = max(ans, dp[l1][i]);
         }
-        return result;
+        return ans;
     }
 
     int main() {
