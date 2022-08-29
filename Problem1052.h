0dbfd0b041760473c26bb0b36b99a6304c4eb15d
fengwei <virusformail@gmail.com>
2022-08-29 14:13:33 -0700

    Reviewed Problem 1052.

--- Problem1052.h
+++ Problem1052.h
@@ -36,12 +36,12 @@ private:
         const int MOD = 1000000007;
         const int m = (int) strlen(str);
         int fail[m];
-        int dp[n + 1][m + 1];
-        int next_state[m + 1][26];
-        memset(dp, 0, sizeof dp);
-        memset(next_state, 0, sizeof next_state);
+        int f[n + 1][m + 1];
+        int next_matched_length[m + 1][26];
+        memset(f, 0, sizeof f);
+        memset(next_matched_length, 0, sizeof next_matched_length);
         buildFail(str, m, fail);
-        dp[0][0] = 1;
+        f[0][0] = 1;
         for (int i = 0; i < m; ++i) {  // 预计算状态转移路径
             for (char ch = 'a'; ch <= 'z'; ++ch) {
                 auto u = i; // u是i的后继状态
@@ -51,22 +51,22 @@ private:
                 if (str[u] == ch) {
                     ++u;
                 }
-                next_state[i][ch - 'a'] = u;
+                next_matched_length[i][ch - 'a'] = u;
             }
         }
-        for (int length = 0; length < n; ++length) {  // 循环当前的密码长度，探索下一位
+        for (int l = 0; l < n; ++l) {  // 循环当前的密码长度，探索下一位
             for (int i = 0; i < m; ++i) {  // 遍历后缀和模式串可能的匹配长度
                 for (char ch = 'a'; ch <= 'z'; ++ch) {
-                    auto u = next_state[i][ch - 'a'];
-                    dp[length + 1][u] = (int) (((long) dp[length + 1][u] + dp[length][i]) % MOD);
+                    auto u = next_matched_length[i][ch - 'a'];
+                    f[l + 1][u] = (int) (((long) f[l + 1][u] + f[l][i]) % MOD);
                 }
             }
         }
-        int result = 0;
+        int ans = 0;
         for (int j = m - 1; j >= 0; --j) { // 统计所有与str匹配的长度小于m的方案
-            result = (int) (((long long) result + dp[n][j]) % MOD);
+            ans = (int) (((long long) ans + f[n][j]) % MOD);
         }
-        return result;
+        return ans;
     }
 
     int main() {
