def4ad8a35dad4e72d5e4cd4a81a6c460810c58d
fengwei <virusformail@gmail.com>
2022-09-03 10:27:42 -0700

    Reviewed Problem 1074.

--- Problem1074.h
+++ Problem1074.h
@@ -14,17 +14,17 @@ class Problem1074 {
     // 分组背包 + 树形DP：https://www.acwing.com/solution/content/10714/
     // f[i][m]：以i为根，保留m个树枝时的最多苹果
 private:
-    void dfs(int u, int p, int m, const vector<vector<pair<int, int>>> &graph, vector<vector<int>> &dp) {
+    void dfs(int u, int p, int m, const vector<vector<pair<int, int>>> &graph, vector<vector<int>> &f) {
         for (const auto &t: graph[u]) {
             auto v = t.first;
             if (v == p) {
                 continue;
             }
-            dfs(v, u, m, graph, dp);
+            dfs(v, u, m, graph, f);
             for (int j = m; j >= 0; --j) {  // 循环体积
                 for (int k = 0; k < j; ++k) {  // 循环决策（这里k不能等于j！）
                     // 不要忘记是j - k - 1，不是j - k，要减去到子结点的边（树枝）
-                    dp[u][j] = max(dp[u][j], dp[u][j - k - 1] + t.second + dp[v][k]);
+                    f[u][j] = max(f[u][j], f[u][j - k - 1] + t.second + f[v][k]);
                 }
             }
         }
@@ -34,15 +34,15 @@ private:
         int n, q;
         scanf("%d%d", &n, &q);
         vector<vector<pair<int, int>>> graph(n + 1, vector<pair<int, int>>());
-        vector<vector<int>> dp(n + 1, vector<int>(q + 1, 0));
+        vector<vector<int>> f(n + 1, vector<int>(q + 1, 0));
         for (int i = 0; i < n - 1; ++i) {
             int a, b, w;
             scanf("%d%d%d", &a, &b, &w);
             graph[a].push_back({b, w});  // 由于我们并不清楚树的形状，所以这里使用双向边
             graph[b].push_back({a, w});
         }
-        dfs(1, -1, q, graph, dp);
-        printf("%d\n", dp[1][q]);
+        dfs(1, -1, q, graph, f);
+        printf("%d\n", f[1][q]);
         return 0;
     }
 };
