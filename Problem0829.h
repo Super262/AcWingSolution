f708e9dc3d24f4b389341eebaf8dacfd9e7b5eea
fengwei <virusformail@gmail.com>
2023-06-22 16:29:22 -0700

    Reviewed Problem 829.

--- Problem0829.h
+++ Problem0829.h
@@ -11,12 +11,14 @@ using namespace std;
 
 class Problem0829 {
 public:
-    void pop(unsigned long &front) {
+    void pop(unsigned long &front, unsigned long m) {
         ++front;
+        front %= m;
     }
 
-    void push(const int value, int data[], unsigned long &end) {
+    void push(const int value, int data[], unsigned long &end, unsigned long m) {
         data[end++] = value;
+        end %= m;
     }
 
     bool empty(const unsigned long front, const unsigned long end) {
@@ -30,7 +32,7 @@ public:
     int main() {
         unsigned long m;
         cin >> m;
-        int *data = new int[m];
+        int data[m];
         unsigned long front = 0;
         unsigned long end = 0;
         string op;
@@ -39,13 +41,13 @@ public:
             cin >> op;
             if (op == "push") {
                 cin >> value;
-                push(value, data, end);
+                push(value, data, end, m);
             } else if (op == "query") {
                 cout << peek(data, front) << endl;
             } else if (op == "empty") {
                 cout << (empty(front, end) ? "YES" : "NO") << endl;
             } else {
-                pop(front);
+                pop(front, m);
             }
         }
         return 0;
