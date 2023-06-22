6401f45628755a076786a4c350f8d19147bb9fb9
fengwei <virusformail@gmail.com>
2023-06-22 16:35:57 -0700

    Reviewed Problem 154.

--- Problem0154.h
+++ Problem0154.h
@@ -9,11 +9,7 @@
 
 class Problem0154 {
 private:
-    static const int N = 1000010;
-    int a[N];
-    int dq[N];
-
-    void printMax(int n, int k) {
+    void printMax(int n, int k, int *a, int *dq) {
         int hh = 0, tt = -1;
         for (int i = 0; i < n; ++i) {
             while (hh <= tt && i - k + 1 > dq[hh]) {
@@ -30,7 +26,7 @@ private:
         printf("\n");
     }
 
-    void printMin(int n, int k) {
+    void printMin(int n, int k, int *a, int *dq) {
         int hh = 0, tt = -1;
         for (int i = 0; i < n; ++i) {
             while (hh <= tt && i - k + 1 > dq[hh]) {
@@ -50,14 +46,15 @@ private:
     int main() {
         int n, k;
         scanf("%d%d", &n, &k);
+        int a[n];
+        int dq[n];
         for (int i = 0; i < n; ++i) {
             scanf("%d", &a[i]);
         }
-        printMin(n, k);
-        printMax(n, k);
+        printMin(n, k, a, dq);
+        printMax(n, k, a, dq);
         return 0;
     }
-
 };
 
 #endif //ACWINGSOLUTION_PROBLEM0154_H
