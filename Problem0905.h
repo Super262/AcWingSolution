8682e36548319c78e5d3f924d87c6d8e43906212
fengwei <virusformail@gmail.com>
2023-07-07 00:41:50 -0700

    Reviewed Problem 905.

--- Problem0905.h
+++ Problem0905.h
@@ -10,19 +10,19 @@
 
 using namespace std;
 
-struct Range {
-    int left;
-    int right;
-
-    bool operator<(const Range &b) const {
-        return right < b.right;
-    }
-};
-
 class Problem0905 {
     // 1. 将每个区间按右端点从小到大排序
     // 2. 从前向后依次枚举每个区间：如果当前区间中已经包含点，跳过：否则，选择当前区间的右端点
-public:
+private:
+    struct Range {
+        int left;
+        int right;
+
+        bool operator<(const Range &b) const {
+            return right < b.right;
+        }
+    };
+    
     int minConnection(Range *ranges, const int N) {
         sort(ranges, ranges + N);
         int ans = 0;
@@ -40,12 +40,11 @@ public:
     int main() {
         int n;
         scanf("%d", &n);
-        auto ranges = new Range[n];
+        Range ranges[n];
         for (int i = 0; i < n; ++i) {
             scanf("%d%d", &ranges[i].left, &ranges[i].right);
         }
         printf("%d\n", minConnection(ranges, n));
-        delete[] ranges;
         return 0;
     }
 };
