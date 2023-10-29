16e7eb31624504354731eba7588d31783db0f298
fengwei <virusformail@gmail.com>
2023-10-29 00:58:29 -0700

    Reviewed Problem 850.

--- Problem0850.h
+++ Problem0850.h
@@ -12,9 +12,11 @@
 
 using namespace std;
 
-class Problem0850 {
+class Problem0850
+{
 private:
-    int dijkstra(const int st, const int ed, const int n, const vector<vector<pair<int, int>>> &graph) {
+    int dijkstra(const int st, const int ed, const int n, const vector<vector<pair<int, int>>> &graph)
+    {
         int dist[n + 1];
         bool selected[n + 1];
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
@@ -22,36 +24,42 @@ private:
         memset(selected, 0, sizeof selected);
         dist[st] = 0;
         heap.push({dist[st], st});
-        while (!heap.empty()) {
+        while (!heap.empty())
+        {
             auto t = heap.top();
             heap.pop();
             auto rv = t.second;
-            if (selected[rv]) {
+            if (selected[rv])
+            {
                 continue;
             }
             selected[rv] = true;
-            for (const auto &nt: graph[rv]) {
+            for (const auto &nt : graph[rv])
+            {
                 auto nv = nt.second;
                 auto nd = nt.first;
-                if (dist[nv] < dist[rv] + nd) {
+                if (dist[nv] < dist[rv] + nd)
+                {
                     continue;
                 }
                 dist[nv] = dist[rv] + nd;
                 heap.push({dist[nv], nv});
             }
         }
-        if (dist[ed] == 0x3f3f3f3f) {
+        if (dist[ed] == 0x3f3f3f3f)
+        {
             return -1;
         }
         return dist[ed];
     }
 
-    int main() {
+    int main()
+    {
         int n, m;
         scanf("%d%d", &n, &m);
-        vector<vector<pair<int, int>>> graph(n + 1, vector<pair<int, int>>());
-        int u, v, w;
-        for (int i = 0; i < m; ++i) {
+        vector<vector<pair<int, int>>> graph(n + 1);
+        for (int i = 0, u, v, w; i < m; ++i)
+        {
             scanf("%d%d%d", &u, &v, &w);
             graph[u].push_back({w, v});
         }
@@ -60,4 +68,4 @@ private:
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0850_H
+#endif // ACWINGSOLUTION_PROBLEM0850_H
