572dcf8954a7a65e33c613fb5a2684c917a6812c
fengwei <virusformail@gmail.com>
2023-10-29 16:50:35 -0700

    Reviewed Problem 860.

--- Problem0860.h
+++ Problem0860.h
@@ -11,49 +11,53 @@
 
 using namespace std;
 
-class Problem0860 {
+class Problem0860
+{
 private:
-    bool dfs(int root, int val, vector<int> &color, const vector<vector<int>> &graph) {
-        color[root] = val;
-        for (const auto &nv: graph[root]) {
-            if (color[nv] == val) {
+    bool dfs(int u, int val, vector<int> &color, const vector<vector<int>> &graph)
+    {
+        color[u] = val;
+        for (const auto &v : graph[u])
+        {
+            if (color[v] == val)
+            {
                 return false;
             }
-            if (color[nv] == 0 && !dfs(nv, 3 - val, color, graph)) {
+            if (!color[v] && !dfs(v, 3 - val, color, graph))
+            {
                 return false;
             }
         }
         return true;
     }
 
-    int main() {
+    int main()
+    {
         int n, m;
         scanf("%d%d", &n, &m);
-        vector<vector<int>> graph(n + 1, vector<int>());
+        vector<vector<int>> graph(n + 1);
         vector<int> color(n + 1, 0);
-        int u, v;
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0, u, v; i < m; ++i)
+        {
             scanf("%d%d", &u, &v);
             graph[u].emplace_back(v);
             graph[v].emplace_back(u);
         }
-        bool result = true;
-        for (int i = 1; i <= n; ++i) {
-            if (color[i] != 0) {
+        for (int i = 1; i <= n; ++i)
+        {
+            if (color[i])
+            {
                 continue;
             }
-            if (!dfs(i, 1, color, graph)) {
-                result = false;
-                break;
+            if (!dfs(i, 1, color, graph))
+            {
+                printf("No\n");
+                return 0;
             }
         }
-        if (result) {
-            printf("Yes\n");
-        } else {
-            printf("No\n");
-        }
+        printf("Yes\n");
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0860_H
+#endif // ACWINGSOLUTION_PROBLEM0860_H
