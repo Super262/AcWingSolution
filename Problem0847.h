7833368c869a60bece972bcf7559221d8067d883
fengwei <virusformail@gmail.com>
2023-10-28 20:57:48 -0700

    Reviewed Problem 847.

--- Problem0847.h
+++ Problem0847.h
@@ -7,64 +7,59 @@
 
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-class Problem0847 {
+class Problem0847
+{
 private:
-    static const int N = 100010;
-    int headIndex[N];
-    int nextIndex[2 * N];
-    int vertexValue[2 * N];
-    int dist[N];
-    int q[2 * N];
-
-    void addEdge(const int a, const int b, int &idx) {
-        vertexValue[idx] = b;
-        nextIndex[idx] = headIndex[a];
-        headIndex[a] = idx;
-        ++idx;
-    }
-
-    int bfs(const int root, const int target) {
+    int bfs(const int root, const int target, const vector<vector<int>> &graph)
+    {
+        queue<int> q;
+        int dist[graph.size()];
         memset(dist, -1, sizeof dist);
-        int hh = 0, tt = -1;
         dist[root] = 0;
-        q[++tt] = root;
-        while (hh <= tt) {
-            auto currentLevelSize = tt - hh + 1;
-            while (currentLevelSize--) {
-                auto node = q[hh++];
-                if (node == target) {
+        q.emplace(root);
+        while (!q.empty())
+        {
+            auto current_level_size = (int)q.size();
+            while (current_level_size--)
+            {
+                auto node = q.front();
+                q.pop();
+                if (node == target)
+                {
                     return dist[node];
                 }
-                for (auto idx = headIndex[node]; idx != -1; idx = nextIndex[idx]) {
-                    auto nextV = vertexValue[idx];
-                    if (dist[nextV] != -1) {
+                for (const auto &next_v : graph[node])
+                {
+                    if (dist[next_v] != -1)
+                    {
                         continue;
                     }
-                    q[++tt] = nextV;
-                    dist[nextV] = dist[node] + 1;
+                    q.emplace(next_v);
+                    dist[next_v] = dist[node] + 1;
                 }
             }
         }
         return dist[target];
     }
 
-    int main() {
-        memset(headIndex, -1, sizeof headIndex);
-        memset(nextIndex, -1, sizeof nextIndex);
+    int main()
+    {
         int n, m;
         scanf("%d%d", &n, &m);
-        int idx = 0;
-        for (int i = 0; i < m; ++i) {
-            int a, b;
+        vector<vector<int>> graph(n + 1);
+        for (int i = 0, a, b; i < m; ++i)
+        {
             scanf("%d%d", &a, &b);
-            addEdge(a, b, idx);
+            graph[a].emplace_back(b);
         }
-        printf("%d\n", bfs(1, n));
+        printf("%d\n", bfs(1, n, graph));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0847_H
+#endif // ACWINGSOLUTION_PROBLEM0847_H
