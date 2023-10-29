a49ec702d1f9fa803b0d5b5a7dde4ff50a8fa97a
fengwei <virusformail@gmail.com>
2023-10-28 21:36:50 -0700

    Reviewed Problem 848.

--- Problem0848.h
+++ Problem0848.h
@@ -8,55 +8,69 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 
 using namespace std;
 
-class Problem0848 {
+class Problem0848
+{
 private:
-    vector<int> bfs(const int n, const vector<vector<int>> &graph, vector<int> &in_degree) {
+    vector<int> bfs(const int n, const vector<vector<int>> &graph, int *in_degree)
+    {
         queue<int> q;
-        vector<int> res;
-        for (int i = 1; i <= n; ++i) {
-            if (in_degree[i] == 0) {
-                q.emplace(i);
+        vector<int> ans;
+        for (int i = 1; i <= n; ++i)
+        {
+            if (in_degree[i])
+            {
+                continue;
             }
+            q.emplace(i);
         }
-        while (!q.empty()) {
+        while (!q.empty())
+        {
             auto t = q.front();
             q.pop();
-            res.emplace_back(t);
-            for (const auto &v: graph[t]) {
+            ans.emplace_back(t);
+            for (const auto &v : graph[t])
+            {
                 --in_degree[v];
-                if (in_degree[v] == 0) {
-                    q.emplace(v);
+                if (in_degree[v])
+                {
+                    continue;
                 }
+                q.emplace(v);
             }
         }
-        return res;
+        return ans;
     }
 
-    int main() {
+    int main()
+    {
         int n, m;
         scanf("%d%d", &n, &m);
-        vector<vector<int>> graph(n + 1, vector<int>());
-        vector<int> in_degree(n + 1, 0);
-        int u, v;
-        for (int i = 0; i < m; ++i) {
+        vector<vector<int>> graph(n + 1);
+        int in_degree[n + 1];
+        memset(in_degree, 0, sizeof in_degree);
+        for (int i = 0, u, v; i < m; ++i)
+        {
             scanf("%d%d", &u, &v);
             graph[u].emplace_back(v);
             ++in_degree[v];
         }
-        auto res = bfs(n, graph, in_degree);
-        if (res.size() != n) {
+        auto ans = bfs(n, graph, in_degree);
+        if (ans.size() != n)
+        {
             printf("-1\n");
-        } else {
-            for (const auto &x: res) {
-                printf("%d ", x);
-            }
-            printf("\n");
+            return 0;
         }
+        for (const auto &x : ans)
+        {
+            printf("%d ", x);
+        }
+        printf("\n");
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0848_H
+#endif // ACWINGSOLUTION_PROBLEM0848_H
