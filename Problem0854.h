ff030f64ee9dc2e5299b538ccf25f829a614d960
fengwei <virusformail@gmail.com>
2023-10-29 15:44:26 -0700

    Reviewed Problem 854.

--- Problem0854.h
+++ Problem0854.h
@@ -11,19 +11,26 @@
 
 using namespace std;
 
-class Problem0854 {
+class Problem0854
+{
 private:
-    const int INF = 0x3f3f3f3f;
+    static const int INF = 0x3f3f3f3f;
 
-    vector<vector<int>> floyd(const int n, const vector<vector<int>> &graph) {
+    vector<vector<int>> floyd(const int n, const vector<vector<int>> &graph)
+    {
         vector<vector<int>> dist(graph);
-        for (int i = 1; i <= n; ++i) {  // 注意：起点和终点相同，距离为0
+        for (int i = 1; i <= n; ++i)
+        { // 注意：起点和终点相同，距离为0
             dist[i][i] = 0;
         }
-        for (int mid = 1; mid <= n; ++mid) {
-            for (int st = 1; st <= n; ++st) {
-                for (int ed = 1; ed <= n; ++ed) {
-                    if (dist[st][mid] == INF || dist[mid][ed] == INF) {  // 排除负边的影响，INF恒为0x3f3f3f3f
+        for (int mid = 1; mid <= n; ++mid)
+        {
+            for (int st = 1; st <= n; ++st)
+            {
+                for (int ed = 1; ed <= n; ++ed)
+                {
+                    if (dist[st][mid] == INF || dist[mid][ed] == INF)
+                    { // 排除负边的影响，INF恒为0x3f3f3f3f
                         continue;
                     }
                     dist[st][ed] = min(dist[st][ed], dist[st][mid] + dist[mid][ed]);
@@ -33,22 +40,26 @@ private:
         return dist;
     }
 
-    int main() {
+    int main()
+    {
         int n, m, k;
         scanf("%d%d%d", &n, &m, &k);
         vector<vector<int>> graph(n + 1, vector<int>(n + 1, INF));
-        int x, y, z;
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0, x, y, z; i < m; ++i)
+        {
             scanf("%d%d%d", &x, &y, &z);
             graph[x][y] = min(graph[x][y], z);
         }
         auto dist = floyd(n, graph);
-        int a, b;
-        for (int i = 0; i < k; ++i) {
+        for (int i = 0, a, b; i < k; ++i)
+        {
             scanf("%d%d", &a, &b);
-            if (dist[a][b] == INF) {
+            if (dist[a][b] == INF)
+            {
                 printf("impossible\n");
-            } else {
+            }
+            else
+            {
                 printf("%d\n", dist[a][b]);
             }
         }
@@ -56,4 +67,4 @@ private:
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0854_H
+#endif // ACWINGSOLUTION_PROBLEM0854_H
