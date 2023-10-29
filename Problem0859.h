9c02a9fdbdd456f053708ca56ce8f284c9f06002
fengwei <virusformail@gmail.com>
2023-10-29 16:29:44 -0700

    Reviewed Problem 859.

--- Problem0859.h
+++ Problem0859.h
@@ -10,26 +10,32 @@
 
 using namespace std;
 
-class Problem0859 {
+class Problem0859
+{
 private:
-    const int INF = 0x3f3f3f3f;
+    static const int INF = 0x3f3f3f3f;
 
-    struct Edge {
+    struct Edge
+    {
         int u;
         int v;
         int w;
 
-        bool operator<(const Edge &e) const {
+        bool operator<(const Edge &e) const
+        {
             return w < e.w;
         }
     };
 
-    int findRoot(int x, int parent[]) {
+    int findRoot(int x, int *parent)
+    {
         auto u = x;
-        while (u != parent[u]) {
+        while (u != parent[u])
+        {
             u = parent[u];
         }
-        while (x != u) {
+        while (x != u)
+        {
             auto p = parent[x];
             parent[x] = u;
             x = p;
@@ -37,11 +43,14 @@ private:
         return u;
     }
 
-    int mergeSets(int pa, int pb, int parent[], int set_size[]) {
-        if (pa == pb) {
+    int mergeSets(int pa, int pb, int *parent, int *set_size)
+    {
+        if (pa == pb)
+        {
             return -1;
         }
-        if (set_size[pb] > set_size[pa]) {
+        if (set_size[pb] > set_size[pa])
+        {
             swap(pa, pb);
         }
         set_size[pa] += set_size[pb];
@@ -49,47 +58,57 @@ private:
         return pa;
     }
 
-    int kruskal(const int n, const int m, Edge edges[], int parent[], int set_size[]) {
+    int kruskal(const int n, const int m, Edge *edges, int *parent, int *set_size)
+    {
         sort(edges, edges + m);
         int ans = 0;
         int cnt = 0;
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < m; ++i)
+        {
             auto pa = findRoot(edges[i].u, parent);
             auto pb = findRoot(edges[i].v, parent);
-            if (pa == pb) {
+            if (pa == pb)
+            {
                 continue;
             }
             mergeSets(pa, pb, parent, set_size);
             ++cnt;
             ans += edges[i].w;
         }
-        if (cnt != n - 1) {  // MST边数应为n-1
+        if (cnt != n - 1)
+        { // MST边数应为n-1
             return INF;
         }
         return ans;
     }
 
-    int main() {
+    int main()
+    {
         int n, m;
         scanf("%d%d", &n, &m);
         int parent[n + 1];
         int set_size[n + 1];
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 1; i <= n; ++i)
+        {
             parent[i] = i;
             set_size[i] = 1;
         }
         Edge edges[m];
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < m; ++i)
+        {
             scanf("%d%d%d", &edges[i].u, &edges[i].v, &edges[i].w);
         }
         auto ans = kruskal(n, m, edges, parent, set_size);
-        if (ans == INF) {
+        if (ans == INF)
+        {
             printf("impossible\n");
-        } else {
+        }
+        else
+        {
             printf("%d\n", ans);
         }
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0859_H
+#endif // ACWINGSOLUTION_PROBLEM0859_H
