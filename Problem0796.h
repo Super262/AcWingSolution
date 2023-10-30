3038af5c8632f34485205d5e689c0bf02fcdb846
fengwei <virusformail@gmail.com>
2023-10-29 23:19:57 -0700

    Reviewed Problem 796.

--- Problem0796.h
+++ Problem0796.h
@@ -10,36 +10,42 @@
 
 using namespace std;
 
-
-class Problem0796 {
-public:
-    vector<vector<int>> getPrefixSum(const vector<vector<int>> &matrix) {
+class Problem0796
+{
+private:
+    vector<vector<int>> get_prefix_sum(const vector<vector<int>> &matrix)
+    {
         vector<vector<int>> res(matrix.size() + 1, vector<int>(matrix[0].size() + 1, 0));
-        for (unsigned long i = 1; i < res.size(); ++i) {
-            for (unsigned long j = 1; j < res[0].size(); ++j) {
+        for (int i = 1; i < res.size(); ++i)
+        {
+            for (int j = 1; j < res[0].size(); ++j)
+            {
                 res[i][j] = res[i - 1][j] + res[i][j - 1] - res[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
         return res;
     }
 
-    int main() {
+    int main()
+    {
         int n, m, q;
         scanf("%d%d%d", &n, &m, &q);
-        vector<vector<int>> matrix(n, vector<int>(m, 0));
-        for (unsigned long i = 0; i < n; ++i) {
-            for (unsigned long j = 0; j < m; ++j) {
+        vector<vector<int>> matrix(n, vector<int>(m));
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
                 scanf("%d", &matrix[i][j]);
             }
         }
-        vector<vector<int>> prefix = getPrefixSum(matrix);
-        int x1, y1, x2, y2;
-        for (int i = 0; i < q; ++i) {
+        const auto &matrix_prefix = get_prefix_sum(matrix);
+        for (int i = 0, x1, y1, x2, y2; i < q; ++i)
+        {
             scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
-            printf("%d\n", prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1]);
+            printf("%d\n", matrix_prefix[x2][y2] - matrix_prefix[x1 - 1][y2] - matrix_prefix[x2][y1 - 1] + matrix_prefix[x1 - 1][y1 - 1]);
         }
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0796_H
+#endif // ACWINGSOLUTION_PROBLEM0796_H
