38e79b7ef9e7a36a9f39899959a5369bba66e945
fengwei <virusformail@gmail.com>
2023-10-29 17:30:11 -0700

    Reviewed Problem 785.

--- Problem0785.h
+++ Problem0785.h
@@ -6,45 +6,55 @@
 #define ACWINGSOLUTION_PROBLEM0785_H
 
 #include <iostream>
-#include <vector>
+#include <cstring>
 
 using namespace std;
 
-class Problem0785 {
+class Problem0785
+{
 private:
-    void quickSort(vector<int> &nums, int st, int ed) {
-        if (st >= ed) {
+    void quick_sort(int *nums, int st, int ed)
+    {
+        if (st >= ed)
+        {
             return;
         }
         // auto p = nums[st + (ed - st) / 2];  // 选择偏左的中点
-        auto p = nums[st + (ed - st + 1) / 2];  // 选择偏右的中点
+        auto p = nums[st + (ed - st + 1) / 2]; // 选择偏右的中点
         auto l = st - 1;
         auto r = ed + 1;
-        while (l < r) {
-            while (nums[++l] < p);  // 注意：循环条件不包含 l < r
-            while (nums[--r] > p);
-            if (l < r) {
+        while (l < r)
+        {
+            while (nums[++l] < p)
+                ; // 注意：循环条件不包含 l < r
+            while (nums[--r] > p)
+                ;
+            if (l < r)
+            {
                 swap(nums[l], nums[r]);
             }
         }
         // 对应偏左的中点
-        // quickSort(nums, st, r);
-        // quickSort(nums, r + 1, ed);
+        // quick_sort(nums, st, r);
+        // quick_sort(nums, r + 1, ed);
 
         // 对应偏右的中点
-        quickSort(nums, st, l - 1);
-        quickSort(nums, l, ed);
+        quick_sort(nums, st, l - 1);
+        quick_sort(nums, l, ed);
     }
 
-    int main() {
+    int main()
+    {
         int n;
         scanf("%d", &n);
-        vector<int> nums(n, 0);
-        for (int i = 0; i < n; ++i) {
+        int nums[n];
+        for (int i = 0; i < n; ++i)
+        {
             scanf("%d", &nums[i]);
         }
-        quickSort(nums, 0, n - 1);
-        for (int i = 0; i < n; ++i) {
+        quick_sort(nums, 0, n - 1);
+        for (int i = 0; i < n; ++i)
+        {
             printf("%d ", nums[i]);
         }
         printf("\n");
@@ -52,4 +62,4 @@ private:
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0785_H
+#endif // ACWINGSOLUTION_PROBLEM0785_H
