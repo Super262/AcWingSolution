3b2942f302ff5c83d86b000409f6f337970381d4
fengwei <virusformail@gmail.com>
2023-11-01 01:21:37 -0700

    Reviewed Problem 2816.

--- Problem2816.h
+++ Problem2816.h
@@ -10,15 +10,19 @@
 
 using namespace std;
 
-class Problem2816 {
+class Problem2816
+{
 private:
-    bool isSubArray(const vector<int> &nums1, const vector<int> &nums2) {
-        const auto n = (int) nums1.size();
-        const auto m = (int) nums2.size();
+    bool is_sub_array(const vector<int> &nums1, const vector<int> &nums2)
+    {
+        const auto n = (int)nums1.size();
+        const auto m = (int)nums2.size();
         int i = 0;
         int j = 0;
-        while (i < n && j < m) {
-            if (nums1[i] == nums2[j]) {
+        while (i < n && j < m)
+        {
+            if (nums1[i] == nums2[j])
+            {
                 ++i;
             }
             ++j;
@@ -26,25 +30,31 @@ private:
         return i == n;
     }
 
-    int main() {
+    int main()
+    {
         int n;
         int m;
         scanf("%d%d", &n, &m);
         vector<int> nums1(n);
         vector<int> nums2(m);
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             scanf("%d", &nums1[i]);
         }
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < m; ++i)
+        {
             scanf("%d", &nums2[i]);
         }
-        if (isSubArray(nums1, nums2)) {
+        if (is_sub_array(nums1, nums2))
+        {
             printf("Yes\n");
-        } else {
+        }
+        else
+        {
             printf("No\n");
         }
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM2816_H
+#endif // ACWINGSOLUTION_PROBLEM2816_H
