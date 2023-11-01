9bea82e3084adf938bbbf673702f0c5c9eb8fa82
fengwei <virusformail@gmail.com>
2023-11-01 00:55:30 -0700

    Reviewed Problem 798.

--- Problem0798.h
+++ Problem0798.h
@@ -10,40 +10,48 @@
 
 using namespace std;
 
-class Problem0798 {
+class Problem0798
+{
 private:
-    void matrixIncrease(vector<vector<int>> &diff, const int x1, const int y1, const int x2, const int y2, const int v) {
+    void matrixIncrease(vector<vector<int>> &diff, const int &x1, const int &y1, const int &x2, const int &y2, const int &v)
+    {
         diff[x1][y1] += v;
         diff[x2 + 1][y1] -= v;
         diff[x1][y2 + 1] -= v;
         diff[x2 + 1][y2 + 1] += v;
     }
 
-    int main() {
+    int main()
+    {
         int n, m, q;
         scanf("%d%d%d", &n, &m, &q);
 
         // 行数至少为n+2，列数至少为m+2；否则，触发越界异常！
         vector<vector<int>> matrix(n + 2, vector<int>(m + 2, 0));
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                int t;
+        for (int i = 1, t; i <= n; ++i)
+        {
+            for (int j = 1; j <= m; ++j)
+            {
                 scanf("%d", &t);
                 matrixIncrease(matrix, i, j, i, j, t);
             }
         }
-        for (int i = 0; i < q; ++i) {
-            int x1, y1, x2, y2, t;
+        for (int i = 0, x1, y1, x2, y2, t; i < q; ++i)
+        {
             scanf("%d%d%d%d%d", &x1, &y1, &x2, &y2, &t);
             matrixIncrease(matrix, x1, y1, x2, y2, t);
         }
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= m; ++j)
+            {
                 matrix[i][j] += matrix[i - 1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1];
             }
         }
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= m; ++j)
+            {
                 printf("%d ", matrix[i][j]);
             }
             printf("\n");
@@ -52,4 +60,4 @@ private:
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0798_H
+#endif // ACWINGSOLUTION_PROBLEM0798_H
