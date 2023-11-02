d430402589c9b55f4a97ee8d9a180eb47acdd149
fengwei <virusformail@gmail.com>
2023-11-02 02:11:02 -0700

    Reviewed Problem 829.

--- Problem0829.h
+++ Problem0829.h
@@ -9,45 +9,58 @@
 
 using namespace std;
 
-class Problem0829 {
+class Problem0829
+{
 private:
-    void pop(unsigned long &front) {
+    void pop(unsigned long &front)
+    {
         ++front;
     }
 
-    void push(const int value, int data[], unsigned long &end, unsigned long m) {
+    void push(const int &value, int *data, unsigned long &end, const unsigned long &m)
+    {
         data[end] = value;
         ++end;
         end %= m;
     }
 
-    bool empty(const unsigned long front, const unsigned long end) {
+    bool empty(const unsigned long &front, const unsigned long &end)
+    {
         return front == end;
     }
 
-    int peek(const int data[], unsigned long &front, unsigned long m) {
+    int peek(int *data, unsigned long &front, const unsigned long &m)
+    {
         front %= m;
         return data[front];
     }
 
-    int main() {
+    int main()
+    {
         unsigned long m;
         cin >> m;
         int data[m];
         unsigned long front = 0;
         unsigned long end = 0;
         string op;
-        int value;
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0, value; i < m; ++i)
+        {
             cin >> op;
-            if (op == "push") {
+            if (op == "push")
+            {
                 cin >> value;
                 push(value, data, end, m);
-            } else if (op == "query") {
+            }
+            else if (op == "query")
+            {
                 cout << peek(data, front, m) << endl;
-            } else if (op == "empty") {
+            }
+            else if (op == "empty")
+            {
                 cout << (empty(front, end) ? "YES" : "NO") << endl;
-            } else {
+            }
+            else
+            {
                 pop(front);
             }
         }
@@ -55,4 +68,4 @@ private:
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0829_H
+#endif // ACWINGSOLUTION_PROBLEM0829_H
