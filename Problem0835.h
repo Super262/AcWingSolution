a5d9f5135ebc779e55c6537795905130ffb95b82
fengwei <virusformail@gmail.com>
2023-11-03 01:56:17 -0700

    Reviewed Problem 835.

--- Problem0835.h
+++ Problem0835.h
@@ -7,26 +7,33 @@
 
 #include <iostream>
 
-class Problem0835 {
+class Problem0835
+{
     // 完整背诵这部分代码！
 private:
-    struct Node {
+    struct Node
+    {
         int cnt;
         Node *children[26]{};
 
-        Node() {
+        Node()
+        {
             cnt = 0;
-            for (auto &item: children) {
+            for (auto &item : children)
+            {
                 item = nullptr;
             }
         }
     };
 
-    void insertStr(Node *root, const char str[]) {
+    void insert_str(Node *root, const char *str)
+    {
         auto current = root;
-        for (int i = 0; str[i]; ++i) {
-            auto idx = str[i] - 'a';
-            if (!current->children[idx]) {
+        for (int i = 0, idx; str[i]; ++i)
+        {
+            idx = str[i] - 'a';
+            if (!current->children[idx])
+            {
                 current->children[idx] = new Node();
             }
             current = current->children[idx];
@@ -34,11 +41,14 @@ private:
         ++current->cnt;
     }
 
-    int queryStr(Node *root, const char str[]) {
+    int query_str(Node *root, const char *str)
+    {
         auto current = root;
-        for (int i = 0; str[i]; ++i) {
-            auto idx = str[i] - 'a';
-            if (!current->children[idx]) {
+        for (int i = 0, idx; str[i]; ++i)
+        {
+            idx = str[i] - 'a';
+            if (!current->children[idx])
+            {
                 return 0;
             }
             current = current->children[idx];
@@ -46,23 +56,27 @@ private:
         return current->cnt;
     }
 
-    int main() {
-        const int N = 100010;
-        char str[N];
+    int main()
+    {
+        char str[100010];
         char op[2];
         int n;
         scanf("%d", &n);
         auto root = new Node();
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             scanf("%s%s", op, str);
-            if (op[0] == 'I') {
-                insertStr(root, str);
-            } else {
-                printf("%d\n", queryStr(root, str));
+            if (op[0] == 'I')
+            {
+                insert_str(root, str);
+            }
+            else
+            {
+                printf("%d\n", query_str(root, str));
             }
         }
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0835_H
+#endif // ACWINGSOLUTION_PROBLEM0835_H
