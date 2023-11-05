3c912b89cbf2561a6ad95f5989a5063913760be9
fengwei <virusformail@gmail.com>
2023-11-05 12:28:01 -0800

    Reviewed Problem 838.

--- Problem0838.h
+++ Problem0838.h
@@ -10,60 +10,74 @@
 
 using namespace std;
 
-class Problem0838 {
-public:
-    void downNode(vector<int> &heap, unsigned long long rootIndex) {
-        auto minimalIndex = rootIndex;
-        while (true) {
-            if (rootIndex * 2 < heap.size() && heap[rootIndex * 2] < heap[minimalIndex]) {
-                minimalIndex = 2 * rootIndex;
+class Problem0838
+{
+private:
+    void down_node(vector<int> &heap, unsigned long long root_index)
+    {
+        auto minimal_index = root_index;
+        while (true)
+        {
+            if (root_index * 2 < heap.size() && heap[root_index * 2] < heap[minimal_index])
+            {
+                minimal_index = 2 * root_index;
             }
-            if (rootIndex * 2 + 1 < heap.size() && heap[rootIndex * 2 + 1] < heap[minimalIndex]) {
-                minimalIndex = 2 * rootIndex + 1;
+            if (root_index * 2 + 1 < heap.size() && heap[root_index * 2 + 1] < heap[minimal_index])
+            {
+                minimal_index = 2 * root_index + 1;
             }
-            if (rootIndex == minimalIndex) {
+            if (root_index == minimal_index)
+            {
                 return;
             }
-            swap(heap[rootIndex], heap[minimalIndex]);
-            rootIndex = minimalIndex;
+            swap(heap[root_index], heap[minimal_index]);
+            root_index = minimal_index;
         }
     }
 
-    void upNode(vector<int> &heap, unsigned long long rootIndex) {
-        while (rootIndex / 2 && heap[rootIndex / 2] > heap[rootIndex]) {
-            swap(heap[rootIndex / 2], heap[rootIndex]);
-            rootIndex /= 2;
+    void up_node(vector<int> &heap, unsigned long long root_index)
+    {
+        while (root_index / 2 && heap[root_index / 2] > heap[root_index])
+        {
+            swap(heap[root_index / 2], heap[root_index]);
+            root_index /= 2;
         }
     }
 
-    void buildHeap(vector<int> &heap) {
-        for (unsigned long long i = (heap.size() - 1) / 2; i > 0; --i) {
-            downNode(heap, i);
+    void build_heap(vector<int> &heap)
+    {
+        for (auto i = (heap.size() - 1) / 2; i > 0; --i)
+        {
+            down_node(heap, i);
         }
     }
 
-    int heapPop(vector<int> &heap) {
+    int heap_pop(vector<int> &heap)
+    {
         auto result = heap[1];
         heap[1] = heap[heap.size() - 1];
         heap.pop_back();
-        downNode(heap, 1);
+        down_node(heap, 1);
         return result;
     }
 
-    int main() {
+    int main()
+    {
         unsigned long long n, m;
         scanf("%lld%lld", &n, &m);
-        vector<int> heap(n + 1, 0);
-        for (unsigned long long i = 1; i <= n; ++i) {
+        vector<int> heap(n + 1);
+        for (unsigned long long i = 1; i <= n; ++i)
+        {
             scanf("%d", &heap[i]);
         }
-        buildHeap(heap);
-        for (unsigned long long i = 0; i < m; ++i) {
-            printf("%d ", heapPop(heap));
+        build_heap(heap);
+        for (unsigned long long i = 0; i < m; ++i)
+        {
+            printf("%d ", heap_pop(heap));
         }
         printf("\n");
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0838_H
+#endif // ACWINGSOLUTION_PROBLEM0838_H
