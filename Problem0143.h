b080e2b89e395a0ad1bfc7d7abcb7af25026e861
fengwei <virusformail@gmail.com>
2023-11-05 00:57:20 -0700

    Reviewed Problem 143.

--- Problem0143.h
+++ Problem0143.h
@@ -9,27 +9,36 @@
 
 using namespace std;
 
-class Problem0143 {
+class Problem0143
+{
 private:
-    struct Node {
+    struct Node
+    {
         Node *next[2]{};
 
-        Node() {
+        Node()
+        {
             next[0] = nullptr;
             next[1] = nullptr;
         }
     };
 
-    void insertNum(Node *root, const unsigned int num) {
-        auto p = root;
-        for (int i = 30; i >= 0; --i) {
-            if ((num >> i) & 1) {
-                if (!p->next[1]) {
+    void insert_num(Node *p, const int &num)
+    {
+        for (int i = 30; i >= 0; --i)
+        {
+            if ((num >> i) & 1)
+            {
+                if (!p->next[1])
+                {
                     p->next[1] = new Node();
                 }
                 p = p->next[1];
-            } else {
-                if (!p->next[0]) {
+            }
+            else
+            {
+                if (!p->next[0])
+                {
                     p->next[0] = new Node();
                 }
                 p = p->next[0];
@@ -37,23 +46,33 @@ private:
         }
     }
 
-    unsigned int maxXOR(Node *root, const unsigned int num) {
-        unsigned int result = 0;
-        auto p = root;
-        for (int i = 30; i >= 0; --i) {
+    int get_max_xor(Node *p, const int &num)
+    {
+        int result = 0;
+        for (int i = 30; i >= 0; --i)
+        {
             result *= 2;
-            if ((num >> i) & 1) {
-                if (p->next[0]) {
+            if ((num >> i) & 1)
+            {
+                if (p->next[0])
+                {
                     p = p->next[0];
                     result += 1;
-                } else {
+                }
+                else
+                {
                     p = p->next[1];
                 }
-            } else {
-                if (p->next[1]) {
+            }
+            else
+            {
+                if (p->next[1])
+                {
                     p = p->next[1];
                     result += 1;
-                } else {
+                }
+                else
+                {
                     p = p->next[0];
                 }
             };
@@ -61,20 +80,21 @@ private:
         return result;
     }
 
-    int main() {
+    int main()
+    {
         int n;
         scanf("%d", &n);
-        unsigned int num;
-        auto root = new Node();
-        unsigned int result = 0;
-        for (int i = 0; i < n; ++i) {
+        auto p = new Node();
+        int result = 0;
+        for (int i = 0, num; i < n; ++i)
+        {
             scanf("%d", &num);
-            insertNum(root, num);
-            result = max(result, maxXOR(root, num));
+            insert_num(p, num);
+            result = max(result, get_max_xor(p, num));
         }
         printf("%d\n", result);
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0143_H
+#endif // ACWINGSOLUTION_PROBLEM0143_H
