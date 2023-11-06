f71007218f2e337c2bff309d03c7f7dea8b9b235
fengwei <virusformail@gmail.com>
2023-11-05 19:23:06 -0800

    Reviewed Problem 845.

--- Problem0845.h
+++ Problem0845.h
@@ -12,44 +12,53 @@
 
 using namespace std;
 
-class Problem0845 {
+class Problem0845
+{
 private:
-    int bfs(const string &start, const string &end) {
+    int bfs(const string &start, const string &end)
+    {
         queue<string> q;
         unordered_map<string, int> dist;
         int dx[] = {0, 1, 0, -1};
         int dy[] = {1, 0, -1, 0};
         dist[start] = 0;
         q.emplace(start);
-        while (!q.empty()) {
+        while (!q.empty())
+        {
             auto node = q.front();
-            if (node == end) {
+            if (node == end)
+            {
                 return dist[node];
             }
             q.pop();
-            auto tagPos = (int) node.find('x');
-            auto currentDis = dist[node];
-            for (int i = 0; i < 4; ++i) {
-                auto nextX = tagPos / 3 + dx[i];
-                auto nextY = tagPos % 3 + dy[i];
-                if (nextX < 0 || nextX >= 3 || nextY < 0 || nextY >= 3) {
+            auto placeholder_idx = (int)node.find('x');
+            auto current_dist = dist[node];
+            for (int i = 0; i < 4; ++i)
+            {
+                auto next_x = placeholder_idx / 3 + dx[i];
+                auto next_y = placeholder_idx % 3 + dy[i];
+                if (next_x < 0 || next_x >= 3 || next_y < 0 || next_y >= 3)
+                {
                     continue;
                 }
-                swap(node[nextX * 3 + nextY], node[tagPos]);
-                if (dist.find(node) == dist.end()) {
+                swap(node[next_x * 3 + next_y], node[placeholder_idx]);
+                if (dist.find(node) == dist.end())
+                {
                     q.emplace(node);
-                    dist[node] = currentDis + 1;
+                    dist[node] = current_dist + 1;
                 }
-                swap(node[nextX * 3 + nextY], node[tagPos]);
+                swap(node[next_x * 3 + next_y], node[placeholder_idx]);
             }
         }
         return -1;
     }
 
-    int main() {
+    int main()
+    {
         string start(9, '\0');
         string end = "12345678x";
-        for (int i = 0; i < 9; ++i) {
+        for (int i = 0; i < 9; ++i)
+        {
             cin >> start[i];
         }
         printf("%d\n", bfs(start, end));
@@ -57,4 +66,4 @@ private:
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0845_H
+#endif // ACWINGSOLUTION_PROBLEM0845_H
