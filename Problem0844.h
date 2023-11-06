538fe030f8daec583d50e35e4e558f1cdf901aaf
fengwei <virusformail@gmail.com>
2023-11-05 16:13:39 -0800

    Reviewed Problem 844.

--- Problem0844.h
+++ Problem0844.h
@@ -5,40 +5,48 @@
 #ifndef ACWINGSOLUTION_PROBLEM0844_H
 #define ACWINGSOLUTION_PROBLEM0844_H
 
-#include <algorithm>
+#include <queue>
+#include <cstring>
 #include <iostream>
 
 using namespace std;
 
-class Problem0844 {
+class Problem0844
+{
+    // 反向BFS：从终点搜索到起点，和正向搜索等价
 private:
-    static const int N = 100;
-    int matrix[N][N];
-    bool visited[N][N];
-    pair<int, int> q[N * N + 10];
-
-    int bfs(const int n, const int m) {  // 反向BFS：从终点搜索到起点，和正向搜索等价
-        int dx[] = {0, 1, 0, -1}, dy[] = {-1, 0, 1, 0};
-        int hh = 0, tt = -1;
-        q[++tt] = pair<int, int>(n - 1, m - 1);
-        visited[n - 1][m - 1] = true;
+    int bfs(const int &n, const int &m, const int *matrix)
+    {
+        const int dx[] = {0, 1, 0, -1};
+        const int dy[] = {-1, 0, 1, 0};
+        bool visited[n * m];
         int result = 0;
-        while (hh <= tt) {
-            auto curL = tt - hh + 1;
-            while (curL--) {
-                auto root = q[hh++];
-                if (root.first == 0 && root.second == 0) {
+        queue<int> q;
+        memset(visited, 0, sizeof visited);
+        q.emplace(0);
+        visited[0] = true;
+        while (!q.empty())
+        {
+            for (auto candidates_num = (int)q.size(); candidates_num > 0; --candidates_num)
+            {
+                auto root = q.front();
+                q.pop();
+                if (root / m == n - 1 && root % m == m - 1)
+                {
                     return result;
                 }
-                for (int i = 0; i < 4; ++i) {
-                    auto nextX = root.first + dx[i];
-                    auto nextY = root.second + dy[i];
-                    if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m || matrix[nextX][nextY] == 1 ||
-                        visited[nextX][nextY]) {
+                for (int i = 0; i < 4; ++i)
+                {
+                    auto next_x = root / m + dx[i];
+                    auto next_y = root % m + dy[i];
+                    auto next_idx = next_x * m + next_y;
+                    if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= m ||
+                        matrix[next_idx] == 1 || visited[next_idx])
+                    {
                         continue;
                     }
-                    q[++tt] = pair<int, int>(nextX, nextY);
-                    visited[nextX][nextY] = true;
+                    q.emplace(next_idx);
+                    visited[next_idx] = true;
                 }
             }
             ++result;
@@ -46,17 +54,21 @@ private:
         return 0x7f7f7f7f;
     }
 
-    int main() {
+    int main()
+    {
         int n, m;
         scanf("%d%d", &n, &m);
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                scanf("%d", &matrix[i][j]);
+        int matrix[n * m];
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                scanf("%d", &matrix[i * m + j]);
             }
         }
-        printf("%d\n", bfs(n, m));
+        printf("%d\n", bfs(n, m, matrix));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0844_H
+#endif // ACWINGSOLUTION_PROBLEM0844_H
