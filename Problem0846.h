025489a99914e9d7e1995f70932e193ecde3c7e6
fengwei <virusformail@gmail.com>
2023-11-05 19:29:04 -0800

    Reviewed Problem 846.

--- Problem0846.h
+++ Problem0846.h
@@ -7,17 +7,22 @@
 
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-class Problem0846 {
+class Problem0846
+{
 private:
-    int dfs(int root, const int n, const vector<vector<int>> &graph, vector<bool> &visited, int &answer) {
+    int dfs(const int &root, const int &n, const vector<vector<int>> &graph, bool *visited, int &answer)
+    {
         visited[root] = true;
         int nodes_count = 1;
         int max_component = 0;
-        for (const auto &v: graph[root]) {
-            if (visited[v]) {
+        for (const auto &v : graph[root])
+        {
+            if (visited[v])
+            {
                 continue;
             }
             auto child_size = dfs(v, n, graph, visited, answer);
@@ -29,22 +34,24 @@ private:
         return nodes_count;
     }
 
-    int main() {
+    int main()
+    {
         int n;
         scanf("%d", &n);
-        vector<vector<int>> graph(n + 1, vector<int>());
-        int a, b;
-        for (int i = 0; i < n - 1; ++i) {
+        vector<vector<int>> graph(n + 1);
+        for (int i = 0, a, b; i < n - 1; ++i)
+        {
             scanf("%d%d", &a, &b);
             graph[a].emplace_back(b);
             graph[b].emplace_back(a);
         }
+        bool visited[n + 1];
         auto answer = n;
-        vector<bool> visited(n + 1, false);
+        memset(visited, 0, sizeof visited);
         dfs(1, n, graph, visited, answer);
         printf("%d\n", answer);
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0846_H
+#endif // ACWINGSOLUTION_PROBLEM0846_H
