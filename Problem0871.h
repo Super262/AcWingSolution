0d68fc73b7461d282b11f58622e71e5d25187966
fengwei <virusformail@gmail.com>
2023-11-12 20:29:17 -0800

    Reviewed Problem 871.

--- Problem0871.h
+++ Problem0871.h
@@ -10,42 +10,51 @@
 
 using namespace std;
 
-class Problem0871 {
+class Problem0871
+{
     // 牢记模版：https://www.acwing.com/solution/content/16981/
 private:
-    void get_factors(int n, unordered_map<int, int> &factor) {
-        for (int f = 2; f <= n / f; ++f) {
-            while (n % f == 0) {
+    void get_factors(int n, unordered_map<int, int> &factor)
+    {
+        for (int f = 2; f <= n / f; ++f)
+        {
+            while (n % f == 0)
+            {
                 n /= f;
                 ++factor[f];
             }
         }
-        if (n > 1) {
+        if (n > 1)
+        {
             ++factor[n];
         }
     }
 
-    int main() {
+    int main()
+    {
         int m;
         scanf("%d", &m);
         unordered_map<int, int> factors;
         int n;
-        while (m--) {
+        while (m--)
+        {
             scanf("%d", &n);
             get_factors(n, factors);
         }
         const int MOD = 1e9 + 7;
         int result = 1;
-        for (const auto &p: factors) {
+        for (const auto &p : factors)
+        {
             int temp = 1;
-            for (int k = 0; k < p.second; ++k) {
-                temp = (int) (((long long) temp * p.first + 1) % MOD);
+            for (int k = 0; k < p.second; ++k)
+            {
+                temp = (int)(((long long)temp * p.first + 1) % MOD);
             }
-            result = (int) ((long long) result * temp % MOD);
+            result = (int)((long long)result * temp % MOD);
         }
         printf("%d\n", result);
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0871_H
+#endif // ACWINGSOLUTION_PROBLEM0871_H
