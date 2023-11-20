821007694fd3cd45329ce7ad80ac9da07698b320
fengwei <virusformail@gmail.com>
2023-11-19 21:10:03 -0800

    Reviewed Problem 3.

--- Problem0003.h
+++ Problem0003.h
@@ -6,39 +6,51 @@
 #define ACWINGSOLUTION_PROBLEM0003_H
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-class Problem0003 {
+class Problem0003
+{
 private:
-    struct Item {
-        int size;
+    struct Item
+    {
         int value;
+        int size;
+
+        Item()
+        {
+            value = 0;
+            size = 0;
+        }
     };
 
-    static const int N = 1010;
-    static const int M = 1010;
-    Item items[N];
-    int dp[M];
-
-    int knapsack(const int v, const int n) {
-        for (int i = 0; i < n; ++i) {
-            for (auto j = items[i].size; j <= v; ++j) {
+    int knapsack_max_value(const int &n, const int &v, const Item *items)
+    {
+        int dp[v + 1];
+        memset(dp, 0, sizeof dp);
+        for (int i = 0; i < n; ++i)
+        {
+            for (auto j = items[i].size; j <= v; ++j)
+            {
                 dp[j] = max(dp[j], dp[j - items[i].size] + items[i].value);
             }
         }
         return dp[v];
     }
 
-    int main() {
+    int main()
+    {
         int n, v;
         scanf("%d%d", &n, &v);
-        for (int i = 0; i < n; ++i) {
+        Item items[n];
+        for (int i = 0; i < n; ++i)
+        {
             scanf("%d%d", &items[i].size, &items[i].value);
         }
-        printf("%d\n", knapsack(v, n));
+        printf("%d\n", knapsack_max_value(n, v, items));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0003_H
+#endif // ACWINGSOLUTION_PROBLEM0003_H
