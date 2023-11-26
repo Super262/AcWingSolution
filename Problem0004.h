a5a9b0e1bbca7b25c8e8ed6ef185e6eeed031ec7
fengwei <virusformail@gmail.com>
2023-11-26 03:34:00 -0800

    Reviewed Problem 4.

--- Problem0004.h
+++ Problem0004.h
@@ -6,42 +6,66 @@
 #define ACWINGSOLUTION_PROBLEM0004_H
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-class Problem0004 {
+class Problem0004
+{
 private:
-    struct Item {
+    struct Item
+    {
         int size;
         int value;
         int num;
+
+        Item()
+        {
+            value = 0;
+            size = 0;
+            num = 0;
+        }
     };
 
-    static const int N = 110;
-    static const int M = 110;
-    Item items[N];
-    int dp[N][M];
-
-    int knapsack(const int n, const int v) {
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= v; ++j) {
-                for (int k = 0; k <= items[i].num && k * items[i].size <= j; ++k) {
+    int knapsack_max_value(const int &n, const int &v, const Item *items)
+    {
+        int **dp = new int *[n + 1];
+        for (int i = 0; i <= n; ++i)
+        {
+            dp[i] = new int[v + 1];
+            memset(dp[i], 0, sizeof(int) * (v + 1));
+        }
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= v; ++j)
+            {
+                for (int k = 0; k <= items[i].num && k * items[i].size <= j; ++k)
+                {
                     dp[i][j] = max(dp[i][j], dp[i - 1][j - k * items[i].size] + k * items[i].value);
                 }
             }
         }
-        return dp[n][v];
+        auto answer = dp[n][v];
+        for (int i = 0; i <= n; ++i)
+        {
+            delete[] dp[i];
+        }
+        delete[] dp;
+        return answer;
     }
 
-    int main() {
+    int main()
+    {
         int n, m;
         scanf("%d%d", &n, &m);
-        for (int i = 1; i <= n; ++i) {
+        Item items[n];
+        for (int i = 1; i <= n; ++i)
+        {
             scanf("%d%d%d", &items[i].size, &items[i].value, &items[i].num);
         }
-        printf("%d\n", knapsack(n, m));
+        printf("%d\n", knapsack_max_value(n, m, items));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0004_H
+#endif // ACWINGSOLUTION_PROBLEM0004_H
