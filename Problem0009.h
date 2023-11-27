1ba40d55e68751f9d6458b68cd56b3007dc3f862
fengwei <virusformail@gmail.com>
2023-11-26 21:54:49 -0800

    Reviewed Problem 9.

--- Problem0009.h
+++ Problem0009.h
@@ -6,56 +6,66 @@
 #define ACWINGSOLUTION_PROBLEM0009_H
 
 #include <iostream>
+#include <cstring>
+#include <vector>
 
 using namespace std;
 
-class Problem0009 {
+class Problem0009
+{
 private:
-    struct Item {
+    struct Item
+    {
         int size;
         int value;
 
-        Item() {
+        Item()
+        {
             size = -1;
             value = 0;
         }
     };
 
-    Item items[110][110];
-    int dp[110];
-
-    int knapsack(const int kinds, const int m) {
+    int knapsack_max_value(const int &n, const int &v, const vector<vector<Item>> &items)
+    {
         // dp[i][j]表示只从前i组物品中选，总体积不大于j的所有选法，
         // 所以注意这里的循环顺序：组、体积、物品
-        for (int i = 0; i < kinds; ++i) {
-            for (int v = m; v >= 0; --v) {
-                for (int j = 0; items[i][j].size != -1; ++j) {
-                    if (v < items[i][j].size) {
+        int dp[v + 1];
+        memset(dp, 0, sizeof dp);
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = v; j >= 0; --j)
+            {
+                for (int k = 0; k < items[i].size(); ++k)
+                {
+                    if (j < items[i][k].size)
+                    {
                         continue;
                     }
-                    dp[v] = max(dp[v], dp[v - items[i][j].size] + items[i][j].value);
+                    dp[j] = max(dp[j], dp[j - items[i][k].size] + items[i][k].value);
                 }
             }
         }
-        return dp[m];
+        return dp[v];
     }
 
-    int main() {
+    int main()
+    {
         int n, v;
         scanf("%d%d", &n, &v);
-        for (int i = 0; i < n; ++i) {
-            int k;
+        vector<vector<Item>> items(n);
+        for (int i = 0, k; i < n; ++i)
+        {
             scanf("%d", &k);
-            for (int j = 0; j < k; ++j) {
-                int a, b;
-                scanf("%d%d", &a, &b);
-                items[i][j].size = a;
-                items[i][j].value = b;
+            items[i].resize(k);
+            for (int j = 0; j < k; ++j)
+            {
+                scanf("%d%d", &items[i][j].size, &items[i][j].value);
             }
         }
-        printf("%d\n", knapsack(n, v));
+        printf("%d\n", knapsack_max_value(n, v, items));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0009_H
+#endif // ACWINGSOLUTION_PROBLEM0009_H
