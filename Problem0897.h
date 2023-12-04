cc4462403a0193bb65a5b78d2eb6cca4d359e75a
fengwei <virusformail@gmail.com>
2023-12-03 20:11:54 -0800

    Reviewed Problem 897.

--- Problem0897.h
+++ Problem0897.h
@@ -6,21 +6,23 @@
 #define ACWINGSOLUTION_PROBLEM0897_H
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Problem0897 {
+class Problem0897
+{
 private:
-    static const int N = 1010;
-    char s1[N];
-    char s2[N];
-    int dp[N][N];
-
-    int longestCommonSequence(const int l1, const int l2) {
-        for (int i = 1; i <= l1; ++i) {
-            for (int j = 1; j <= l2; ++j) {
+    int longest_common_sequence(const char *s1, const int &l1, const char *s2, const int &l2)
+    {
+        vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1));
+        for (int i = 1; i <= l1; ++i)
+        {
+            for (int j = 1; j <= l2; ++j)
+            {
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                if (s1[i] != s2[j]) {
+                if (s1[i] != s2[j])
+                {
                     continue;
                 }
                 dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
@@ -29,14 +31,17 @@ private:
         return dp[l1][l2];
     }
 
-    int main() {
+    int main()
+    {
         int l1, l2;
         scanf("%d%d", &l1, &l2);
+        char s1[l1 + 2];
+        char s2[l2 + 2];
         scanf("%s", s1 + 1);
         scanf("%s", s2 + 1);
-        printf("%d\n", longestCommonSequence(l1, l2));
+        printf("%d\n", longest_common_sequence(s1, l1, s2, l2));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0897_H
+#endif // ACWINGSOLUTION_PROBLEM0897_H
