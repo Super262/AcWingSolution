57ddb5ae440fa5e6d9009e0ec237632e475adb44
fengwei <virusformail@gmail.com>
2023-12-03 17:05:02 -0800

    Reviewed Problem 895.

--- Problem0895.h
+++ Problem0895.h
@@ -9,36 +9,44 @@
 
 using namespace std;
 
-class Problem0895 {
-public:
-    int longestAscendingSubsequence(const int *arr, const int n) {
+class Problem0895
+{
+private:
+    int longest_ascending_subsequence(const int *arr, const int n)
+    {
         int dp[n];
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             dp[i] = 1;
-            for (int j = 0; j < i; ++j) {
-                if (arr[j] >= arr[i]) {
+            for (int j = 0; j < i; ++j)
+            {
+                if (arr[j] >= arr[i])
+                {
                     continue;
                 }
                 dp[i] = max(dp[i], dp[j] + 1);
             }
         }
         int result = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             result = max(result, dp[i]);
         }
         return result;
     }
 
-    int main() {
+    int main()
+    {
         int n;
         scanf("%d", &n);
         int arr[n];
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             scanf("%d", &arr[i]);
         }
-        printf("%d\n", longestAscendingSubsequence(arr, n));
+        printf("%d\n", longest_ascending_subsequence(arr, n));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0895_H
+#endif // ACWINGSOLUTION_PROBLEM0895_H
