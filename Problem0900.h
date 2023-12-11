b3cbecf8a0b491769c18bdf0c76ce3fa3c21eb3d
fengwei <virusformail@gmail.com>
2023-12-10 17:49:09 -0800

    Reviewed Problem 900.

--- Problem0900.h
+++ Problem0900.h
@@ -10,27 +10,32 @@
 
 using namespace std;
 
-class Problem0900 {
+class Problem0900
+{
     // 类似“完全背包”问题
 private:
-    int combinationsOfSum(const int sum) {
-        int dp[sum + 1];
-        memset(dp, 0, sizeof dp);
-        dp[0] = 1;  // 不要忘记初始化！
-        for (int a = 1; a <= sum; ++a) {
-            for (auto b = a; b <= sum; ++b) {
-                dp[b] = (dp[b] + dp[b - a]) % 1000000007;
+    int count_division(const int &n)
+    {
+        int f[n + 1];
+        memset(f, 0, sizeof f);
+        f[0] = 1; // 不要忘记初始化！
+        for (int a = 1; a <= n; ++a)
+        {
+            for (auto b = a; b <= n; ++b)
+            {
+                f[b] = (f[b] + f[b - a]) % 1000000007;
             }
         }
-        return dp[sum];
+        return f[n];
     }
 
-    int main() {
+    int main()
+    {
         int n;
         scanf("%d", &n);
-        printf("%d\n", combinationsOfSum(n));
+        printf("%d\n", count_division(n));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0900_H
+#endif // ACWINGSOLUTION_PROBLEM0900_H
