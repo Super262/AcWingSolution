a844fc90e1a597576db7e8da35eb8131946f0114
fengwei <virusformail@gmail.com>
2023-12-20 04:34:19 -0800

    Reviewed Problem 906.

--- Problem0906.h
+++ Problem0906.h
@@ -11,50 +11,61 @@
 
 using namespace std;
 
-class Problem0906 {
+class Problem0906
+{
     // 1. 将所有区间按照左端点从小到大排序
-    // 2. 从前向后处理每个区间：判断能否将其放到某个现有的组i中（left > minRight[i]）
-    // 3. 如果不存在这样的组，就开设一个新组；如果存在这样的组j，则将其放入并更新minRight[j]
+    // 2. 从前向后处理每个区间：判断能否将其放到某个现有的组i中（left > min_right[i]）
+    // 3. 如果不存在这样的组，就开设一个新组；如果存在这样的组j，则将其放入并更新min_right[j]
 private:
-    struct Range {
+    struct Range
+    {
         int left;
         int right;
 
-        bool operator<(const Range &b) const {
+        bool operator<(const Range &b) const
+        {
             return left < b.left;
         }
     };
 
-    int minPartition(Range ranges[], const int n) {
+    int min_partition(Range *ranges, const int &n)
+    {
         sort(ranges, ranges + n);
         priority_queue<int, vector<int>, greater<int>> pq;
-        for (int i = 0; i < n; ++i) {
-            if (pq.empty()) {
+        for (int i = 0; i < n; ++i)
+        {
+            if (pq.empty())
+            {
                 pq.emplace(ranges[i].right);
                 continue;
             }
-            if (pq.top() >= ranges[i].left) {
+            if (pq.top() >= ranges[i].left)
+            {
                 // 不存在这样的组，创建新组
                 pq.emplace(ranges[i].right);
-            } else {
+            }
+            else
+            {
                 // 存在这样的组，更新最小值
                 pq.pop();
                 pq.emplace(ranges[i].right);
             }
         }
-        return (int) pq.size();
+        return (int)pq.size();
     }
 
-    int main() {
+    int main()
+    {
         int n;
         scanf("%d", &n);
         Range ranges[n];
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             scanf("%d%d", &ranges[i].left, &ranges[i].right);
         }
-        printf("%d\n", minPartition(ranges, n));
+        printf("%d\n", min_partition(ranges, n));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0906_H
+#endif // ACWINGSOLUTION_PROBLEM0906_H
