77bfd17d8d85aa6b991d07e6af0068c598078f9c
fengwei <virusformail@gmail.com>
2024-01-07 03:09:33 -0800

    Reviewed Problem 125.

--- Problem0125.h
+++ Problem0125.h
@@ -10,41 +10,48 @@
 
 using namespace std;
 
-class Problem0125 {
+class Problem0125
+{
 private:
-    struct WSPair {
+    struct WSPair
+    {
         int weight;
         int strength;
 
-        bool operator<(const WSPair &b) const {
+        bool operator<(const WSPair &b) const
+        {
             return weight + strength < b.weight + b.strength;
         }
     };
 
-    int minRisk(WSPair *cows, const unsigned int n) {
-        if (!cows || n == 0) {
+    int min_risk(WSPair *cows, const int &n)
+    {
+        if (!cows || n == 0)
+        {
             return 0;
         }
         sort(cows, cows + n);
-        auto result = -cows[0].strength;  // 注意result的初始值！
-        auto weightSum = cows[0].weight;
-        for (unsigned int i = 1; i < n; ++i) {
-            result = max(result, weightSum - cows[i].strength);
-            weightSum += cows[i].weight;
+        auto result = -cows[0].strength; // 注意result的初始值！
+        for (int i = 1, weight_sum = cows[0].weight; i < n; ++i)
+        {
+            result = max(result, weight_sum - cows[i].strength);
+            weight_sum += cows[i].weight;
         }
         return result;
     }
 
-    int main() {
-        unsigned int n;
+    int main()
+    {
+        int n;
         scanf("%d", &n);
         WSPair cows[n];
-        for (unsigned int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             scanf("%d%d", &cows[i].weight, &cows[i].strength);
         }
-        printf("%d\n", minRisk(cows, n));
+        printf("%d\n", min_risk(cows, n));
         return 0;
     }
 };
 
-#endif //ACWINGSOLUTION_PROBLEM0125_H
+#endif // ACWINGSOLUTION_PROBLEM0125_H
