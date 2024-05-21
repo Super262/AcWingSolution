526ad9e55033bc1b0ad673d146a187c96f34fbdb
fengwei <virusformail@gmail.com>
2024-05-21 01:25:49 -0700

    Updated.

--- Problem0798.h
+++ Problem0798.h
@@ -13,7 +13,8 @@ using namespace std;
 class Problem0798
 {
 private:
-    void matrix_increase(vector<vector<int>> &diff, const int &x1, const int &y1, const int &x2, const int &y2, const int &v)
+    void MatrixIncrement(vector<vector<int>> &diff, const int &x1, const int &y1,
+                         const int &x2, const int &y2, const int &v)
     {
         diff[x1][y1] += v;
         diff[x2 + 1][y1] -= v;
@@ -27,32 +28,32 @@ private:
         scanf("%d%d%d", &n, &m, &q);
 
         // 行数至少为n+2，列数至少为m+2；否则，触发越界异常！
-        vector<vector<int>> matrix(n + 2, vector<int>(m + 2, 0));
+        vector<vector<int>> diff(n + 2, vector<int>(m + 2, 0));
         for (int i = 1, t; i <= n; ++i)
         {
             for (int j = 1; j <= m; ++j)
             {
                 scanf("%d", &t);
-                matrix_increase(matrix, i, j, i, j, t);
+                MatrixIncrement(diff, i, j, i, j, t);
             }
         }
         for (int i = 0, x1, y1, x2, y2, t; i < q; ++i)
         {
             scanf("%d%d%d%d%d", &x1, &y1, &x2, &y2, &t);
-            matrix_increase(matrix, x1, y1, x2, y2, t);
+            MatrixIncrement(diff, x1, y1, x2, y2, t);
         }
         for (int i = 1; i <= n; ++i)
         {
             for (int j = 1; j <= m; ++j)
             {
-                matrix[i][j] += matrix[i - 1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1];
+                diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1];
             }
         }
         for (int i = 1; i <= n; ++i)
         {
             for (int j = 1; j <= m; ++j)
             {
-                printf("%d ", matrix[i][j]);
+                printf("%d ", diff[i][j]);
             }
             printf("\n");
         }
