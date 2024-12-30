5f5be888ca4f0bcce77f9cbfe942dae369c4e0ca
Super262 <fengweiz929@yahoo.com>
2024-12-29 16:34:44 -0800

    Updated 785.

--- Problem0785.h
+++ Problem0785.h
@@ -26,8 +26,16 @@ private:
         auto r = ed + 1;
         while (l < r)
         {
-            while (nums[++l] < p); // 注意：循环条件不包含 l < r
-            while (nums[--r] > p);
+            ++l;
+            while (nums[l] < p)
+            {
+                ++l;
+            }
+            --r;
+            while (nums[r] > p)
+            {
+                --r;
+            }
             if (l < r)
             {
                 swap(nums[l], nums[r]);
