f68694f6b369070a07e1db44d4dfaf9007d06421
Super262 <fengweiz929@yahoo.com>
2024-12-31 00:44:28 -0800

    Updated 796.

--- Problem0796.h
+++ Problem0796.h
@@ -5,47 +5,73 @@
 #ifndef ACWINGSOLUTION_PROBLEM0796_H
 #define ACWINGSOLUTION_PROBLEM0796_H
 
-#include <vector>
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
+#include <cstdlib>
 
 class Problem0796
 {
 private:
-    vector<vector<int>> GetPrefixSum(const vector<vector<int>> &matrix)
+    void getPrefixSum(int **matrix, size_t n, size_t m, int **prefix_sum)
     {
-        vector<vector<int>> res(matrix.size() + 1, vector<int>(matrix[0].size() + 1, 0));
-        for (int i = 1; i < res.size(); ++i)
+        for (size_t i = 1; i <= n; ++i)
         {
-            for (int j = 1; j < res[0].size(); ++j)
+            for (size_t j = 1; j <= m; ++j)
             {
-                res[i][j] = res[i - 1][j] + res[i][j - 1]
-                            - res[i - 1][j - 1] + matrix[i - 1][j - 1];
+                prefix_sum[i][j] = prefix_sum[i - 1][j] + prefix_sum[i][j - 1] - prefix_sum[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
-        return res;
     }
 
     int main()
     {
-        int n, m, q;
-        scanf("%d%d%d", &n, &m, &q);
-        vector<vector<int>> matrix(n, vector<int>(m));
-        for (int i = 0; i < n; ++i)
+        size_t n, m, q;
+        scanf("%llu%llu%llu", &n, &m, &q);
+
+        int **matrix = (int **)malloc(n * sizeof(int *));
+        for (size_t i = 0; i < n; ++i)
         {
-            for (int j = 0; j < m; ++j)
+            matrix[i] = (int *)malloc(m * sizeof(int));
+            for (size_t j = 0; j < m; ++j)
             {
                 scanf("%d", &matrix[i][j]);
             }
         }
-        const auto &matrix_prefix = GetPrefixSum(matrix);
-        for (int i = 0, x1, y1, x2, y2; i < q; ++i)
+
+        int **prefix_sum = (int **)malloc((n + 1) * sizeof(int *));
+        for (size_t i = 0; i < n + 1; ++i)
         {
-            scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
-            printf("%d\n", matrix_prefix[x2][y2] - matrix_prefix[x1 - 1][y2]
-                           - matrix_prefix[x2][y1 - 1] + matrix_prefix[x1 - 1][y1 - 1]);
+            // 将prefix_sum第0行和第0列所有元素初始化为0
+            if (i)
+            {
+                prefix_sum[i] = (int *)malloc((m + 1) * sizeof(int));
+                prefix_sum[i][0] = 0;
+            }
+            else
+            {
+                prefix_sum[i] = (int *)calloc(m + 1, sizeof(int));
+            }
         }
+
+        getPrefixSum(matrix, n, m, prefix_sum);
+
+        for (size_t i = 0, x1, y1, x2, y2; i < q; ++i)
+        {
+            scanf("%llu%llu%llu%llu", &x1, &y1, &x2, &y2);
+            printf("%d\n", prefix_sum[x2][y2] - prefix_sum[x1 - 1][y2] - prefix_sum[x2][y1 - 1] + prefix_sum[x1 - 1][y1 - 1]);
+        }
+
+        for (size_t i = 0; i < n; ++i)
+        {
+            free(matrix[i]);
+        }
+        free(matrix);
+
+        for (size_t i = 0; i < n + 1; ++i)
+        {
+            free(prefix_sum[i]);
+        }
+        free(prefix_sum);
+
         return 0;
     }
 };
