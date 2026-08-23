ef99c341537fe14259d8428dccc5b5d512bc881e
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-08-23 08:22:48 -0700

    Refactor MergeSegments for improved clarity

--- Problem0803.h
+++ Problem0803.h
@@ -13,35 +13,30 @@ using namespace std;
 
 class Problem0803
 {
-    // 贪心算法：左端点排序
+    /* 贪心算法：左端点排序 */
+    /* 如果不需要输出合并后的所有区间，我们可以省去result和start，*/
+    /* 只记录end和结果集的大小 */
 private:
     vector<pair<int, int>> MergeSegments(vector<pair<int, int>> &segs)
     {
-        sort(segs.begin(), segs.end());
-        const int invalid_value = -2e9;
         vector<pair<int, int>> result;
-        auto start = invalid_value;
-        auto end = invalid_value;
+        if (!segs.size())
+            return result;
+        sort(segs.begin(), segs.end());
+        auto start = segs[0].first;
+        auto end = segs[0].second;
         for (const auto &s : segs)
         {
             if (s.first > end)
             {
-                if (end != invalid_value)
-                {
-                    result.emplace_back(pair<int, int>(start, end));
-                }
+                result.emplace_back(pair<int, int>(start, end));
                 start = s.first;
                 end = s.second;
             }
             else
-            {
                 end = max(end, s.second);
-            }
         }
-        if (end != invalid_value)
-        {
-            result.emplace_back(pair<int, int>(start, end));
-        }
+        result.emplace_back(pair<int, int>(start, end));
         return result;
     }
 
@@ -51,9 +46,7 @@ private:
         scanf("%d", &n);
         vector<pair<int, int>> segs(n);
         for (int i = 0; i < n; ++i)
-        {
             scanf("%d%d", &segs[i].first, &segs[i].second);
-        }
         printf("%d\n", (int)MergeSegments(segs).size());
         return 0;
     }
