6ef6a30abab5ac3a0569b3457c6c9296ab7392b8
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-04 14:19:39 -0700

    Refactor SPFA function for better memory management

--- Problem0852.h
+++ Problem0852.h
@@ -17,70 +17,80 @@ class Problem0852
     // 1. 虚拟源点
     // 2. 最短路径经过的边数大于等于n，有负环
 private:
-    bool spfa(const int &n, const vector<vector<pair<int, int>>> &graph)
+    bool spfa(int n, const vector<vector<pair<int, int>>> &graph)
     {
-        int dist[n + 1];
-        bool in_queue[n + 1];
-        int edges_cnt[n + 1];
         queue<int> q;
-        memset(in_queue, 0, sizeof in_queue);
-        memset(edges_cnt, 0, sizeof edges_cnt);
-        for (int v = 1; v <= n; ++v)
+        int *dist = (int *)malloc(sizeof(int) * (n + 1));
+        int *counter = (int *)malloc(sizeof(int) * (n + 1));
+        bool *in_queue = (bool *)malloc(sizeof(bool) * (n + 1));
+        bool answer = false;
+        
+        /* 初始化虚拟原点：所有点入队；所有路径初始长度为0、边数为1 */
+        for (int i = 1; i <= n; ++i)
         {
-            q.emplace(v);
-            dist[v] = 0;
-            in_queue[v] = true;
+            dist[i] = 0;
+            counter[i] = 1;
+            q.emplace(i);
+            in_queue[i] = true;
         }
+        
         while (!q.empty())
         {
-            auto root = q.front();
+            int root = q.front();
             q.pop();
             in_queue[root] = false;
-            for (const auto &t : graph[root])
+    
+            for (const pair<int, int> &p : graph[root])
             {
-                auto nd = t.first;
-                auto nv = t.second;
-                if (dist[nv] <= dist[root] + nd)
+                if (dist[p.second] > dist[root] + p.first)
                 {
-                    continue;
+                    /* 发现负环：前序路径边数为n */
+                    if (counter[root] == n)
+                    {
+                        answer = true;
+                        break;
+                    }
+    
+                    dist[p.second] = dist[root] + p.first;
+                    counter[p.second] = counter[root] + 1;
+    
+                    if (!in_queue[p.second])
+                    {
+                        q.emplace(p.second);
+                        in_queue[p.second] = true;
+                    }
                 }
-                dist[nv] = dist[root] + nd;
-                edges_cnt[nv] = edges_cnt[root] + 1;
-                if (edges_cnt[nv] >= n)
-                {
-                    return true;
-                }
-                if (in_queue[nv])
-                {
-                    continue;
-                }
-                q.emplace(nv);
-                in_queue[nv] = true;
             }
+            
+            if (answer)
+                break;
         }
-        return false;
+        
+        free(in_queue);
+        free(counter);
+        free(dist);
+        return answer;
     }
-
+    
     int main()
     {
         int n, m;
         scanf("%d%d", &n, &m);
         vector<vector<pair<int, int>>> graph(n + 1);
-        for (int i = 0, u, v, w; i < m; ++i)
+        
+        for (int i = 0, x, y, z; i < m; ++i)
         {
-            scanf("%d%d%d", &u, &v, &w);
-            graph[u].push_back({w, v});
+            scanf("%d%d%d", &x, &y, &z);
+            graph[x].emplace_back(z, y);
         }
+
         if (spfa(n, graph))
-        {
             printf("Yes\n");
-        }
         else
-        {
             printf("No\n");
-        }
+    
         return 0;
-    }
+}
 };
 
 #endif // ACWINGSOLUTION_PROBLEM0852_H
