7611c3f9f1c95154bee29374def7dd8e82f56dad
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-06 16:22:17 -0700

    Refactor prim function and update main logic

--- Problem0858.h
+++ Problem0858.h
@@ -14,70 +14,101 @@ using namespace std;
 class Problem0858
 {
 private:
-    static const int INF = 0x3f3f3f3f;
-
-    int prim(const int &n, const vector<vector<int>> &graph)
+    bool prim(const vector<vector<int>> &graph,
+              const vector<vector<bool>> &connected,
+              int n,
+              int &answer)
     {
-        int result = 0;
-        int dist[n + 1];
-        bool selected[n + 1];
-        memset(dist, 0x3f, sizeof dist);
-        memset(selected, 0, sizeof selected);
-        for (int k = 1; k <= n; ++k)
+        bool *selected = (bool *)calloc(n + 1, sizeof(bool));
+        bool *reachable = (bool *)calloc(n + 1, sizeof(bool));
+        int *dist = (int *)malloc(sizeof(int) * (n + 1));
+        int temp_result = 0;
+    
+        // Start from vertex 1
+        reachable[1] = true;
+        dist[1] = 0;
+    
+        for (int k = 0; k < n; ++k)
         {
             int closest_v = -1;
-            for (int v = 1; v <= n; ++v)
+    
+            for (int i = 1; i <= n; ++i)
             {
-                if (selected[v])
-                {
+                if (selected[i] || !reachable[i])
                     continue;
-                }
-                if (closest_v == -1 || dist[closest_v] >= dist[v])
+    
+                if (closest_v == -1 ||
+                    dist[i] < dist[closest_v])
                 {
-                    closest_v = v;
+                    closest_v = i;
                 }
             }
+    
             if (closest_v == -1)
-            { // 不要忘记这个判断
-                return INF;
+            {
+                free(selected);
+                free(reachable);
+                free(dist);
+                return false;
             }
-            if (k > 1 && dist[closest_v] == INF)
-            { // 不要忘记这个判断
-                return INF;
-            }
+    
             selected[closest_v] = true;
-            if (k > 1)
+            temp_result += dist[closest_v];
+    
+            for (int i = 1; i <= n; ++i)
             {
-                result += dist[closest_v];
-            }
-            for (int v = 1; v <= n; ++v)
-            {
-                dist[v] = min(dist[v], graph[closest_v][v]);
+                if (!connected[closest_v][i] || selected[i])
+                    continue;
+    
+                if (!reachable[i])
+                {
+                    reachable[i] = true;
+                    dist[i] = graph[closest_v][i];
+                }
+                else
+                    dist[i] = min(dist[i], graph[closest_v][i]);
             }
         }
-        return result;
+    
+        answer = temp_result;
+    
+        free(selected);
+        free(reachable);
+        free(dist);
+        return true;
     }
-
+    
     int main()
     {
         int n, m;
         scanf("%d%d", &n, &m);
-        vector<vector<int>> graph(n + 1, vector<int>(n + 1, INF));
+    
+        vector<vector<int>> graph(n + 1, vector<int>(n + 1));
+        vector<vector<bool>> connected(n + 1, vector<bool>(n + 1, false));
+    
         for (int i = 0, u, v, w; i < m; ++i)
         {
             scanf("%d%d%d", &u, &v, &w);
-            graph[u][v] = min(graph[u][v], w);
-            graph[v][u] = min(graph[v][u], w);
+            if (!connected[u][v] || !connected[v][u])
+            {
+                graph[u][v] = w;
+                graph[v][u] = w;
+                connected[u][v] = true;
+                connected[v][u] = true;
+            }
+            else
+            {
+                graph[u][v] = min(w, graph[u][v]);
+                graph[v][u] = min(w, graph[v][u]);
+            }
         }
-        const auto &result = prim(n, graph);
-        if (result == INF)
-        {
-            printf("impossible\n");
-        }
+    
+        int answer;
+        if (prim(graph, connected, n, answer))
+            printf("%d\n", answer);
         else
-        {
-            printf("%d\n", result);
-        }
+            printf("impossible\n");
+    
         return 0;
     }
 };
