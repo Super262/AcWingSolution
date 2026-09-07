a4a6789ae73c6ca10e9c11da6fa1e13f9c6469a3
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-06 22:57:45 -0700

    Refactor prim function to use arrays instead of vectors

--- Problem0858.h
+++ Problem0858.h
@@ -7,24 +7,20 @@
 
 #include <iostream>
 #include <cstring>
-#include <vector>
 
 using namespace std;
 
 class Problem0858
 {
 private:
-    bool prim(const vector<vector<int>> &graph,
-              const vector<vector<bool>> &connected,
-              int n,
-              int &answer)
+    bool prim(int *graph, bool *connected, int n, int *answer)
     {
         bool *selected = (bool *)calloc(n + 1, sizeof(bool));
         bool *reachable = (bool *)calloc(n + 1, sizeof(bool));
         int *dist = (int *)malloc(sizeof(int) * (n + 1));
         int temp_result = 0;
     
-        // Start from vertex 1
+        /* Start from vertex 1 */
         reachable[1] = true;
         dist[1] = 0;
     
@@ -57,20 +53,20 @@ private:
     
             for (int i = 1; i <= n; ++i)
             {
-                if (!connected[closest_v][i] || selected[i])
+                if (!connected[closest_v * (n + 1) + i] || selected[i])
                     continue;
     
                 if (!reachable[i])
                 {
                     reachable[i] = true;
-                    dist[i] = graph[closest_v][i];
+                    dist[i] = graph[closest_v * (n + 1) + i];
                 }
                 else
-                    dist[i] = min(dist[i], graph[closest_v][i]);
+                    dist[i] = min(dist[i], graph[closest_v * (n + 1) + i]);
             }
         }
     
-        answer = temp_result;
+        *answer = temp_result;
     
         free(selected);
         free(reachable);
@@ -82,33 +78,37 @@ private:
     {
         int n, m;
         scanf("%d%d", &n, &m);
-    
-        vector<vector<int>> graph(n + 1, vector<int>(n + 1));
-        vector<vector<bool>> connected(n + 1, vector<bool>(n + 1, false));
+        
+        /* 用等体积的数组表示（映射）矩阵 */
+        int *graph = (int *)malloc((n + 1) * (n + 1) * sizeof(int));
+        bool *connected = (bool *)calloc((n + 1) * (n + 1), sizeof(bool));
     
         for (int i = 0, u, v, w; i < m; ++i)
         {
             scanf("%d%d%d", &u, &v, &w);
-            if (!connected[u][v] || !connected[v][u])
+            if (!connected[u * (n + 1) + v] ||
+                !connected[v * (n + 1) + u])
             {
-                graph[u][v] = w;
-                graph[v][u] = w;
-                connected[u][v] = true;
-                connected[v][u] = true;
+                graph[u * (n + 1) + v] = w;
+                graph[v * (n + 1) + u] = w;
+                connected[u * (n + 1) + v] = true;
+                connected[v * (n + 1) + u] = true;
             }
             else
             {
-                graph[u][v] = min(w, graph[u][v]);
-                graph[v][u] = min(w, graph[v][u]);
+                graph[u * (n + 1) + v] = min(w, graph[u * (n + 1) + v]);
+                graph[v * (n + 1) + u] = graph[u * (n + 1) + v];
             }
         }
     
         int answer;
-        if (prim(graph, connected, n, answer))
+        if (prim(graph, connected, n, &answer))
             printf("%d\n", answer);
         else
             printf("impossible\n");
-    
+            
+        free(graph);
+        free(connected);
         return 0;
     }
 };
