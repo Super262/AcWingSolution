cd59f013ab1d070f210ed910dc97e9c1d865dd40
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-08 15:24:24 -0700

    Refactor has_matched function and update main logic

--- Problem0861.h
+++ Problem0861.h
@@ -14,16 +14,22 @@ using namespace std;
 class Problem0861
 {
 private:
-    bool has_matched(const int &u, const vector<vector<int>> &graph, int *friend_from_left, bool *right_visited)
+    bool has_matched(int u,
+                     int stamp,
+                     const vector<vector<int>> &graph,
+                     int *friend_from_left,
+                     int *right_visited)
     {
         for (const auto &v : graph[u])
         {
-            if (right_visited[v])
-            {
+            if (right_visited[v] == stamp) // 检查时间戳
                 continue;
-            }
-            right_visited[v] = true;
-            if (friend_from_left[v] == 0 || has_matched(friend_from_left[v], graph, friend_from_left, right_visited))
+
+            right_visited[v] = stamp;
+            if (!friend_from_left[v] ||
+                has_matched(friend_from_left[v], stamp, 
+                            graph, friend_from_left,
+                            right_visited))
             {
                 friend_from_left[v] = u;
                 return true;
@@ -36,25 +42,29 @@ private:
     {
         int n1, n2, m;
         scanf("%d%d%d", &n1, &n2, &m);
+
         vector<vector<int>> graph(n1 + 1);
+        int *friend_from_left = (int *)calloc(n2 + 1, sizeof(int));
+        int *right_visited = (int *)calloc(n2 + 1, sizeof(int));
+        int result = 0;
+
         for (int i = 0, u, v; i < m; ++i)
         {
             scanf("%d%d", &u, &v);
             graph[u].emplace_back(v); // 这里我们只建立单向边
         }
-        int friend_from_left[n2 + 1];
-        bool right_visited[n2 + 1];
-        int result = 0;
-        memset(friend_from_left, 0, sizeof friend_from_left);
-        for (int u = 1; u <= n1; ++u)
+
+        for (int u = 1, stamp = 0; u <= n1; ++u)
         {
-            memset(right_visited, 0, sizeof right_visited);
-            if (has_matched(u, graph, friend_from_left, right_visited))
-            {
+            ++stamp;
+            if (has_matched(u, stamp, graph,
+                            friend_from_left, right_visited))
                 ++result;
-            }
         }
+
         printf("%d\n", result);
+        free(friend_from_left);
+        free(right_visited);
         return 0;
     }
 };
