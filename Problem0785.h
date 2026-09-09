69055033906b7b3112ff81fc3621758ff04536c7
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-08 23:57:30 -0700

    Refactor quicksort implementation and cleanup code

--- Problem0785.h
+++ Problem0785.h
@@ -14,59 +14,54 @@ class Problem0785
 private:
     // 类似题目：LeetCode 912
     // 这里的实现比LeetCode所用的解法稍快，所以不会在AcWing超时
-    void myQuickSort(int *nums, const int &st, const int &ed)
+void my_quick_sort(int *nums, int st, int ed)
+{
+    if (st >= ed)
+        return;
+
+    int p = nums[st + (ed - st + 1) / 2];
+    // int p = nums[st + (ed - st) / 2];
+    int l = st;
+    int r = ed;
+
+    while (l <= r)
     {
-        if (st >= ed)
-        {
-            return;
-        }
-        // auto p = nums[st + (ed - st) / 2];  // 选择偏左的中点
-        auto p = nums[st + (ed - st + 1) / 2]; // 选择偏右的中点
-        auto l = st - 1;
-        auto r = ed + 1;
-        while (l < r)
-        {
+        while (nums[l] < p)
             ++l;
-            while (nums[l] < p)
-            {
-                ++l;
-            }
+
+        while (nums[r] > p)
             --r;
-            while (nums[r] > p)
-            {
-                --r;
-            }
-            if (l < r)
-            {
-                swap(nums[l], nums[r]);
-            }
-        }
-        // 对应偏左的中点
-        // myQuickSort(nums, st, r);
-        // myQuickSort(nums, r + 1, ed);
 
-        // 对应偏右的中点
-        myQuickSort(nums, st, l - 1);
-        myQuickSort(nums, l, ed);
+        if (l <= r)
+        {
+            swap(nums[l], nums[r]);
+            ++l;
+            --r;
+        }
     }
 
-    int main()
-    {
-        int n;
-        scanf("%d", &n);
-        int nums[n];
-        for (int i = 0; i < n; ++i)
-        {
-            scanf("%d", &nums[i]);
-        }
-        myQuickSort(nums, 0, n - 1);
-        for (int i = 0; i < n; ++i)
-        {
-            printf("%d ", nums[i]);
-        }
-        printf("\n");
-        return 0;
-    }
+    my_quick_sort(nums, st, r);
+    my_quick_sort(nums, l, ed);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int *nums = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &nums[i]);
+
+    my_quick_sort(nums, 0, n - 1);
+
+    for (int i = 0; i < n; ++i)
+        printf("%d ", nums[i]);
+
+    printf("\n");
+    free(nums);
+    return 0;
+}
 };
 
 #endif // ACWINGSOLUTION_PROBLEM0785_H
