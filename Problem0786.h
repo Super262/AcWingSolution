13ff4ae118fac99b0406e50088f2dedeb895bfad
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-09 00:06:44 -0700

    Refactor myQuickSelect function and memory allocation

--- Problem0786.h
+++ Problem0786.h
@@ -12,52 +12,50 @@ using namespace std;
 class Problem0786
 {
 private:
-    void myQuickSelect(int *nums, const int &st, const int & ed, const int &k)
+    void myQuickSelect(int *nums, int st, int ed, const int &k)
     {
         if (st >= ed)
-        {
             return;
-        }
+
         auto p = nums[st + (ed - st) / 2];
-        auto l = st - 1;
-        auto r = ed + 1;
-        while (l < r)
+        auto l = st;
+        auto r = ed;
+
+        while (l <= r)
         {
-            ++l;
-            while (nums[l] < p) {
+            while (nums[l] < p)
                 ++l;
-            }
-            --r;
-            while (nums[r] > p) {
+
+            while (nums[r] > p)
                 --r;
-            }
-            if (l < r)
+
+            if (l <= r)
             {
                 swap(nums[l], nums[r]);
+                ++l;
+                --r;
             }
         }
+
         if (k <= r - st + 1)
-        {
             myQuickSelect(nums, st, r, k);
-        }
         else
-        {
-            myQuickSelect(nums, r + 1, ed, k - (r - st + 1));
-        }
+            myQuickSelect(nums, l, ed, k - (l - st));
     }
 
     int main()
     {
-        int n;
-        int k;
+        int n, k;
         scanf("%d%d", &n, &k);
-        int nums[n];
+
+        int *nums = (int *)malloc(sizeof(int) * n);
         for (int i = 0; i < n; ++i)
-        {
             scanf("%d", &nums[i]);
-        }
+        
         myQuickSelect(nums, 0, n - 1, k);
         printf("%d\n", nums[k - 1]);
+        
+        free(nums);
         return 0;
     }
 };
