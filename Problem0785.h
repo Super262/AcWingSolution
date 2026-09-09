c316bcf1fb627ef5168e40c58cb43edfeeac6e91
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-08 23:58:43 -0700

    Refactor quick sort implementation in Problem0785.h

--- Problem0785.h
+++ Problem0785.h
@@ -14,54 +14,53 @@ class Problem0785
 private:
     // 类似题目：LeetCode 912
     // 这里的实现比LeetCode所用的解法稍快，所以不会在AcWing超时
-void my_quick_sort(int *nums, int st, int ed)
-{
-    if (st >= ed)
-        return;
-
-    int p = nums[st + (ed - st + 1) / 2];
-    // int p = nums[st + (ed - st) / 2];
-    int l = st;
-    int r = ed;
-
-    while (l <= r)
+    void my_quick_sort(int *nums, int st, int ed)
     {
-        while (nums[l] < p)
-            ++l;
-
-        while (nums[r] > p)
-            --r;
-
-        if (l <= r)
+        if (st >= ed)
+            return;
+    
+        int p = nums[st + (ed - st) / 2];
+        int l = st;
+        int r = ed;
+    
+        while (l <= r)
         {
-            swap(nums[l], nums[r]);
-            ++l;
-            --r;
+            while (nums[l] < p)
+                ++l;
+    
+            while (nums[r] > p)
+                --r;
+    
+            if (l <= r)
+            {
+                swap(nums[l], nums[r]);
+                ++l;
+                --r;
+            }
         }
+    
+        my_quick_sort(nums, st, r);
+        my_quick_sort(nums, l, ed);
     }
-
-    my_quick_sort(nums, st, r);
-    my_quick_sort(nums, l, ed);
-}
-
-int main()
-{
-    int n;
-    scanf("%d", &n);
-
-    int *nums = (int *)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; ++i)
-        scanf("%d", &nums[i]);
-
-    my_quick_sort(nums, 0, n - 1);
-
-    for (int i = 0; i < n; ++i)
-        printf("%d ", nums[i]);
-
-    printf("\n");
-    free(nums);
-    return 0;
-}
+    
+    int main()
+    {
+        int n;
+        scanf("%d", &n);
+    
+        int *nums = (int *)malloc(sizeof(int) * n);
+        for (int i = 0; i < n; ++i)
+            scanf("%d", &nums[i]);
+    
+        my_quick_sort(nums, 0, n - 1);
+    
+        for (int i = 0; i < n; ++i)
+            printf("%d ", nums[i]);
+    
+        printf("\n");
+        free(nums);
+        return 0;
+    }
 };
 
 #endif // ACWINGSOLUTION_PROBLEM0785_H
